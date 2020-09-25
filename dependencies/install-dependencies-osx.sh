c71fcbaf06e9bba70dfe52cf8827845e2d55aa71
JJ Allaire <jj@rstudio.com>
2020-09-25 09:38:53 -0400

    install deps script for osx

---
+++ dependencies/install-dependencies-osx.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# pandoc
+brew install pandoc
+brew install pandoc-citeproc
+
+# deno
+brew install deno
+
+# tinytex
+curl -sL "https://yihui.org/tinytex/install-bin-unix.sh" | sh
+
