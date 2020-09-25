3236fb829498242466a914f4a3cf763369446c13
JJ Allaire <jj@rstudio.com>
2020-09-25 14:01:30 -0400

    bootstrap quarto script

--- dependencies/install-dependencies-osx.sh
+++ dependencies/install-dependencies-osx.sh
@@ -21,7 +21,7 @@ pushd ../sandbox
 R --quiet -e "renv::restore()"
 
 # install Python dependencies
-if [ -d "pyenv" ] 
+if [ -d "pyenv" ]
 then
   conda activate ./pyenv
   conda env update -f environment.yml
@@ -29,5 +29,15 @@ then
 else
   conda env create -f environment.yml --prefix pyenv
 fi
+
+# generate quarto symlink
+QUARTO_TS=`realpath ../src/quarto.ts`
+cat > quarto.sh <<EOL
+#!/bin/zsh
+deno run --allow-run ${QUARTO_TS} \$@
+EOL
+chmod +x quarto.sh
+QUARTO_SH=`realpath quarto.sh`
+ln -fs ${QUARTO_SH} /usr/local/bin/quarto
 
 popd
---
+++ src/quarto.ts
@@ -0,0 +1,8 @@
+
+
+
+
+for (let i = 0; i < Deno.args.length; i++) {
+  const arg = Deno.args[i];
+  console.log(arg);
+}
