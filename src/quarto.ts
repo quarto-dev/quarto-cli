23618444fd71441b4d50864fb2f5b3bb2d8fddad
JJ Allaire <jj@rstudio.com>
2020-09-28 11:47:10 -0400

    move exitProcess out of quarto function

--- src/quarto.ts
+++ src/quarto.ts
@@ -9,22 +9,19 @@ import { render } from "./command/render.ts";
 
 export async function quarto(args: CommandLineArgs) {
   const [command, input] = args["_"];
-
-  // dispatch command
-  try {
-    if (command === "render") {
-      await render(input.toString());
-    } else {
-      logError("Unknown command " + command);
-      exitProcess(1);
-    }
-  } catch (error) {
-    logError(error.toString());
-    exitProcess(1);
+  if (command === "render") {
+    await render(input.toString());
+  } else {
+    throw new Error("Unknown command " + command);
   }
 }
 
 // main
 if (import.meta.main) {
-  quarto(commandLineArgs());
+  try {
+    await quarto(commandLineArgs());
+  } catch (error) {
+    logError(error.toString());
+    exitProcess(1);
+  }
 }
