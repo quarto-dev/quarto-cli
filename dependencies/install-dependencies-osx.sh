98a9d25e876babf140fd9604b73b6b155caf3276
JJ Allaire <jj@rstudio.com>
2020-09-28 07:12:35 -0400

    pass python binary via environment variable

--- dependencies/install-dependencies-osx.sh
+++ dependencies/install-dependencies-osx.sh
@@ -28,9 +28,11 @@ conda env update -f environment.yml
 # generate quarto symlink
 QUARTO_TS=`realpath ../src/quarto.ts`
 QUARTO_IMPORT_MAP=`realpath ../src/import_map.json`
+QUARTO_PYTHON=`realpath ~/opt/miniconda3/envs/quarto-cli/bin/python`
 cat > quarto.sh <<EOL
 #!/bin/zsh
-deno run --unstable --allow-run --importmap=${QUARTO_IMPORT_MAP} ${QUARTO_TS} \$@
+export QUARTO_PYTHON=${QUARTO_PYTHON}
+deno run --unstable --allow-run --allow-env --importmap=${QUARTO_IMPORT_MAP} ${QUARTO_TS} \$@
 EOL
 chmod +x quarto.sh
 QUARTO_SH=`realpath quarto.sh`
