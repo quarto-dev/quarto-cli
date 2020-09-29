443e2639d0b746e1034951e09ab381a00335d591
JJ Allaire <jj@rstudio.com>
2020-09-29 12:25:50 -0400

    use rmarkdown::render for knitr preprocessor

--- src/computation/preprocessor/knitr/knitr.R
+++ src/computation/preprocessor/knitr/knitr.R
@@ -4,10 +4,31 @@ args <- commandArgs(trailingOnly = TRUE)[-1]
 input <- args[[1]]
 output <- args[[2]]
 
-# convert to markdown
-knitr::opts_chunk$set(comment = NA)
-knitr::knit(input, output)
+# synthesize output format
+library(rmarkdown)
+knitr_options <- knitr_options_html(
+  fig_width = 7,
+  fig_height = 5,
+  fig_retina = 2,
+  FALSE,
+  dev = "png"
+)
+pandoc_options <- pandoc_options(
+  to = "html"
+)
+output_format <- output_format(
+  knitr = knitr_options,
+  pandoc = pandoc_options
+)
 
+# run knitr but not pandoc
+md_result <- rmarkdown::render(
+  input = input,
+  output_format = output_format,
+  run_pandoc = FALSE
+)
 
+# rename the markdown file
+file.rename(file.path(dirname(input), md_result), output)
 
 
