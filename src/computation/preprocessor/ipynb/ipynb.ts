4a98a226e8e64bd48dcde5a178f0a4001da1b22c
JJ Allaire <jj@rstudio.com>
2020-09-30 14:53:02 -0400

    stub for metadata

--- src/computation/preprocessor/ipynb/ipynb.ts
+++ src/computation/preprocessor/ipynb/ipynb.ts
@@ -1,11 +1,16 @@
-import type { ComputationPreprocessor } from "../../../api/computation.ts";
+import type { Metadata } from "../../../core/metadata.ts";
 import { execProcess } from "../../../core/process.ts";
+import type { ComputationPreprocessor } from "../preprocessor.ts";
 
 export const ipynbPreprocessor: ComputationPreprocessor = {
   name: "ipynb",
 
   canHandle: (ext: string) => {
     return [".ipynb"].includes(ext.toLowerCase());
+  },
+
+  metadata: async (file: string): Promise<Metadata> => {
+    return {};
   },
 
   preprocess: async (file: string, outputFile: string): Promise<void> => {
--- src/computation/preprocessor/preprocessor.ts
+++ src/computation/preprocessor/preprocessor.ts
@@ -1,5 +1,8 @@
+import type { Metadata } from "../../core/metadata.ts";
+
 export interface ComputationPreprocessor {
   name: string;
   canHandle: (ext: string) => boolean;
+  metadata: (file: string) => Promise<Metadata>;
   preprocess: (file: string, outputFile: string) => Promise<void>;
 }
