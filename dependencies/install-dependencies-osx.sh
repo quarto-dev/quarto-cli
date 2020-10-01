00a10c18e8e3f289f029911929a52abc8614229b
JJ Allaire <jj@rstudio.com>
2020-10-01 10:05:44 -0400

    ensure that tests and debugger can see QUARTO_RESOURCES

--- dependencies/install-dependencies-osx.sh
+++ dependencies/install-dependencies-osx.sh
@@ -26,6 +26,12 @@ R --quiet -e "renv::restore()"
 # update w/: conda env export > environment.yml
 conda env update -f environment.yml
 
+# generate scripts, first some common vars
+QUARTO_TS=`realpath ../src/main.ts`
+QUARTO_IMPORT_MAP=`realpath ../src/import_map.json`
+QUARTO_RESOURCES=`realpath ../src/resources/`
+DENO_OPTIONS="--unstable --allow-read --allow-run --allow-env --importmap=${QUARTO_IMPORT_MAP}"
+
 # generate quarto symlink
 QUARTO_TS=`realpath ../src/main.ts`
 QUARTO_IMPORT_MAP=`realpath ../src/import_map.json`
@@ -33,10 +39,18 @@ QUARTO_RESOURCES=`realpath ../src/resources/`
 cat > quarto.sh <<EOL
 #!/bin/zsh
 export QUARTO_RESOURCES=${QUARTO_RESOURCES}
-deno run --unstable --allow-read --allow-run --allow-env --importmap=${QUARTO_IMPORT_MAP} ${QUARTO_TS} \$@
+deno run ${DENO_OPTIONS} ${QUARTO_TS} \$@
 EOL
 chmod +x quarto.sh
 QUARTO_SH=`realpath quarto.sh`
 ln -fs ${QUARTO_SH} /usr/local/bin/quarto
+
+# generate test script
+cat > ../tests/run-tests.sh <<EOL
+#!/bin/zsh
+export QUARTO_RESOURCES=${QUARTO_RESOURCES}
+deno test ${DENO_OPTIONS} \$@
+EOL
+chmod +x ../tests/run-tests.sh
 
 popd
