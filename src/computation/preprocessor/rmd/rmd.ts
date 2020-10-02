9b6b6628342ef9001342bbad77da488cce8b9996
JJ Allaire <jj@rstudio.com>
2020-10-02 16:08:33 -0400

    more work on formats

--- src/computation/preprocessor/rmd/rmd.ts
+++ src/computation/preprocessor/rmd/rmd.ts
@@ -3,6 +3,7 @@ import { execProcess } from "../../../core/process.ts";
 import type { ComputationPreprocessor } from "../preprocessor.ts";
 import { resourcePath } from "../../../core/resources.ts";
 import { metadataFromFile } from "../../../core/metadata.ts";
+import type { Format } from "../../../api/format.ts";
 
 export const rmdPreprocessor: ComputationPreprocessor = {
   name: "rmd",
@@ -13,16 +14,26 @@ export const rmdPreprocessor: ComputationPreprocessor = {
 
   metadata: metadataFromFile,
 
-  preprocess: async (file: string, outputFile: string): Promise<void> => {
-    const result = await execProcess({
-      cmd: [
-        "Rscript",
-        resourcePath("rmd.R"),
-        "--args",
-        file,
-        outputFile,
-      ],
+  preprocess: async (
+    file: string,
+    format: Format,
+    outputFile: string,
+  ): Promise<void> => {
+    const input = JSON.stringify({
+      input: file,
+      format,
+      output: outputFile,
     });
+
+    const result = await execProcess(
+      {
+        cmd: [
+          "Rscript",
+          resourcePath("rmd.R"),
+        ],
+      },
+      input,
+    );
 
     if (!result.success) {
       return Promise.reject();
--- src/formats/html_document.ts
+++ src/formats/html_document.ts
@@ -6,20 +6,32 @@ export const htmlDocument: FormatDefinition = {
 
   options: [
     ...standardDocOptions(),
+    {
+      name: "self_contained",
+      description:
+        "Produce a standalone HTML file with no external dependencies",
+      default: false,
+    },
   ],
 
   create: (options: FormatOptions) => {
     // unroll options
     const toc = options.toc as boolean;
+    const self_contained = options.self_contained as boolean;
     const fig_width = options.fig_width as number;
     const fig_height = options.fig_height as number;
 
     // build pandoc args
-    const args: string[] = [];
+    const args: string[] = ["--standalone"];
 
     // table of contents
     if (toc) {
       args.push("--toc");
+    }
+
+    // self-contianed
+    if (self_contained) {
+      args.push("--self-contained");
     }
 
     return {
