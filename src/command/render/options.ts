d6185657e576437c3b57d4763f0bf105253c828d
JJ Allaire <jj@rstudio.com>
2020-10-06 14:24:22 -0400

    merge arrays by concatenation

--- src/command/render/options.ts
+++ src/command/render/options.ts
@@ -1,8 +1,8 @@
 import { ld } from "lodash/mod.ts";
 
-import type { FormatOptions, FormatPandocOptions } from "../../api/format.ts";
+import type { FormatOptions } from "../../api/format.ts";
 
-import type { QuartoConfig } from "../../core/config.ts";
+import { arrayMerger, QuartoConfig } from "../../core/config.ts";
 
 // TODO: the first format listed in the file needs to take precedence over formats
 // listed in the project file
@@ -104,7 +104,7 @@ function formatOptions(ext: string, ...options: FormatOptions[]) {
 function mergeFormatOptions(
   ...options: FormatOptions[]
 ): FormatOptions {
-  return ld.merge(options[0], ...options.slice(1));
+  return ld.mergeWith(options[0], ...options.slice(1), arrayMerger);
 }
 
 function defaultFormatOptions(): FormatOptions {
@@ -130,72 +130,3 @@ function defaultFormatOptions(): FormatOptions {
     },
   };
 }
-
-/*
-export function formatFromConfig(config: QuartoConfig, name?: string) {
-  // if there is an explicit name then look for it in the config
-  let options: FormatOptions = {};
-  if (name) {
-    if (config.output) {
-      options = config.output[name] || {};
-    }
-  } else {
-    name = htmlDocument.name;
-    if (config.output) {
-      const formats = Object.keys(config.output);
-      if (formats.length > 0) {
-        name = formats[0];
-        options = config.output[name] || {};
-      }
-    }
-  }
-
-  // determine target format
-  const format = allFormats.find((format) => format.name === name) ||
-    htmlDocument;
-
-  // resolve all format options
-  const resolvedOptions: FormatOptions = {};
-  format.options.forEach((option) => {
-    const value = options[option.name] || option.default;
-    if (typeof value === typeof option.default) {
-      resolvedOptions[option.name] = value;
-    }
-  });
-
-  // create the format
-  const fmt = format.create(resolvedOptions);
-
-  // provide defaults
-  fmt.preprocessor = fmt.preprocessor || {};
-  fmt.preprocessor = {
-    fig_width: 7,
-    fig_height: 5,
-    fig_format: "png",
-    fig_dpi: 96,
-    hide_code: false,
-    show_warnings: false,
-    show_messages: false,
-    ...fmt.preprocessor,
-  };
-  fmt.pandoc = fmt.pandoc || {};
-  fmt.pandoc = {
-    to: "html5",
-    from: "markdown",
-    ext: "html",
-    args: [],
-    ...fmt.pandoc,
-  };
-
-  fmt.keep_md = fmt.keep_md || false;
-  fmt.keep_tex = fmt.keep_tex || false;
-  fmt.clean_supporting = fmt.clean_supporting || false;
-
-  return fmt;
-}
-
-const allFormats: FormatDefinition[] = [
-  htmlDocument,
-  pdfDocument,
-];
-*/
--- src/core/config.ts
+++ src/core/config.ts
@@ -59,5 +59,14 @@ export function mergeConfigs(...configs: QuartoConfig[]): QuartoConfig {
     });
   }
 
-  return ld.merge(configs[0], ...configs.slice(1));
+  return ld.mergeWith(configs[0], ...configs.slice(1), arrayMerger);
+}
+
+export function arrayMerger(objValue: unknown, srcValue: unknown) {
+  if (ld.isArray(objValue) && ld.isArray(srcValue)) {
+    const combined = (objValue as Array<unknown>).concat(
+      srcValue as Array<unknown>,
+    );
+    return ld.uniqBy(combined, ld.toString);
+  }
 }
