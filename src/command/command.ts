c5e2d172a418a23ef244cf16e3ceef0c866a69f3
JJ Allaire <jj@rstudio.com>
2020-10-17 15:35:05 -0400

    add license files

--- src/command/command.ts
+++ src/command/command.ts
@@ -1,3 +1,18 @@
+/*
+* command.ts
+*
+* Copyright (C) 2020 by RStudio, PBC
+*
+* Unless you have received this program directly from RStudio pursuant
+* to the terms of a commercial license agreement with RStudio, then
+* this program is licensed to you under the terms of version 3 of the
+* GNU General Public License. This program is distributed WITHOUT
+* ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+* MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+* GPL (http://www.gnu.org/licenses/gpl-3.0.txt) for more details.
+*
+*/
+
 import type { Command } from "cliffy/command/mod.ts";
 
 import { renderCommand } from "./render/render.ts";
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,16 @@
+/*
+* constants.ts
+*
+* Copyright (C) 2020 by RStudio, PBC
+*
+* Unless you have received this program directly from RStudio pursuant
+* to the terms of a commercial license agreement with RStudio, then
+* this program is licensed to you under the terms of version 3 of the
+* GNU General Public License. This program is distributed WITHOUT
+* ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+* MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+* GPL (http://www.gnu.org/licenses/gpl-3.0.txt) for more details.
+*
+*/
+
 export const kSelfContained = "self-contained";
