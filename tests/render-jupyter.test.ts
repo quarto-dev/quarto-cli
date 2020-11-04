9e79c6c2da24b30cc17feb68c492e7d6d7d15cb5
Charles Teague <cteague@gmail.com>
2020-11-04 15:36:20 -0500

    Further verification

--- tests/render-jupyter.test.ts
+++ tests/render-jupyter.test.ts
@@ -1,22 +1,33 @@
+
+// TODO: Intermediary test.exec.ipynb is hangout around after test completion
+
 import { quarto } from "../src/quarto.ts";
-import { verifyAndCleanOutput } from "./verify.ts";
+import { verifyAndCleanOutput, verifyNoPath } from "./verify.ts";
+
+
+const htmlOutput = "docs/test-jupyter.html";
+const supportFilesPath = "docs/test-jupyter_files";
 
 Deno.test("ipynb render", async () => {
   await quarto(["render", "docs/test-jupyter.ipynb"]);
-  verifyAndCleanOutput("docs/test-jupyter.html");
+  verifyAndCleanOutput(htmlOutput);
+  verifyAndCleanOutput(supportFilesPath);
 });
 
 Deno.test("Jupyter Markdown Render", async () => {
   await quarto(["render", "docs/test-jupyter.md"]);
-  verifyAndCleanOutput("docs/test-jupyter.html");
+  verifyAndCleanOutput(htmlOutput);
+  verifyAndCleanOutput(supportFilesPath);
 });
 
 Deno.test("Python Jupytext Render", async () => {
   await quarto(["render", "docs/test-jupyter.py"]);
-  verifyAndCleanOutput("docs/test-jupyter.html");
+  verifyAndCleanOutput(htmlOutput);
+  verifyAndCleanOutput(supportFilesPath);
 });
 
 Deno.test("Python Markdown PDF Render", async () => {
   await quarto(["render", "docs/test-jupyter.md", "--to", "pdf"]);
   verifyAndCleanOutput("docs/test-jupyter.pdf");
+  verifyNoPath(supportFilesPath);
 });
