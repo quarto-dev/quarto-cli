90b49f031e429f3b6b3cf78b2ea4b3199b453c21
Charles Teague <cteague@gmail.com>
2020-11-04 15:36:20 -0500

    Further verification

--- tests/render-r.test.ts
+++ tests/render-r.test.ts
@@ -1,11 +1,12 @@
 import { quarto } from "../src/quarto.ts";
 import { join } from "path/mod.ts";
-import { verifyAndCleanOutput } from "./verify.ts";
+import { verifyAndCleanOutput, verifyNoPath } from "./verify.ts";
 
+const supportingFilesPath = "docs/test_files";
 
 // Basic HTML rendering with a plot
 Deno.test("Rmd Render", async () => {
-  const supportingFilesPath = "docs/test_files";
+  
   const plotOutputPath = join(
     supportingFilesPath,
     "figure-html/unnamed-chunk-1-1.png",
@@ -21,12 +22,15 @@ Deno.test("Rmd Render", async () => {
 // Basic PDF rendering
 Deno.test("Rmd Render", async () => {
   await quarto(["render", "docs/test.Rmd", "--to", "pdf"]);
+  
   verifyAndCleanOutput("docs/test.pdf");
+  verifyNoPath(supportingFilesPath);
 });
 
 // Basic script rendering
 Deno.test("R Script Render", async () => {
   await quarto(["render", "docs/test.R"]);
+  
   verifyAndCleanOutput("docs/test.html");
 });
 
@@ -35,5 +39,6 @@ Deno.test("Rmd with Params", async () => {
   await quarto(
     ["render", "docs/test-params.Rmd", "--execute-params", "params.yml"],
   );
+  
   verifyAndCleanOutput("docs/test-params.html");
 });
