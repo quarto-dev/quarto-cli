9d690f5655a9f162f37fc1ac3f4cbceb222297c0
JJ Allaire <jj@rstudio.com>
2020-11-07 14:23:03 -0500

    automatically  use mathjax when math is in the document

---
+++ src/resources/filters/html-defaults.lua
@@ -0,0 +1,26 @@
+
+-- detect math
+local math_detected = false
+function DetectMath(s)
+  math_detected = true
+  return ""
+end
+InlineMath = DetectMath
+DisplayMath = DetectMath
+
+-- provide defaults based on tokens detected
+function Doc(body, metadata, variables)
+  if math_detected then
+     return 'html-math-method: mathjax\n'
+  else
+     return ''
+  end
+end
+
+-- ingore all other tokens
+local meta = {}
+meta.__index =
+  function(_, key)
+    return function() return "" end
+  end
+setmetatable(_G, meta)
