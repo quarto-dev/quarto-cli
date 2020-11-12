981b2cf4e8352e864d3017c1751aa1cfea7586b0
JJ Allaire <jj@rstudio.com>
2020-11-12 11:33:26 -0500

    more format work

--- src/resources/julia.jl
+++ src/resources/julia.jl
@@ -27,61 +27,259 @@ using Weave
 # - correct handling of rich (e.g. plotly) outputs 
 #
 
+function execute(target, output, format, quiet)
+    # input/output
+    input = get(target, "input", "")
+
+    # execution
+    execution = get(format, "execution", Dict([]))
+    fig_format = get(execution, "fig-format", "png")
+    cache = get(execution, "cache", "user")
+    if cache == "none"
+        cache = "off"
+    end
+
+    # metadata
+    metadata = get(format, "metadata", Dict([]))
+    weave_options = get(format, "weave_options", nothing)
+
+    # pandoc
+    pandoc = get(format, "pandoc", Dict([]))
+    to = get(pandoc, "to", "html")
+
+    # progress
+    if !quiet 
+        write(stderr, string("Weaving ", input, "\n\n"))
+    end
+
+    # if fig_format is 'retina' then convert it to 'png'
+    if fig_format == "retina"
+        fig_format = "png"
+    end
+
+    # compute the fig_path
+    stem = splitext(basename(input))[1]
+    files_dir = string(stem, "_files")
+    to = replace(to, r"[\+\-].*$" => s"");
+    fig_dir = joinpath(files_dir, string("figure-", to));
+    mkpath(fig_dir)
+
+    # set chunk defaults
+    # https://github.com/JunoLab/Weave.jl/blob/8206ca2d14b56293e55964f1cf53ef53c4238cc7/src/config.jl#L2
+    show_output = get(execution, "show-output", true)
+    set_chunk_defaults!(
+      :error => get(execution, "allow-errors", false),
+      :echo => get(execution, "show-code", true),
+      :results => ifelse(show_output, "markup", "hidden"),
+      :fig => show_output,
+      :fig_width => get(execution, "fig-width", 7),
+      :fig_height => get(execution, "fig-height", 5),
+      :dpi => get(execution, "fig-dpi", 96)
+    )
+
+    # set the cache default correctly if it's not 'user'
+    if cache == "all" || cache == "refresh"
+        set_chunk_defaults!(:cache => true)
+    elseif cache == "off"
+        set_chunk_defaults!(:cache => false)
+    end
+
+    # weave the doc
+    doc = weave_doc(input, get(format, "metadata", Dict([])))
+
+    # run the doc
+    doc = Weave.run_doc(
+      doc;
+      doctype="pandoc",
+      out_path=:doc,
+      fig_path=fig_dir,
+      fig_ext=string(".", fig_format),
+      cache_path=".julia-cache",
+      cache=Symbol(cache),
+    )
+    
+
+    # render chunks
+    pandoc_format = Weave.Pandoc()
+    Weave.restore_header!(doc)
+    body = []
+    docformat = doc.format
+    chunks = copy(doc.chunks)
+    for (index, value) in enumerate(chunks)
+        rendered = render_chunk(pandoc_format, value)
+        append!(body, rendered)
+        append!(body, "\n")
+    end
+    body = join(body, "")
+
+    # write out the results
+    open(output, "w") do io
+        write(io, body)
+    end;
+
+    # check if cache was used (affects supporting)
+    defaults = get_chunk_defaults()
+    cache_active = get(defaults, "cache", false)
+
+    # return result
+    result = Dict([
+      ("supporting", ifelse(cache_active, [], [fig_dir])), 
+      ("pandoc", Dict())
+    ])
+    return result
+end
+
+function weave_doc(input, metadata)
+
+    # weave the doc
+    header, chunks = Weave.parse_doc(read(input, String), "markdown")
+    
+    # update default chunk options from header
+    chunk_defaults = deepcopy(get_chunk_defaults())
+    if (weave_options = get(metadata, Weave.WEAVE_OPTION_NAME, nothing)) !== nothing
+        for key in keys(chunk_defaults)
+            if (val = get(weave_options, string(key), nothing)) !== nothing
+                chunk_defaults[key] = val
+            end
+        end
+    end
+
+    # some paths
+    path = abspath(input)
+    _, fname = splitdir(path)
+    basename = splitext(fname)[1]
+
+    # do the weave
+    doc = Weave.WeaveDoc(
+      input,
+      basename,
+      path,
+      chunks,
+      "",
+      nothing,
+      "",
+      "",
+      header,
+      chunk_defaults,
+    ) 
+
+    # if execution is turned off then turn off eval for all the cells
+    if !get(get(format, "execution", Dict([])), "execute", true)
+        for (index, value) in enumerate(doc.chunks)
+            if typeof(value) == Weave.CodeChunk
+                value.options[:eval] = false
+            end
+        end
+    end
+
+    # return 
+    return doc
+
+end
+
+render_chunk(docformat::Weave.WeaveFormat, chunk::Weave.DocChunk) = join((Weave.render_inline(c) for c in chunk.content))
+
+
+function render_code(code)
+    return string("```julia", code, "```\n")
+end
+
+function render_output(output, classes)
+    result = string("\n::: {.output ", classes, "}", "\n")
+    result *= output
+    result *= "\n:::\n"
+    return result
+end
+
+function render_raw_output(output, type)
+    raw = string("```{=", type, "}\n")
+    raw *= output
+    if !endswith(raw, "\n")
+        raw *= "\n"
+    end
+    raw *= "```"
+    return render_output(raw, ".display_data")
+end
+
+function render_chunk(docformat::Weave.WeaveFormat, chunk::Weave.CodeChunk)
+
+    # code cell div
+    result = string("::: {.cell .code}", "\n")
+    
+    # check echo option
+    echo = chunk.options[:echo]
+
+    # return just code if there is no eval
+    if !chunk.options[:eval]
+        if echo
+            result *= render_code(chunk.content)
+            result *= ":::\n"
+            return result
+        else
+            return ""
+        end
+    end
+
+    if chunk.options[:term]
+        if Weave.should_render(chunk)
+            result *= render_code(chunk.output)
+        end
+    else
+        result *= if echo
+            render_code(chunk.content)
+        else
+            ""
+        end
+
+        if (strip(chunk.output) ≠ "" || strip(chunk.rich_output) ≠ "") && (chunk.options[:results] ≠ "hidden")
+            if chunk.options[:results] ≠ "markup" && chunk.options[:results] ≠ "hold"
+                type = chunk.options[:results]
+                if strip(chunk.output) ≠ ""
+                    result *= render_raw_output(chunk.output, type)
+                end
+                if strip(chunk.rich_output) ≠ ""
+                    result *= render_raw_output(chunk.rich_output, type)
+                end
+            else
+                if chunk.options[:wrap]
+                    chunk.output =
+                      '\n' * Weave.wraplines(chunk.output, chunk.options[:line_width])
+                else
+                    chunk.output = '\n' * rstrip(chunk.output)
+                end
+
+                if strip(chunk.output) ≠ ""
+                    result *= render_output(chunk.output, ".stream .stdout")
+                end
+                if strip(chunk.rich_output) ≠ ""
+                    result *= render_output(chunk.rich_output, ".display_data")
+                end
+            end
+        end
+    end
+
+    # Handle figures
+    if chunk.options[:fig] && length(chunk.figures) > 0
+        result *= Weave.render_figures(docformat, chunk)
+    end
+
+    result *= ":::\n"
+
+    return result
+end
+
 
 # read options
 options = JSON.parse(readline(stdin))
 target = get(options, "target", Dict([]))
-input = get(target, "input", "")
 output = get(options, "output", "")
-quiet = get(options, "quiet", false)
 format = get(options, "format", Dict([]))
-execution = get(format, "execution", Dict([]))
-fig_format = get(execution, "fig-format", "png")
-pandoc = get(format, "pandoc", Dict([]))
-to = get(pandoc, "to", "html")
+quiet = get(options, "quiet", false)
 
-if !quiet 
-    write(stderr, string("Weaving ", input, "\n\n"))
-end
-
-# if fig_format is 'retina' then convert it to 'png'
-if fig_format == "retina"
-    fig_format = "png"
-end
-
-# compute the fig_path
-stem = splitext(basename(input))[1]
-files_dir = string(stem, "_files")
-to = replace(to, r"[\+\-].*$" => s"");
-fig_dir = joinpath(files_dir, string("figure-", to));
-mkpath(fig_dir)
-
-# set chunk defaults
-# https://github.com/JunoLab/Weave.jl/blob/8206ca2d14b56293e55964f1cf53ef53c4238cc7/src/config.jl#L2
-set_chunk_defaults!(
-  :error => get(execution, "allow-errors", false),
-  :echo => get(execution, "show-code", true),
-  :results => ifelse(get(execution, "show-output", true), "markup", "hidden"),
-  :fig_width => get(execution, "fig-width", 7),
-  :fig_height => get(execution, "fig-height", 5),
-  :dpi => get(execution, "fig-dpi", 96)
-)
-
-# weave to pandoc markdown
-out_path = weave(
-  input, 
-  doctype="pandoc",
-  fig_path=fig_dir,
-  fig_ext=string(".", fig_format),
-)
-mv(out_path, output, force=true)
+# execute
+result = execute(target, output, format, quiet)
 
 # return result
-result = Dict([
-  ("supporting", []), 
-  ("pandoc", Dict())
-])
 JSON.print(stdout, result)
-
 
 
