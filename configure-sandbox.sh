c9f604916f41d9e9b0ffc9cf1c5b5544001df3ac
Charles Teague <cteague@gmail.com>
2020-11-19 08:02:51 -0500

    quarto script self awareness

---
+++ configure-sandbox.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+
+# prerequisites:
+#   - Installation of R
+#   - Installation of Conda
+
+
+# install tinytext
+# curl -sL "https://yihui.org/tinytex/install-bin-unix.sh" | sh
+
+# init sandbox
+pushd sandbox
+
+# install R dependencies
+# update w: renv::snapshot()
+R --quiet -e "renv::restore()"
+
+# install Python dependencies
+# update w/: conda env export > environment.yml
+conda env update -f environment.yml
+
+popd
