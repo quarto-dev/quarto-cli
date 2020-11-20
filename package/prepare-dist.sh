4d8f403b1bdc887393bf58819912c4daaa3b0b58
Charles Teague <cteague@gmail.com>
2020-11-20 13:37:35 -0500

    cleanups

--- package/prepare-dist.sh
+++ package/prepare-dist.sh
@@ -2,6 +2,12 @@ source ../configure
 
 pushd $WORKING_DIR
 
+
+# TODO: What are we getting out of bundling, if anything
+	# maybe should just bundle ts files to get good stack traces
+	# if better than 200ms startup time, that would be worth it
+	# test with wrapper doing simple render and timing it
+
 # Create the Deno bundle
 $BIN_DIR/deno bundle --unstable --importmap=../../src/import_map.json ../../src/quarto.ts bin/quarto.js
 
