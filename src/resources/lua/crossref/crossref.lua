4b2517ff548c79d563b58f1afe331e29557eb0c3
JJ Allaire <jj@rstudio.com>
2020-11-20 09:59:11 -0500

    fix indentation

--- src/resources/lua/crossref/crossref.lua
+++ src/resources/lua/crossref/crossref.lua
@@ -1,8 +1,8 @@
 
 -- imports
 function script_path()
-   local str = debug.getinfo(2, "S").source:sub(2)
-   return str:match("(.*/)")
+  local str = debug.getinfo(2, "S").source:sub(2)
+  return str:match("(.*/)")
 end
 dofile(script_path() .. "figure.lua")
 dofile(script_path() .. "index.lua")
