3f3c95a6ae1ba1be16dfffa792f155e2c2752e07
Charles Teague <cteague@gmail.com>
2020-11-25 12:48:14 -0500

    a little cleanup

--- configure.sh
+++ configure.sh
@@ -12,18 +12,20 @@ if [ ! -d "$WORKING_DIR" ]; then
 fi
 pushd $WORKING_DIR
 
-## Gather the resources
-if [ ! -d "$RESOURCES_DIR" ]; then
-	mkdir -p "$RESOURCES_DIR"
+## Share Directory
+if [ ! -d "$SHARE_DIR" ]; then
+	mkdir -p "$SHARE_DIR"
 fi
-cp -a ../../src/resources/* $RESOURCES_DIR/
+cp -a ../../src/resources/* $SHARE_DIR/
 
+## Binary Directory
 if [ ! -d "$BIN_DIR" ]; then
 	mkdir -p "$BIN_DIR"
 fi
 pushd $BIN_DIR
 
 # Download Dependencies
+## TODO: Convert to S3 / repo location
 DENOURL=https://github.com/denoland/deno/releases/download/
 DENOFILE=deno-x86_64-apple-darwin.zip
 curl -fail -L $DENOURL/$DENO/$DENOFILE -o $DENOFILE --no-include
@@ -39,13 +41,8 @@ cp $PANDOCDIR/bin/pandoc .
 rm -rf $PANDOCDIR
 rm $PANDOCFILE
 
-PANDOCCROSSREFURL=https://github.com/lierdakil/pandoc-crossref/releases/download/
-PANDOCCROSSREFFILE=pandoc-crossref-macOS.tar.xz
-curl -fail -L $PANDOCCROSSREFURL/$PANDOCCROSSREF/$PANDOCCROSSREFFILE -o $PANDOCCROSSREFFILE --no-include
-tar -xf $PANDOCCROSSREFFILE
-rm $PANDOCCROSSREFFILE
-
 popd
+
 
 # Move the quarto shell script into place
 cp ../scripts/macos/quarto $BIN_DIR/quarto
@@ -54,7 +51,7 @@ cp ../scripts/macos/quarto $BIN_DIR/quarto
 cp ../../COPYRIGHT .
 cp ../../COPYING.md .
 
-# setup symlink
+# setup local symlink
 ln -fs $(realpath $BIN_DIR/quarto) /usr/local/bin/quarto
 
 popd
