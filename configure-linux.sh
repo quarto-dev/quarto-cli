55faa9b8e873922a587d02978c2c2fa2434be80d
Charles Teague <cteague@gmail.com>
2020-12-02 14:46:54 -0500

    Reduce sharing

--- configure-linux.sh
+++ configure-linux.sh
@@ -2,7 +2,29 @@
 
 source configuration
 
-export DENOFILE=deno-x86_64-unknown-linux-gnu.zip
-export PANDOCFILE=andoc-2.11.2-linux-amd64.tar.gz
-
 source $QUARTO_PACKAGE_DIR/scripts/configure-base.sh
+
+pushd $QUARTO_PACKAGE_DIR/$QUARTO_DIST_DIR/$QUARTO_BIN_DIR
+
+# Download Dependencies
+## TODO: Convert to S3 / repo location
+DENOURL=https://github.com/denoland/deno/releases/download/
+DENOFILE=deno-x86_64-apple-darwin.zip
+wget -O $DENOFILE $DENOURL/$DENO/$DENOFILE
+unzip -o $DENOFILE
+rm $DENOFILE
+
+PANDOCURL=https://github.com/jgm/pandoc/releases/download/
+PANDOCFILE=pandoc-$PANDOC-macOS.zip
+PANDOCDIR=pandoc-$PANDOC
+wget -O $PANDOCFILE $PANDOCURL/$PANDOC/$PANDOCFILE
+unzip -o $PANDOCFILE
+cp $PANDOCDIR/bin/pandoc .
+rm -rf $PANDOCDIR
+rm $PANDOCFILE
+
+popd
+
+
+
+
--- configure-macos.sh
+++ configure-macos.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+set -x 
 
 source configuration
 
-export DENOFILE=deno-x86_64-apple-darwin.zip
-export PANDOCFILE=pandoc-$PANDOC-macOS.zip
 source $QUARTO_PACKAGE_DIR/scripts/configure-base.sh
+
+pushd $QUARTO_PACKAGE_DIR/$QUARTO_DIST_DIR/$QUARTO_BIN_DIR
+
+# Download Dependencies
+## TODO: Convert to S3 / repo location
+DENOURL=https://github.com/denoland/deno/releases/download/
+DENOFILE=deno-x86_64-apple-darwin.zip
+curl -fail -L $DENOURL/$DENO/$DENOFILE -o $DENOFILE --no-include
+unzip -o $DENOFILE
+rm $DENOFILE
+
+PANDOCURL=https://github.com/jgm/pandoc/releases/download/
+PANDOCFILE=pandoc-$PANDOC-macOS.zip
+PANDOCDIR=pandoc-$PANDOC
+curl -fail -L $PANDOCURL/$PANDOC/$PANDOCFILE -o $PANDOCFILE --no-include
+unzip -o $PANDOCFILE
+cp $PANDOCDIR/bin/pandoc .
+rm -rf $PANDOCDIR
+rm $PANDOCFILE
+
+popd
+
+
+
+
