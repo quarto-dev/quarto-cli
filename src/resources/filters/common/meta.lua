4d350dac125886dc4ceb28ca8c5bf193d211acd3
JJ Allaire <jj@rstudio.com>
2020-12-03 16:36:46 -0500

    use subcaption rather than subfig package

--- src/resources/filters/common/meta.lua
+++ src/resources/filters/common/meta.lua
@@ -1,8 +1,10 @@
 
 
+-- constants
+local kHeaderIncludes = "header-includes"
+
 -- ensure that header-includes is a MetaList
 function ensureHeaderIncludes(doc)
-  local kHeaderIncludes = "header-includes"
   if not doc.meta[kHeaderIncludes] then
     doc.meta[kHeaderIncludes] = pandoc.MetaList({})
   elseif doc.meta[kHeaderIncludes].t == "MetaInlines" then
@@ -12,7 +14,7 @@ end
 
 -- add a header include as a raw block
 function addHeaderInclude(doc, format, include)
-  doc.meta["header-includes"]:insert(pandoc.MetaBlocks(pandoc.RawBlock(format, include)))
+  doc.meta[kHeaderIncludes]:insert(pandoc.MetaBlocks(pandoc.RawBlock(format, include)))
 end
 
 -- conditionally include a package
