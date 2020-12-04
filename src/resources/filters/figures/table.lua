8ee741a17c2687362459d47c2d9172f931e2d94d
JJ Allaire <jj@rstudio.com>
2020-12-04 13:30:50 -0500

    simple layout for table subfigs

--- src/resources/filters/figures/table.lua
+++ src/resources/filters/figures/table.lua
@@ -2,6 +2,32 @@
 
 
 function tablePanel(divEl, subfigures)
-  return divEl
+  
+    -- create panel
+  local panel = pandoc.Div({})
+  
+  
+  -- alignment
+  -- local align = attribute(divEl, "fig-align", nil)
+  -- if align then
+  --  panel.content:insert(latexBeginAlign(align))
+  -- end
+  
+  -- subfigures
+  local subfiguresEl = pandoc.Para({})
+  for i, row in ipairs(subfigures) do
+    
+    for _, image in ipairs(row) do
+      
+      if image.t == "Image" then
+        panel.content:insert(pandoc.Para(image))
+      else
+        panel.content:insert(image)
+      end
+      
+    end
+  end
+  
+  return panel
 end
 
