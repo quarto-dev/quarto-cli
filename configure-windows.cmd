abbe8ff1485e9f2d621590e7ed3a2bbe18a77631
Charles Teague <charles@loseit.com>
2020-12-04 14:53:59 -0500

    Create Quarto-Cmd shortcut

--- configure-windows.cmd
+++ configure-windows.cmd
@@ -29,23 +29,22 @@ IF NOT EXIST %BIN_DIR% (
 
 PUSHD %BIN_DIR%
 
+ECHO Copying Quarto command...
 REM Create quarto cmd
 COPY ..\..\scripts\windows\quarto.cmd quarto.cmd
+ECHO.
 
-REM Add Quarto Bin to Path
-SET currentDir=%~dp0
-SET "binPath=%currentDir%"  
-IF "!path:%binPath%=!" EQU "%path%" (
-	SETX PATH "%PATH%;%binPath%"
-)
-
+ECHO Downloading Dependencies...
+ECHO Deno
 REM Download Deno
 SET DENO_FILE="deno-x86_64-pc-windows-msvc.zip"
 SET DENO_URL="https://github.com/denoland/deno/releases/download/%DENO%/%DENO_FILE%"
 CURL --fail -L %DENO_URL% -o %DENO_FILE%
 TAR -xvf %DENO_FILE%
 DEL %DENO_FILE%
+ECHO .
 
+ECHO Pandoc
 REM download Pandoc
 SET PANDOC_FILE="pandoc-%PANDOC%-windows-x86_64.zip"
 SET PANDOC_URL="https://github.com/jgm/pandoc/releases/download/%PANDOC%/%PANDOC_FILE%"
@@ -54,10 +53,12 @@ TAR -xvf %PANDOC_FILE%
 MOVE /Y pandoc-%PANDOC%\pandoc.exe pandoc.exe
 DEL %PANDOC_FILE%
 RMDIR/S /Q pandoc-%PANDOC%
+ECHO.
+ECHO.
 
 POPD
 POPD
 POPD
 
-ECHO PLEASE ADD THE FOLLOWING TO YOUR SYSTEM PATH TO COMPLETE CONFIGURATION:
+ECHO NOTE: To use quarto please use quarto-cmd (located in this folder) or add the following path to your PATH
 ECHO %binPath%
