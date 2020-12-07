8f7f272b525753814a8abd00f9aa6677e2dd6ced
Charles Teague <cteague@gmail.com>
2020-12-07 14:12:57 -0500

    Run quarto reload from within bin

--- configure-linux.sh
+++ configure-linux.sh
@@ -24,12 +24,13 @@ cp $PANDOCDIR/bin/pandoc .
 rm -rf $PANDOCDIR
 rm $PANDOCFILE
 
-popd
-
 # Run the quarto command with 'reload', which will force the import_map dependencies
 # to be reloaded
 export QUARTO_DENO_EXTRA_OPTIONS="--reload"
 quarto --version
+
+popd
+
 
 
 
