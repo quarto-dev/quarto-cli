7c9c3111df8d316714a5015ac35d16d1c45c90ac
JJ Allaire <jj@rstudio.com>
2020-12-07 16:36:23 -0500

    more work on docx

--- src/resources/filters/figures/docx.lua
+++ src/resources/filters/figures/docx.lua
@@ -7,37 +7,50 @@ function docxPanel(divEl, subfigures)
   local layoutCols = 20
   local pageWidth = 12240 - 1440 - 1440
   
+   -- alignment
+  local align = docxAlign(attribute(divEl, "fig-align", "default"))
+  
   -- table and functions to append to it
   local docxTable = pandoc.Div({})
-  function addBlock(openxml) 
-    docxTable.content:insert(pandoc.RawBlock("openxml", openxml))
-  end
-  function addInline(para, openxml)
-    para.content:insert(pandoc.RawInline("openxml", openxml))
-  end
   
   -- begin table  
-  addBlock("<w:tbl>\n")
+  addRawBlock(docxTable, "<w:tbl>\n")
   
   -- table props
   local tblProps = pandoc.Para({})
-  addInline(tblProps, "<w:tblPr>\n<w:tblStyle w:val=\"TableGrid\"/>\n")
-  addInline(tblProps, "<w:tblW w:w=\"" .. tostring(pageWidth) .. "\" w:type=\"dcx\"/>\n")
-  addInline(tblProps, "<w:tblLayout w:type=\"fixed\"/>\n</w:tblPr>\n")
+  addRawInline(tblProps, "<w:tblPr>\n<w:tblStyle w:val=\"TableGrid\"/>\n")
+  addRawInline(tblProps, "<w:tblW w:w=\"" .. tostring(pageWidth) .. "\" w:type=\"dcx\"/>\n")
+  addRawInline(tblProps, "<w:tblLayout w:type=\"fixed\"/>\n</w:tblPr>\n")
   docxTable.content:insert(tblProps)
   
   -- table grid
   local layoutColWidth = math.floor(pageWidth / layoutCols)
   local tblGrid = pandoc.Para({})
-  addInline(tblGrid, "<w:tblGrid>\n")
+  addRawInline(tblGrid, "<w:tblGrid>\n")
   for i=1,layoutCols do
-    addInline(tblGrid, "</w:gridCol w:w=\"" .. tostring(layoutColWidth) .. "\"/>\n")
+    addRawInline(tblGrid, "</w:gridCol w:w=\"" .. tostring(layoutColWidth) .. "\"/>\n")
   end
-  addInline(tblGrid, "</w:tblGrid>\n")
+  addRawInline(tblGrid, "</w:tblGrid>\n")
   docxTable.content:insert(tblGrid)
+    
+  for i, row in ipairs(subfigures) do
+    addRawBlock(docxTable, "<w:tr>")
+    for _, subfigure in ipairs(row) do
+      docxTable.content:insert(docxSubfigureCell(subfigure, align, pageWidth, layoutCols))
+    end
+    addRawBlock(docxTable, "</w:tr>")
+  end
 
   -- end table
-  addBlock("</w:tbl>\n")
+  addRawBlock(docxTable, "</w:tbl>\n")
+  
+  -- write table caption 
+  local caption = figureDivCaption(divEl)
+  if caption and #caption.content > 0 then
+    local captionPara = pandoc.Para({})
+    writeCaption(captionPara, caption.content, align)
+    docxTable.content:insert(captionPara)
+  end
   
   -- return table
   return docxTable
@@ -45,61 +58,101 @@ function docxPanel(divEl, subfigures)
 end
 
 
+function docxAlign(align)
+  if align == "left" then
+    return "start"
+  elseif align == "center" then
+    return "center"
+  elseif align == "right" then
+    return "end"
+  else
+    return nil
+  end
+end
 
---[[
+function docxSubfigureCell(subfigure, align, pageWidth, layoutCols)
+   
+   -- cell and function for adding to it
+   local cell = pandoc.Para({})
+   
+   -- compute width and span
+   
+   -- TODO: this should never return nil for docx (and for latex?)
+   local layoutPercent = horizontalLayoutPercent(subfigure)
+   if layoutPercent then
+     subfigure.attr.attributes["width"] = nil
+   else
+     layoutPercent = 25
+   end
+   local width = math.floor((layoutPercent/100)*pageWidth)
+   local layoutCols = math.floor((layoutPercent/100)*layoutCols)
+   
+   local cellPrefix = [[
+<w:tc>
+<w:tcPr>   
+]] .. 
+      "<w:tcW w:w=\"" .. tostring(width) .. "\" w:type=\"dcx\"/>\n" ..
+      "<w:gridSpan w:val=\"" .. tostring(layoutCols) .. "\"/>\n" .. [[
+</w:tcPr>
+<w:p>
+]]
 
-  -- create panel
-  local panel = pandoc.Div({})
-  
-  
-  
-  
-  
-  -- alignment
-  local align = tableAlign(attribute(divEl, "fig-align", "default"))
-  
-  -- subfigures
-  local subfiguresEl = pandoc.Para({})
-  for i, row in ipairs(subfigures) do
-    
-    local aligns = row:map(function() return align end)
-    local widths = row:map(function() return 0 end)
-     
-    local figuresRow = pandoc.List:new()
-    for _, image in ipairs(row) do
-      local cell = pandoc.List:new()
-      if image.t == "Image" then
-        cell:insert(pandoc.Para(image))
-      else
-        cell:insert(image)
-      end
-      figuresRow:insert(cell)
-    end
-    
-    -- make the table
-    local figuresTable = pandoc.SimpleTable(
-      pandoc.List:new(), -- caption
-      aligns,
-      widths,
-      pandoc.List:new(), -- headers
-      { figuresRow }
-    )
-    
-    -- add it to the panel
-    panel.content:insert(pandoc.utils.from_simple_table(figuresTable))
+  if align then
+    cellPrefix = cellPrefix .. "<w:pPr>" .. "<w:jc w:val=\"" .. align .. "\"/></w:pPr>\n"
   end
   
-  -- insert caption
-  local divCaption = figureDivCaption(divEl)
-  if divCaption and #divCaption.content > 0 then
-    panel.content:insert(divCaption)
+  addRawInline(cell, cellPrefix)
+  
+  --  write the figure (behavior varies based on whether this is an img or div
+  local captionInlines 
+  if subfigure.t == "Image" then
+    -- get caption and clear it
+    local cellFig = subfigure:clone()
+    captionInlines = cellFig.caption:clone()
+    tclear(cellFig.caption)
+    -- insert image
+    cell.content:insert(cellFig)
+  else
+    -- get caption 
+    captionInlines = figureDivCaption(subfigure).content
+    -- write blocks
+    tappend(cell.content, pandoc.utils.blocks_to_inlines(
+      tslice(subfigure.content, 1, #subfigure.content-1),
+      { pandoc.LineBreak() }
+    ))
   end
   
-  -- return panel
-  return panel
+  -- write end paragraph
+  addRawInline(cell,"</w:p>\n")
+  
+  -- write the caption paragraph if we have a caption
+  if #captionInlines > 0 then
+    writeCaption(cell, captionInlines, align)
+  end
+  
+  -- write end table cell
+  addRawInline(cell, "</w:tc>\n")
+  
+  -- return the cell
+  return cell
+end
 
+function addRawBlock(div, openxml) 
+  div.content:insert(pandoc.RawBlock("openxml", openxml))
+end
 
---]]
+function addRawInline(el, openxml)
+  el.content:insert(pandoc.RawInline("openxml", openxml))
+end 
 
-
-
+function writeCaption(para, captionInlines, align)
+  addRawInline(para, "<w:p>\n")
+  addRawInline(para, "<w:pPr>\n")
+  if align then
+    addRawInline(para, "<w:jc w:val=\"" .. align .. "\"/>\n")
+  end
+  addRawInline(para, "<w:pStyle w:val=\"ImageCaption\" />\n")
+  addRawInline(para, "</w:pPr>\n")
+  tappend(para.content, captionInlines)
+  addRawInline(para, "/w:p>\n")
+end
