284951ad3dd549ca547c86251276a1d1af8feda8
Charles Teague <cteague@gmail.com>
2021-01-08 14:57:17 -0500

    include version in msi name

--- package/src/windows/installer.ts
+++ package/src/windows/installer.ts
@@ -18,7 +18,7 @@ export async function makeInstallerWindows(configuration: Configuration) {
 
     const heatCmd = join(wixDir, "heat");
     const candleCmd = join(wixDir, "candle");
-    const lightCmd = join(wixDir, "light");    
+    const lightCmd = join(wixDir, "light");
 
     // Download tools, if necessary
     if (!existsSync(workingDir) || !existsSync(wixDir) || !existsSync(heatCmd + ".exe")) {
@@ -46,10 +46,10 @@ export async function makeInstallerWindows(configuration: Configuration) {
 
     // heat the directory to generate a wix file for it 
     const heatOutput = join(workingDir, "quarto-frag.wxs");
-    await runCmd(heatCmd, ["dir", configuration.dirs.dist, "-var",  "var.SourceDir", "-gg", "-sfrag", "-srd", "-cg", "ProductComponents", "-dr", "INSTALLFOLDER", "-out", heatOutput], configuration.log);
+    await runCmd(heatCmd, ["dir", configuration.dirs.dist, "-var", "var.SourceDir", "-gg", "-sfrag", "-srd", "-cg", "ProductComponents", "-dr", "INSTALLFOLDER", "-out", heatOutput], configuration.log);
 
     // TODO: Process the version and other metadata into the WXS file
-     // use candle to build the wixobj file
+    // use candle to build the wixobj file
     const candleFiles = [join(configuration.dirs.pkg, "src", "windows", "quarto.wxs"), heatOutput]
     const candleOutput: string[] = []
     await Promise.all(candleFiles.map(async candleInput => {
@@ -59,12 +59,12 @@ export async function makeInstallerWindows(configuration: Configuration) {
         return runCmd(candleCmd, [`-dSourceDir=${configuration.dirs.dist}`, "-out", outputPath, candleInput], configuration.log);
     }));
 
-    
-    const lightOutput = join(workingDir, "quarto.msi");
+
+    const lightOutput = join(workingDir, `quarto-${configuration.version}.msi`);
     await runCmd(lightCmd, ["-out", lightOutput, ...candleOutput], configuration.log);
-    
+
     configuration.log.info(`Moving ${lightOutput} to ${configuration.dirs.out}`);
-    moveSync(lightOutput, join(configuration.dirs.out, basename(lightOutput)), {overwrite: true});
+    moveSync(lightOutput, join(configuration.dirs.out, basename(lightOutput)), { overwrite: true });
 
     // Clean up the working directory
     Deno.remove(workingDir);
