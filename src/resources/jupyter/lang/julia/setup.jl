6300b4daf9a705fda934cd5d57a46fc629620322
JJ Allaire <jj@rstudio.com>
2021-01-12 11:26:26 -0500

    julia setup/cleanup cells

---
+++ src/resources/jupyter/lang/julia/setup.jl
@@ -0,0 +1,16 @@
+import JSON
+import IJulia
+
+# The julia kernel has built in support for Revise.jl, so this is the 
+# recommended approach for long-running sessions:
+# https://github.com/JuliaLang/IJulia.jl/blob/9b10fa9b879574bbf720f5285029e07758e50a5e/src/kernel.jl#L46-L51
+
+# Users should enable revise within .julia/config/startup_ijulia.jl:
+# https://timholy.github.io/Revise.jl/stable/config/#Using-Revise-automatically-within-Jupyter/IJulia-1
+
+# clear console history
+IJulia.clear_history()
+
+# return kernel dependencies (always empty b/c Revise should take care of dependencies)
+kernel_deps = Dict()
+println(JSON.json(kernel_deps))
