6d62393980c99f9f35a990e4d7ba00be694f82e9
Charles Teague <cteague@gmail.com>
2021-01-13 13:19:56 -0500

    notarization + secure timestamp

--- package/src/macos/installer.ts
+++ package/src/macos/installer.ts
@@ -10,6 +10,7 @@ import { existsSync, ensureDirSync } from "fs/mod.ts";
 import { Configuration } from "../common/config.ts";
 import { Logger } from "../util/logger.ts";
 import { runCmd } from "../util/cmd.ts";
+import { getEnv } from "../util/utils.ts";
 
 // Packaging specific configuration
 // (Some things are global others may be platform specific)
@@ -64,10 +65,19 @@ export async function makeInstallerMac(config: Configuration) {
 
   config.log.info("Signing file");
   config.log.info(unsignedPackagePath);
-  await signInstaller(unsignedPackagePath, join(config.directoryInfo.out, packageName), config.log);
+  const signedPackage = join(config.directoryInfo.out, packageName);
+  await signInstaller(unsignedPackagePath, signedPackage, config.log);
 
   config.log.info("Cleaning unsigned file");
   Deno.removeSync(unsignedPackagePath);
+
+  // Submit package for notary
+  const username = getEnv("QUARTO_APPLE_CONNECT_UN");
+  const password = getEnv("QUARTO_APPLE_CONNECT_PW");
+  const requestId = await submitNotary(signedPackage, "org.quarto.cli", username, password, config.log);
+
+  // This will succeed or throw
+  await waitForNotaryStatus(requestId, username, password, config.log);
 }
 
 const installerCertificate = "Developer ID Installer";
@@ -85,8 +95,60 @@ async function signFile(input: string, output: string, certificate: string, log:
     "productsign",
     ["--sign",
       certificate,
+      "--timestamp",
       input,
       output],
     log
   );
+}
+
+async function submitNotary(input: string, bundleId: string, username: string, password: string, log: Logger) {
+  const result = await runCmd(
+    "xcrun",
+    ["altool",
+      "--notarize-app",
+      "--primary-bundle-id", bundleId,
+      "--username", username,
+      "--password", password,
+      "--file", input
+    ],
+    log
+  )
+  const match = result.stdout.match(/RequestUUID = (.*)/);
+  if (match) {
+    const requestId = match[1];
+    return requestId;
+  } else {
+    throw new Error("Unable to start notarization " + result.stdout);
+  }
+}
+
+async function waitForNotaryStatus(requestId: string, username: string, password: string, log: Logger) {
+  let notaryResult = undefined;
+  while (notaryResult == undefined) {
+    const result = await runCmd(
+      "xcrun",
+      ["altool",
+        "--notarization-info", requestId,
+        "--username", username,
+        "--password", password,
+      ],
+      log
+    );
+
+    const match = result.stdout.match(/Status: (.*)\n/);
+    if (match) {
+      const status = match[1];
+      if (status === "in progress") {
+        // Sleep for 15 seconds between checks
+        await new Promise((resolve) => setTimeout(resolve, 15 * 1000));
+      } else if (status === "success") {
+        notaryResult = "Success";
+      } else {
+        log.error(result.stderr);
+        throw new Error("Failed to Notarize - " + status);
+      }
+    }
+  }
+  return notaryResult;
 }
