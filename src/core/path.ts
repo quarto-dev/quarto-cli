e4b4232ab344eb3905657bf37f6a4f7b8705727a
Charles Teague <cteague@gmail.com>
2021-01-26 16:01:58 -0500

    platform independent 'which' command

--- src/core/path.ts
+++ src/core/path.ts
@@ -9,6 +9,7 @@ import { basename, dirname, extname } from "path/mod.ts";
 
 import { existsSync } from "fs/exists.ts";
 import { getenv } from "./env.ts";
+import { execProcess } from "./process.ts";
 
 export function removeIfExists(file: string) {
   if (existsSync(file)) {
@@ -25,4 +26,18 @@ export function dirAndStem(file: string) {
 
 export function expandPath(path: string) {
   return path.replace(/^~\//, getenv("HOME", "~") + "/");
+}
+
+export async function which(cmd: string) {
+  const args = Deno.build.os === "windows"
+    ? ["CMD", "/C", "where", cmd]
+    : ["which", cmd];
+  const result = await execProcess(
+    { cmd: args, stderr: "piped", stdout: "piped" },
+  );
+  if (result.code === 0) {
+    return result.stdout?.trim();
+  } else {
+    return undefined;
+  }
 }
