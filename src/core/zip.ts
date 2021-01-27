ceda27df37fe263d8cc6ad8f0564a95542b304b2
Charles Teague <cteague@gmail.com>
2021-01-27 15:03:19 -0500

    unzip util

---
+++ src/core/zip.ts
@@ -0,0 +1,35 @@
+/*
+* zip.ts
+*
+* Copyright (C) 2020 by RStudio, PBC
+*
+*/
+import { dirname } from "path/mod.ts";
+import { execProcess } from "./process.ts";
+
+export async function unzip(file: string) {
+  const dir = dirname(file);
+
+  if (file.endsWith("zip")) {
+    // It's a zip file
+    if (Deno.build.os === "windows") {
+      return execProcess(
+        {
+          cmd: ["powershell", "Expand-Archive", file],
+          cwd: dir,
+          stdout: "piped",
+        },
+      );
+    } else {
+      // Use the built in unzip command
+      return execProcess(
+        { cmd: ["unzip", "-o", file], cwd: dir, stdout: "piped" },
+      );
+    }
+  } else {
+    // use the tar command to untar this
+    return execProcess(
+      { cmd: ["tar", "-x", "-f", file], cwd: dir, stdout: "piped" },
+    );
+  }
+}
