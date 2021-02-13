0804799e1e9f6e80314d7dad23aa6543325aa7b6
JJ Allaire <jj@rstudio.com>
2021-02-13 07:24:49 -0500

    make images responsive

--- src/resources/filters/formats/html.lua
+++ src/resources/filters/formats/html.lua
@@ -1,15 +1,14 @@
--- html-pre.lua
+-- html.lua
 -- Copyright (C) 2021 by RStudio, PBC
 
 -- required version
 PANDOC_VERSION:must_be_at_least '2.11.2'
 
--- required modules
-text = require 'text'
 
-return {
-
-
-
-
-}
+-- make images responsive (unless they have an explicit height attribute)
+Image = function(image)
+  if not image.attr.attributes["height"] then
+    image.attr.classes:insert("img-fluid")
+    return image
+  end
+end
