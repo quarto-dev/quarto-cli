96a8f69bea11c9e297b1d60c0580f5a08993e742
JJ Allaire <jj@rstudio.com>
2021-03-07 06:55:32 -0500

    don't capture yaml within code blocks and html comments

--- src/core/yaml.ts
+++ src/core/yaml.ts
@@ -12,6 +12,9 @@ import { parse } from "encoding/yaml.ts";
 const kRegExYAML =
   /(^)(---[ \t]*[\r\n]+(?![ \t]*[\r\n]+)[\W\w]*?[\r\n]+(?:---|\.\.\.))([ \t]*)$/gm;
 
+const kRegxHTMLComment = /<!--[\W\w]*?-->/gm;
+const kRegexFencedCode = /^([\t >]*`{3,})[^`\n]*\n[\W\w]*?\n\1\s*$/gm;
+
 export function readYaml(file: string) {
   if (existsSync(file)) {
     const decoder = new TextDecoder("utf-8");
@@ -30,6 +33,10 @@ export function readYamlFromMarkdown(
   markdown: string,
 ): { [key: string]: unknown } {
   if (markdown) {
+    // remove html comments and fenced code regions
+    markdown = markdown.replaceAll(kRegxHTMLComment, "");
+    markdown = markdown.replaceAll(kRegexFencedCode, "");
+
     // capture all yaml blocks as a single yaml doc
     let yaml = "";
     kRegExYAML.lastIndex = 0;
