7913387207d10b85d33efe02df71de84bade57f3
Charles Teague <cteague@gmail.com>
2021-03-08 20:04:51 -0500

    prettier error messages on windows

--- src/core/log.ts
+++ src/core/log.ts
@@ -9,7 +9,7 @@ import { getenv } from "./env.ts";
 import * as colors from "fmt/colors.ts";
 
 export function logError(error: Error) {
-  const isDebug = getenv("QUARTO_DEBUG", "0");
+  const isDebug = getenv("QUARTO_DEBUG", "false") === "true";
   const errorMessage = isDebug
     ? error.stack || error.message
     : `${error.name}: ${error.message}`;
