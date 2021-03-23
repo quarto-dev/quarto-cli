9bea6c94290cd2f49f8ce48b59f33d5caa7cbaa3
JJ Allaire <jj@rstudio.com>
2021-03-23 18:16:35 -0400

    functions for notice and tabset

--- src/resources/formats/html/html.lua
+++ src/resources/formats/html/html.lua
@@ -4,8 +4,6 @@
 -- required version
 PANDOC_VERSION:must_be_at_least '2.13'
 
-
-
 -- make images responsive (unless they have an explicit height attribute)
 Image = function(image)
   if not image.attr.attributes["height"] then
@@ -14,43 +12,52 @@ Image = function(image)
   end
 end
 
+-- tabsets and notices
 Div = function(div)
-  -- look for divs with a class admonition-*
   if div.attr.classes:find("admonition") then
-
-    -- capture type information
-   local type = div.attr.attributes["type"] 
-    if type == nil then
-      type = "info"
-    end
-
-    -- capture caption information
-    local caption = div.attr.attributes["caption"]
-    div.attr.attributes["caption"] = nil
-
-    -- Make an outer card div and transfer classes
-    local cardDiv = pandoc.Div({})
-    cardDiv.attr.classes = div.attr.classes:clone()
-    div.attr.classes = pandoc.List:new() 
-
-    -- add card attributes
-    cardDiv.attr.classes:insert("card")
-    
-    -- create a card header
-    if caption ~= nil then
-      local cardHeaderDiv = pandoc.Div({})
-      cardHeaderDiv.attr.classes:insert("card-header")
-      cardHeaderDiv.content:insert(pandoc.Plain(type))
-      cardDiv.content:insert(cardHeaderDiv)
-    end
-
-    -- create a card body
-    div.attr.classes:insert("card-body")
-    cardDiv.content:insert(div)
-    
-    return cardDiv
+    return noticeDiv(div)
+  elseif div.attr.classes:find("tabset") then
+    return tabsetDiv(div)
   end  
 end
 
 
+function noticeDiv(div)
+  -- capture type information
+  local type = div.attr.attributes["type"] 
+  if type == nil then
+    type = "info"
+  end
 
+  -- capture caption information
+  local caption = div.attr.attributes["caption"]
+  div.attr.attributes["caption"] = nil
+
+  -- Make an outer card div and transfer classes
+  local cardDiv = pandoc.Div({})
+  cardDiv.attr.classes = div.attr.classes:clone()
+  div.attr.classes = pandoc.List:new() 
+
+  -- add card attributes
+  cardDiv.attr.classes:insert("card")
+  
+  -- create a card header
+  if caption ~= nil then
+    local cardHeaderDiv = pandoc.Div({})
+    cardHeaderDiv.attr.classes:insert("card-header")
+    cardHeaderDiv.content:insert(pandoc.Plain(type))
+    cardDiv.content:insert(cardHeaderDiv)
+  end
+
+  -- create a card body
+  div.attr.classes:insert("card-body")
+  cardDiv.content:insert(div)
+  
+  return cardDiv
+end
+
+
+function tabsetDiv(div)
+  print('tabset')
+  return div
+end
