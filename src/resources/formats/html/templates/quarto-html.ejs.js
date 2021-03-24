825266d9a394e159dba362447e011fd074f28934
JJ Allaire <jj@rstudio.com>
2021-03-24 15:43:49 -0400

    copy code to clipboard

--- src/resources/formats/html/templates/quarto-html.ejs.js
+++ src/resources/formats/html/templates/quarto-html.ejs.js
@@ -12,6 +12,30 @@ window.document.addEventListener("DOMContentLoaded", function (event) {
   };
   anchorJS.add();
   <% } %>
- 
 
+  <% if (copyCode) { %>
+
+  const clipboard = new window.ClipboardJS('.code-copy-button', {
+    target: function(trigger) {
+      return trigger.previousElementSibling;
+    }
+  });
+
+  clipboard.on('success', function(e) {
+    // button target
+    const button = e.trigger;
+    // don't keep focus
+    button.blur();
+    // flash "checked"
+    button.classList.add('code-copy-button-checked');
+    setTimeout(function() {
+      button.classList.remove('code-copy-button-checked');
+    }, 1000);
+    // clear code selection
+    e.clearSelection();
+  });
+
+  <% } %>
+ 
+ // https://icons.getbootstrap.com/assets/js/application.min.js
 });
