7c6b54a14ca9b5dbf0392ba209b4fa84a300f395
JJ Allaire <jj@rstudio.com>
2021-03-30 08:17:55 -0400

    no need to init website navigation on startup

--- src/project/types/website/website-serve.ts
+++ src/project/types/website/website-serve.ts
@@ -1,25 +1,10 @@
 /*
-* website-server.ts
+* website-serve.ts
 *
 * Copyright (C) 2020 by RStudio, PBC
 *
 */
 
-import { ProjectContext } from "../../project-context.ts";
 import { ProjectServe } from "../project-types.ts";
-import { initWebsiteNavigation } from "./website-navigation.ts";
 
-export const websiteServe: ProjectServe = {
-  init: async (project: ProjectContext) => {
-    await initWebsiteNavigation(project);
-  },
-};
-
-// things we need to know to implement hot-reload:
-//
-//
-//   - the last modified time of _quarto.yml (and any file referenced from it, including recursively)
-//     (modulo the inputFiles -- consider include or exclude list
-//
-//
-//
+export const websiteServe: ProjectServe = {};
