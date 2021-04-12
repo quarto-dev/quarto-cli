a387a75ac98c4de65e4951d23ba799e8766a9d2c
Charles Teague <cteague@gmail.com>
2021-04-12 17:00:28 -0400

    Log options are global, but appear in command help (at the bottom of the list)

--- src/quarto.ts
+++ src/quarto.ts
@@ -15,58 +15,29 @@ import {
 
 import { commands } from "./command/command.ts";
 import {
+  appendLogOptions,
   cleanupLogger,
   initializeLogger,
   logError,
-  LogOptions,
+  logOptions,
 } from "./core/log.ts";
 import { cleanupSessionTempDir, initSessionTempDir } from "./core/temp.ts";
 import { quartoConfig } from "./core/quarto.ts";
 import { Args, parse } from "flags/mod.ts";
 
-export async function quarto(args: string[]) {
+export function quarto(args: string[]) {
   const quartoCommand = new Command()
     .name("quarto")
     .version(quartoConfig.version())
     .description("Quarto CLI")
-    .option(
-      "-l, --log <file>",
-      "Log to this file",
-      {
-        global: true,
-      },
-    )
-    .option(
-      "-ll, --log-level <level>",
-      "Log level (info, warning, error, critical)",
-      {
-        global: true,
-      },
-    )
-    .option(
-      "-lf, --log-format <level>",
-      "Log format (plain, json-stream)",
-      {
-        global: true,
-      },
-    )
-    .option(
-      "-q, --quiet",
-      "Suppress console output.",
-      {
-        global: true,
-      },
-    )
     .throwErrors();
 
   commands().forEach((command) => {
-    quartoCommand.command(command.getName(), command);
+    quartoCommand.command(command.getName(), appendLogOptions(command));
   });
 
-  await quartoCommand
-    .command("help", new HelpCommand().global())
-    .command("completions", new CompletionsCommand()).hidden()
-    .parse(args);
+  quartoCommand.command("help", new HelpCommand().global())
+    .command("completions", new CompletionsCommand()).hidden().parse(args);
 }
 
 if (import.meta.main) {
@@ -97,28 +68,4 @@ function cleanup() {
   cleanupSessionTempDir();
   cleanupLogger();
   Deno.exit(1);
-}
-
-function logOptions(args: Args) {
-  const logOptions: LogOptions = {};
-  logOptions.log = args.l || args.log;
-  logOptions.level = args.ll || args["log-level"];
-  logOptions.quiet = args.q || args.quiet;
-  logOptions.format = parseFormat(args.lf || args["log-format"]);
-  return logOptions;
-}
-
-function parseFormat(format?: string) {
-  if (format) {
-    format = format.toLowerCase();
-    switch (format) {
-      case "plain":
-      case "json-stream":
-        return format;
-      default:
-        return "plain";
-    }
-  } else {
-    return "plain";
-  }
 }
