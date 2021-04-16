5822184d0dff65b4adb75060a171c539e7a46a57
Charles Teague <cteague@gmail.com>
2021-04-16 08:35:20 -0400

    clean up lint

--- package/src/common/configure.ts
+++ package/src/common/configure.ts
@@ -65,7 +65,7 @@ export async function configure(
         join(config.directoryInfo.bin, "quarto"),
         symlinkPath,
       );
-    } catch () {
+    } catch {
       warning("Failed to create symlink to quarto.");
     }
   }
