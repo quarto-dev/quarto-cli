8e89fd85cca7eb92f46227b04a32fa0c449a1826
Charles Teague <cteague@gmail.com>
2021-05-14 09:00:27 -0400

    Add displayErrorAndExit to improve error messages when exiting book render

---
+++ src/resources/filters/common/error.lua
@@ -0,0 +1,35 @@
+-- debug.lua
+-- Copyright (C) 2020 by RStudio, PBC
+
+
+
+
+
+function displayErrorAndExit(message)
+  local file = currentFile()
+  if file then
+    print("An error occurred while processing '" .. file .. "'")
+  else
+    print("An error occurred")
+  end
+  print(message)
+  os.exit(1)
+end
+
+
+function currentFile() 
+  
+  if currentFileMetadataState ~= nil then
+    -- if we're in a multifile contatenated render, return which file we're rendering
+    local fileState = currentFileMetadataState()
+    if fileState ~= nil and fileState.file ~= nil and fileState.file.bookItemFile ~= nil then
+      return fileState.file.bookItemFile
+    else
+      return nil
+    end
+  else
+    -- if we're not in a concatenated scenario, file name doesn't really matter since the invocation is only
+    -- targeting a single file
+    return nil
+  end
+end
