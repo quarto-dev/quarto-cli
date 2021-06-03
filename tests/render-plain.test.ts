a74b4c39be0159a429dabf5fad2bf9a52943fd61
Charles Teague <cteague@gmail.com>
2021-06-03 10:04:41 -0400

    Refactoring of Tests
    -> Extract core test logic with setup and teardown
    -> Create render test wrapper
    -> Convert existing tests to use new tests
    -> Verify that all are passing and leaving directories clean

--- tests/render-plain.test.ts
+++ tests/render-plain.test.ts
@@ -1,5 +1,13 @@
+/*
+* render-plain.test.ts
+*
+* Copyright (C) 2020 by RStudio, PBC
+*
+*/
 import { testRender } from "./render.ts";
+import { hasSupportingFiles, outputCreated } from "./verify.ts";
 
-Deno.test("render: plain md", async () => {
-  await testRender("docs/test-plain.md", true);
-});
+testRender("docs/test-plain.md", "html", [
+  outputCreated,
+  hasSupportingFiles,
+]);
---
+++ tests/utils.ts
@@ -0,0 +1,24 @@
+/*
+* utils.ts
+*
+* Copyright (C) 2020 by RStudio, PBC
+*
+*/
+
+import { basename, dirname, extname, join } from "path/mod.ts";
+
+// Gets output that should be created for this input file and target format
+export function outputForInput(input: string, to: string) {
+  const dir = dirname(input);
+  const stem = basename(input, extname(input));
+
+  const outputExt = to || "html";
+
+  const outputPath = join(dir, `${stem}.${outputExt}`);
+  const supportPath = join(dir, `${stem}_files`);
+
+  return {
+    outputPath,
+    supportPath,
+  };
+}
