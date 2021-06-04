c726f3248cc88f35456a3a8ffff67c6f1ee6df36
Charles Teague <cteague@gmail.com>
2021-06-04 17:40:41 -0400

    cleanup env test

--- tests/env/environment.test.ts
+++ tests/env/environment.test.ts
@@ -8,7 +8,7 @@ import { assert, assertEquals } from "testing/asserts.ts";
 import { getenv } from "../../src/core/env.ts";
 import { binaryPath, resourcePath } from "../../src/core/resources.ts";
 
-Deno.test("environment: resource path available", () => {
+Deno.test("> environment: resource path available", () => {
   const path = resourcePath("metadata.template");
   assert(
     path.length > 0,
@@ -16,7 +16,7 @@ Deno.test("environment: resource path available", () => {
   );
 });
 
-Deno.test("environment: default getenv behavior ", () => {
+Deno.test("> environment: default getenv behavior ", () => {
   const d = "B4CE1323-5ED7-43A8-AEF0-4409F42CF3C8";
   const path = getenv("D684CD56-D4DF-476D-ACB7-7E5D404BC743", d);
   assertEquals(
@@ -26,7 +26,7 @@ Deno.test("environment: default getenv behavior ", () => {
   );
 });
 
-Deno.test("environment: binary path available", () => {
+Deno.test("> environment: binary path available", () => {
   const path = binaryPath("pandoc");
   assert(
     path.length > 0,
