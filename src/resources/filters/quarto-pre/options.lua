6249dbb1a0ea7a7854c6008ec550438d7fd2547c
Charles Teague <cteague@gmail.com>
2021-06-11 16:27:03 -0400

    Add support for vars

--- src/resources/filters/quarto-pre/options.lua
+++ src/resources/filters/quarto-pre/options.lua
@@ -22,12 +22,22 @@ end
 
 -- get option value
 function option(name, default)
+  return parseOption(name, allOptions, default)
+end
+
+local kVarNamespace = "_quarto-vars"
+function var(name, default)
+  local vars = allOptions[kVarNamespace]
+  return parseOption(name, vars, default)
+end
+
+function parseOption(name, options, default) 
   local keys = split(name, ".")
 
   local value = nil
   for i, key in ipairs(keys) do
     if value == nil then
-      value = readOption(allOptions, key, nil)
+      value = readOption(options, key, nil)
     else
       value = value[key]
 
@@ -39,6 +49,3 @@ function option(name, default)
   end
   return value
 end
-
-
-
