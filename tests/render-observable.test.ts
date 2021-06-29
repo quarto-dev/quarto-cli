a0ec2fcd8680322bfe99caf50a61e1e0c1bfedfc
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2021-06-29 14:55:26 -0700

    import render from correct location

--- tests/render-observable.test.ts
+++ tests/render-observable.test.ts
@@ -4,14 +4,6 @@
 * Copyright (C) 2020 by RStudio, PBC
 *
 */
-import { testRender } from "./render.ts";
-import {
-  hasSupportingFiles,
-  noSupportingFiles,
-  outputCreated,
-} from "./verify.ts";
+import { testRender } from "./smoke/render/render.ts";
 
-testRender("docs/test-observable.md", "html", [
-  outputCreated,
-  hasSupportingFiles,
-]);
+testRender("docs/test-observable.md", "html", false);
