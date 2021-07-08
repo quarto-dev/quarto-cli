75c78c7f16ce983838f00c736f8e20ca817ac1a9
JJ Allaire <jj@rstudio.com>
2021-07-08 12:54:59 -0400

    improve inspect cli docs

--- src/command/inspect/cmd.ts
+++ src/command/inspect/cmd.ts
@@ -28,9 +28,9 @@ export const inspectCommand = new Command()
   .name("inspect")
   .arguments("[path:string]")
   .description(
-    "Inspect a Quarto project or input path. Inspecting a project returns its config and " +
-      "engines; inspecting an input path return its formats, engine, and dependent resources.\n\n" +
-      "Emits results of inspection as JSON to stdout",
+    "Inspect a Quarto project or input path. Inspecting a project returns its config and engines.\n" +
+      "Inspecting an input path return its formats, engine, and dependent resources.\n\n" +
+      "Emits results of inspection as JSON to stdout.",
   )
   .example(
     "Inspect project in current directory",
