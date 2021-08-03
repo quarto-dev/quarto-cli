6ab64c8165700a6c3daf62df1885c1232fe04a88
Charles Teague <cteague@gmail.com>
2021-08-03 15:07:44 -0400

    Only run for single file books

--- src/resources/filters/quarto-pre/book-links.lua
+++ src/resources/filters/quarto-pre/book-links.lua
@@ -4,56 +4,63 @@
 local path = require 'pandoc.path'
 
 function indexBookFileTargets() 
-    return {
-      Header = function(el)
-      local file = currentFileMetadataState().file
-      if file ~= nil and (param("single-file-book", false)) then
+    if not param("single-file-book", false) then
+      return {} 
+    else 
+      return {
+        Header = function(el)
         if el.level == 1 then 
-          local filename = file.bookItemFile;
-          if filename ~= nil and preState.fileSectionIds[filename] == nil then
-            preState.fileSectionIds[filename] = el.identifier
+          local file = currentFileMetadataState().file
+          if file ~= nil then   
+            local filename = file.bookItemFile;
+            if filename ~= nil and preState.fileSectionIds[filename] == nil then
+              preState.fileSectionIds[filename] = el.identifier
+            end
           end
         end
       end
-    end
-  }
+    }
+  end
 end
 
 function resolveBookFileTargets() 
-  return {
-    Link = function(el)
-      local linkTarget = el.target
-      -- if this is a local path
-      if isRelativeRef(linkTarget) then
-        local file = currentFileMetadataState().file
-
-        -- normalize the linkTarget (collapsing any '..')
-        if linkTarget ~= nil then
-          local fullPath = linkTarget
-          if file ~= nil and file.resourceDir ~= nil then
-            fullPath = path.join({file.resourceDir, linkTarget})
+  if not param("single-file-book", false) then
+    return {} 
+  else 
+    return {
+      Link = function(el)
+        local linkTarget = el.target
+        -- if this is a local path
+        if isRelativeRef(linkTarget) then
+          local file = currentFileMetadataState().file
+  
+          -- normalize the linkTarget (collapsing any '..')
+          if linkTarget ~= nil then
+            local fullPath = linkTarget
+            if file ~= nil and file.resourceDir ~= nil then
+              fullPath = path.join({file.resourceDir, linkTarget})
+            end
+            linkTarget = path.normalize(flatten(fullPath));
           end
-          linkTarget = path.normalize(flatten(fullPath));
-        end
-        
-        -- resolve the path
-        local hashPos = string.find(linkTarget, '#')
-        if hashPos ~= nil then
-          -- deal with a link that includes a hash (just remove the prefix)
-          local target = string.sub(linkTarget, hashPos, #linkTarget)
-          el.target = target
-        else
-          -- Deal with bare file links
-          local sectionId = preState.fileSectionIds[linkTarget];
-          if sectionId ~= nil then
-            el.target = '#' .. sectionId
+          
+          -- resolve the path
+          local hashPos = string.find(linkTarget, '#')
+          if hashPos ~= nil then
+            -- deal with a link that includes a hash (just remove the prefix)
+            local target = string.sub(linkTarget, hashPos, #linkTarget)
+            el.target = target
+          else
+            -- Deal with bare file links
+            local sectionId = preState.fileSectionIds[linkTarget];
+            if sectionId ~= nil then
+              el.target = '#' .. sectionId
+            end
           end
         end
-      end
-      return el
-    end
-    
-  }
+        return el
+      end 
+    }  
+  end
 end
 
 function flatten(targetPath) 
