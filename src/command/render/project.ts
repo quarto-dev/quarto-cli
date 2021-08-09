bc12e51628b0701ab6eeaeeff4cc5dc8b1b8dc78
JJ Allaire <jj@rstudio.com>
2021-08-09 08:53:24 -0400

    don't do incremental copying for lib dir

--- src/command/render/project.ts
+++ src/command/render/project.ts
@@ -236,7 +236,14 @@ export async function renderProject(
           // if this is an incremental render or we are uzing the freezer, then
           // copy lib dirs incrementally (don't replace the whole directory).
           // otherwise, replace the whole thing so we get a clean start
-          const libsIncremental = !!(incremental || options.useFreezer);
+          // NOTE: we are disabling incremental render b/c in spite of the
+          // potential performance gains there are scenarios where a lib_dir
+          // has only partial contents and the incremental approach can
+          // never "fix" the partial contents in the destination
+          // Is there even a meaningful performance win here? We don't know
+          // If there is a performance issue created by this non-incremental
+          // copying we should investigate whethere to just use copyMinimal
+          const libsIncremental = false; // !!(incremental || options.useFreezer);
 
           // determine format lib dirs (for pruning)
           const formatLibDirs = projType.formatLibDirs
