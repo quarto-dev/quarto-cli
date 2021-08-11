a28f9254c81875610c4141d42ccfc6ebfc247848
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2021-08-11 12:01:43 -0700

    remove cell output display check where it's no longer needed

--- src/resources/formats/html/ojs/ojs-bundle.js
+++ src/resources/formats/html/ojs/ojs-bundle.js
@@ -313,9 +313,6 @@ export class OJSConnector {
         // (with ids "ojs-cell-*")
         
         observer.observe(element, config);
-        if (cellOutputDisplay === undefined) {
-          throw new Error("Internal error: Couldn't find output display cell while handling output:!all");
-        }
         
         element.classList.add("ojs-in-a-box-waiting-for-module-import");
 
@@ -918,6 +915,8 @@ export function createRuntime() {
   }
   lib.FileAttachment = () => FileAttachments(fileAttachmentPathResolver);
 
+  lib.require = () => require;
+
   const ojsConnector = new OJSConnector({
     paths: quartoOjsGlobal.paths,
     inspectorClass: isShiny ? ShinyInspector : undefined,
@@ -1069,3 +1068,179 @@ window._ojs = {
   // via DOM
 };
 window._ojs.runtime = createRuntime();
+
+//////////////////////////////////////////////////////////////////////////////
+// this is our patched d3-require
+
+const metas = new Map;
+const queue = [];
+const map = queue.map;
+const some = queue.some;
+const hasOwnProperty = queue.hasOwnProperty;
+const origin = "https://cdn.jsdelivr.net/npm/";
+const identifierRe = /^((?:@[^/@]+\/)?[^/@]+)(?:@([^/]+))?(?:\/(.*))?$/;
+const versionRe = /^\d+\.\d+\.\d+(-[\w-.+]+)?$/;
+const extensionRe = /\.[^/]*$/;
+const mains = ["unpkg", "jsdelivr", "browser", "main"];
+
+export class RequireError extends Error {
+  constructor(message) {
+    super(message);
+  }
+}
+
+RequireError.prototype.name = RequireError.name;
+
+function main(meta) {
+  for (const key of mains) {
+    const value = meta[key];
+    if (typeof value === "string") {
+      return extensionRe.test(value) ? value : `${value}.js`;
+    }
+  }
+}
+
+function parseIdentifier(identifier) {
+  const match = identifierRe.exec(identifier);
+  return match && {
+    name: match[1],
+    version: match[2],
+    path: match[3]
+  };
+}
+
+function resolveMeta(target) {
+  const url = `${origin}${target.name}${target.version ? `@${target.version}` : ""}/package.json`;
+  let meta = metas.get(url);
+  if (!meta) metas.set(url, meta = fetch(url).then(response => {
+    if (!response.ok) throw new RequireError("unable to load package.json");
+    if (response.redirected && !metas.has(response.url)) metas.set(response.url, meta);
+    return response.json();
+  }));
+  return meta;
+}
+
+async function resolve(name, base) {
+  if (name.startsWith(origin)) name = name.substring(origin.length);
+  if (/^(\w+:)|\/\//i.test(name)) return name;
+  if (/^[.]{0,2}\//i.test(name)) return new URL(name, base == null ? location : base).href;
+  if (!name.length || /^[\s._]/.test(name) || /\s$/.test(name)) throw new RequireError("illegal name");
+  const target = parseIdentifier(name);
+  if (!target) return `${origin}${name}`;
+  if (!target.version && base != null && base.startsWith(origin)) {
+    const meta = await resolveMeta(parseIdentifier(base.substring(origin.length)));
+    target.version = meta.dependencies && meta.dependencies[target.name] || meta.peerDependencies && meta.peerDependencies[target.name];
+  }
+  if (target.path && !extensionRe.test(target.path)) target.path += ".js";
+  if (target.path && target.version && versionRe.test(target.version)) return `${origin}${target.name}@${target.version}/${target.path}`;
+  const meta = await resolveMeta(target);
+  return `${origin}${meta.name}@${meta.version}/${target.path || main(meta) || "index.js"}`;
+}
+
+export var require = requireFrom(resolve);
+
+export function requireFrom(resolver) {
+  const cache = new Map;
+  const requireBase = requireRelative(null);
+
+  function requireAbsolute(url) {
+    if (typeof url !== "string") return url;
+    let module = cache.get(url);
+    if (!module) cache.set(url, module = new Promise((resolve, reject) => {
+      const needsSave = define !== window.define;
+      const prevDefine = window.define;
+      const script = document.createElement("script");
+      script.onload = () => {
+        try { resolve(queue.pop()(requireRelative(url))); }
+        catch (error) { reject(new RequireError("invalid module")); }
+        script.remove();
+        if (needsSave) {
+          window.define = prevDefine;
+        }
+      };
+      script.onerror = () => {
+        reject(new RequireError("unable to load module"));
+        script.remove();
+        if (needsSave) {
+          window.define = prevDefine;
+        }
+      };
+      script.async = true;
+      script.src = url;
+      window.define = define;
+      document.head.appendChild(script);
+    }));
+    return module;
+  }
+
+  function requireRelative(base) {
+    return name => Promise.resolve(resolver(name, base)).then(requireAbsolute);
+  }
+
+  function requireAlias(aliases) {
+    return requireFrom((name, base) => {
+      if (name in aliases) {
+        name = aliases[name], base = null;
+        if (typeof name !== "string") return name;
+      }
+      return resolver(name, base);
+    });
+  }
+
+  function require(name) {
+    return arguments.length > 1
+        ? Promise.all(map.call(arguments, requireBase)).then(merge)
+        : requireBase(name);
+  }
+
+  require.alias = requireAlias;
+  require.resolve = resolver;
+
+  return require;
+}
+
+function merge(modules) {
+  const o = {};
+  for (const m of modules) {
+    for (const k in m) {
+      if (hasOwnProperty.call(m, k)) {
+        if (m[k] == null) Object.defineProperty(o, k, {get: getter(m, k)});
+        else o[k] = m[k];
+      }
+    }
+  }
+  return o;
+}
+
+function getter(object, name) {
+  return () => object[name];
+}
+
+function isbuiltin(name) {
+  name = name + "";
+  return name === "exports" || name === "module";
+}
+
+function define(name, dependencies, factory) {
+  const n = arguments.length;
+  if (n < 2) factory = name, dependencies = [];
+  else if (n < 3) factory = dependencies, dependencies = typeof name === "string" ? [] : name;
+  queue.push(some.call(dependencies, isbuiltin) ? require => {
+    const exports = {};
+    const module = {exports};
+    return Promise.all(map.call(dependencies, name => {
+      name = name + "";
+      return name === "exports" ? exports : name === "module" ? module : require(name);
+    })).then(dependencies => {
+      factory.apply(null, dependencies);
+      return module.exports;
+    });
+  } : require => {
+    return Promise.all(map.call(dependencies, require)).then(dependencies => {
+      return typeof factory === "function" ? factory.apply(null, dependencies) : factory;
+    });
+  });
+}
+
+define.amd = {};
+
