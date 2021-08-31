8ed7d2d9e4c583882de3109a74dd85a23e4738e6
JJ Allaire <jj@rstudio.com>
2021-08-31 17:11:38 -0600

    backticks w/ escaped engine chunk inherit lang

--- src/resources/filters/quarto-pre/engine-escape.lua
+++ src/resources/filters/quarto-pre/engine-escape.lua
@@ -4,7 +4,13 @@
 function engineEscape()
   return {
     CodeBlock = function(el)
-      el.text = el.text:gsub("```{({+[^}]+}+)}", "```%1")
+      el.text = el.text:gsub("```{({+([^}]+)}+)}", function(engine, lang)
+        if #el.attr.classes == 0 then
+          lang = lang:match("^%w+")
+          el.attr.classes:insert(lang)
+        end
+        return "```" .. engine 
+      end)
       return el
     end
   }
