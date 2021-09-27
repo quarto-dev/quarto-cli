5a00513454753dc4934312752411a89b5351ad0e
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2021-09-27 16:34:20 -0700

    jupyter cell metadata

--- src/core/schema/chunk-metadata.ts
+++ src/core/schema/chunk-metadata.ts
@@ -27,21 +27,122 @@ import {
 
 import {
   anySchema as anyS,
-  objectSchema as objectS,
-  enumSchema as enumS,
   anyOfSchema as anyOfS,
+  arraySchema as arrayS,
+  enumSchema as enumS,
+  numericSchema as numericS,
+  objectSchema as objectS,
+  
   BooleanSchema as BooleanS,
+  StringSchema as StringS,
+  NullSchema as NullS,
 } from "./common.ts";
 
-export const ojsChunkMetadataSchema = objectS({
+import {
+  kCellAutoscroll,
+  kCellClasses,
+  kCellColab,
+  kCellColabType,
+  kCellColbOutputId,
+  kCellCollapsed,
+  kCellDeletable,
+  kCellFigAlign,
+  kCellFigAlt,
+  kCellFigCap,
+  kCellFigEnv,
+  kCellFigLink,
+  kCellFigPos,
+  kCellFigScap,
+  kCellFigSubCap,
+  kCellFormat,
+  kCellId,
+  kCellLabel,
+  kCellLinesToNext,
+  kCellLstCap,
+  kCellLstLabel,
+  kCellMdIndent,
+  kCellName,
+  kCellOutHeight,
+  kCellOutWidth,
+  kCellPanel,
+  kCellTags,
+  kCodeFold,
+  kCodeOverflow,
+  kCodeSummary,
+  kEcho,
+  kError,
+  kEval,
+  kInclude,
+  kLayout,
+  kLayoutAlign,
+  kLayoutNcol,
+  kLayoutNrow,
+  kLayoutVAlign,
+  kOutput,
+  kRawMimeType,
+  kWarning,
+} from "../../config/constants.ts";
+
+const commonCellOptionsSchema = objectS({
   properties: {
-    echo: anyOfS(BooleanS, enumS("fenced"))
+    [kCellLabel]: StringS,
+    [kCellFigCap]: anyOfS(StringS, arrayS(StringS)),
+    [kCellFigSubCap]: arrayS(StringS),
+    [kCellLstLabel]: StringS,
+    [kCellLstCap]: StringS,
+    [kCellClasses]: StringS,
+    [kCellPanel]: StringS,
+    [kCodeFold]: StringS,
+    [kCodeSummary]: StringS,
+    [kCodeOverflow]: StringS, // FIXME should this be enumS("wrap", "scroll")?
+    
+    [kCellFigScap]: StringS,
+    [kCellFigLink]: StringS,
+    [kCellFigAlign]: StringS,
+    [kCellFigEnv]: StringS,
+    [kCellFigPos]: StringS,
+    [kCellFigAlt]: StringS,
+    
+    [kEval]: anyOfS(BooleanS, NullS),
+    [kError]: BooleanS,
+    [kEcho]: anyOfS(BooleanS, enumS("fenced")),
+    [kOutput]: anyOfS(BooleanS, enumS("all", "asis")),
+    [kInclude]: BooleanS,
+    
+    [kLayoutNcol]: numericS({
+      "type": "integer",
+      "minimum": 1,
+    }),
+    [kLayoutNrow]: numericS({
+      "type": "integer",
+      "minimum": 1,
+    }),
   },
-  description: "be an OJS chunk metadata object"
+  
 });
 
-export const ojsChunkMetadata = new YAMLSchema(ojsChunkMetadataSchema);
+export const ojsCellOptionsSchema = objectS({
+  baseSchema: commonCellOptionsSchema,
+  properties: {
+    classes: arrayS(StringS)
+  },
+  description: "be an OJS cell options object"
+});
 
-export const languageChunkValidators: Record<string, YAMLSchema> = {
-  "ojs": ojsChunkMetadata
+export const jupyterCellOptionsSchema = objectS({
+  baseSchema: commonCellOptionsSchema,
+  properties: {
+    [kCellMdIndent]: StringS,
+    [kWarning]: BooleanS,
+    },
+  description: "be a Jupyter cell options object"
+});
+
+export const ojsCellOptions = new YAMLSchema(ojsCellOptionsSchema);
+export const jupyterCellOptions = new YAMLSchema(jupyterCellOptionsSchema);
+
+export const languageOptionsValidators: Record<string, YAMLSchema> = {
+  "ojs": ojsCellOptions,
+  "python": jupyterCellOptions
 };
+
