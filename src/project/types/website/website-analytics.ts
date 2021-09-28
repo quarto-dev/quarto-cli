b90c69059a656ff54ddcdd7d2b00275e2ee958ab
Charles Teague <cteague@gmail.com>
2021-09-28 10:49:11 -0400

    set page_load_consent_levels based upon type

--- src/project/types/website/website-analytics.ts
+++ src/project/types/website/website-analytics.ts
@@ -330,6 +330,10 @@ gtag('js', new Date());`);
 function cookieConsentScript(
   config: CookieConsentConfiguration,
 ) {
+  const consentLevels = (config.type === "implied")
+    ? `["strictly-necessary","functionality","tracking","targeting"]`
+    : `["strictly-necessary"]`;
+
   const privacyJs = config.policyUrl !== undefined
     ? `,\n"website_privacy_policy_url":"${config.policyUrl}"`
     : "";
@@ -342,7 +346,7 @@ cookieconsent.run({
   "consent_type":"${config.type}",
   "palette":"${config.palette === "dark" ? "dark" : "light"}",
   "language":"en",
-  "page_load_consent_levels":["strictly-necessary","functionality","tracking","targeting"],
+  "page_load_consent_levels":${consentLevels},
   "notice_banner_reject_button_hide":false,
   "preferences_center_close_button_hide":false,
   "website_name":"${config.siteName}"${privacyJs}
