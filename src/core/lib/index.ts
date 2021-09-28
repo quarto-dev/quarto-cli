2c23041f505def9c8e048056af13dcc1fe3aaead
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2021-09-28 14:11:55 -0700

    add esbuild target stub

---
+++ src/core/lib/index.ts
@@ -0,0 +1,15 @@
+/**
+* index.ts
+*
+* imports all of core lib for the esbuild target
+*
+* Copyright (C) 2021 by RStudio, PBC
+*
+*/
+
+export * from "./binary-search.ts";
+export * from "./break-quarto-md.ts";
+export * from "./mapped-text.ts";
+export * from "./partition-cell-options.ts";
+export * from "./ranged-text.ts";
+export * from "./text.ts";
