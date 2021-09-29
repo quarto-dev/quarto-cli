61e914ffcbd8438b4b42bdd5cdef9bdf1a31d4a5
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2021-09-29 10:12:43 -0700

    add binary search tests

---
+++ tests/unit/binary-search.test.ts
@@ -0,0 +1,42 @@
+/*
+* binary-search.test.ts
+*
+* Copyright (C) 2021 by RStudio, PBC
+*
+*/
+
+import { glb } from "../../src/core/lib/binary-search.ts";
+import bounds from "binary-search-bounds";
+
+import { unitTest } from "../test.ts";
+import { assert } from "testing/asserts.ts";
+
+unitTest("binary-search-test - glb property tests", () => {
+  // test by randomization that glb() behaves like le()
+
+  const nTests = 1000;
+  // const nTests = 10000000; // this passed locally
+  for (let i = 0; i < nTests; ++i) {
+    const sz = ~~(Math.random() * 10);
+    const a: number[] = [];
+    for (let j = 0; j < sz; ++j) {
+      a.push(~~(Math.random() * 10));
+    }
+    a.sort();
+    const n = ~~(Math.random() * 10);
+    const ours = glb(a, n);
+    const theirs = bounds.le(a, n);
+    if (ours !== theirs) {
+      console.log("randomization failed!", {a, n});
+    }
+    assert(ours === theirs);
+  }
+})
+
+unitTest("binary-search-test - previous failures", () => {
+  assert(glb([1, 2, 4, 5, 5, 6, 6, 7, 7], 9) === 8);
+  assert(glb([5, 8, 9], 1) === -1);
+  assert(glb([5], 4) === -1);
+  assert(glb([5], 5) === 0);
+  assert(glb([5], 6) === 0);
+});
