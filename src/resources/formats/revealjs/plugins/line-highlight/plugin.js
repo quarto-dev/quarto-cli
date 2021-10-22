dccd83c5d72292b0086b000a899bbf4f780397da
Christophe Dervieux <christophe.dervieux@gmail.com>
2021-10-22 16:16:20 +0200

    Support highlighting in fragment

--- src/resources/formats/revealjs/plugins/line-highlight/plugin.js
+++ src/resources/formats/revealjs/plugins/line-highlight/plugin.js
@@ -14,66 +14,94 @@ const delimiters = {
     lineRange: '-',
 };
 
+const kCodeLineNumbersAttr = 'data-code-line-numbers';
+const kFragmentIndex = 'data-fragment-index';
+
 function initQuartoLineHighlight(Reveal) {
 
     const divSourceCode = Reveal.getRevealElement().querySelectorAll('div.sourceCode');
 
     divSourceCode.forEach(el => {
-        if (el.hasAttribute('data-code-line-numbers')) {
-            const codeLineAttr = el.getAttribute('data-code-line-numbers')
-            if (!isLinesSelector(codeLineAttr)) {
-                el.removeAttribute('data-code-line-numbers')
-            } else {
-                // highlightLines;
-                const highlightSteps = splitLineNumbers(codeLineAttr);
-                if (highlightSteps.length) {
-                    // If we have at least one step, we generate fragments
-                    /*                 if (highlightSteps > 1) {
-                                        let fragmentIndex = null
-                                        const codeBlock = el.querySelectorAll("pre code");
+        if (el.hasAttribute(kCodeLineNumbersAttr)) {
+            const codeLineAttr = el.getAttribute(kCodeLineNumbersAttr);
+            el.removeAttribute('data-code-line-numbers');
+            if (isLinesSelector(codeLineAttr)) {
+                // Only process if attr is a string to select lines to highlights
+                // e.g "1|3,6|8-11"
+                const codeBlock = el.querySelectorAll("pre code");
+                codeBlock.forEach(
+                    code => {
+                        // move attributes on code block
+                        code.setAttribute(kCodeLineNumbersAttr, codeLineAttr)
 
-                                        // Generate fragments for all steps except the original block
-                                        highlightSteps.slice(1).forEach(
-                                            highlight => {
-                                                var fragmentBlock = codeBlock.cloneNode(true);
-                                                fragmentBlock.setAttribute('data-code-line-numbers', )
-                                                fragmentBlock.classList.add("fragment");
+                        // Check if there is steps and duplicate code block accordingly
+                        const highlightSteps = splitLineNumbers(codeLineAttr);
+                        if (highlightSteps.length > 1) {
+                            // If the original code block has a fragment-index,
+                            // each clone should follow in an incremental sequence
+                            let fragmentIndex = parseInt(code.getAttribute(kFragmentIndex), 10);
+                            fragmentIndex = typeof fragmentIndex !== 'number' || isNaN(fragmentIndex) ? null : fragmentIndex;
 
+                            highlightSteps.slice(1).forEach(
+                                // Generate fragments for all steps except the original block
+                                step => {
+                                    var fragmentBlock = code.cloneNode(true);
+                                    fragmentBlock.setAttribute('data-code-line-numbers', joinLineNumbers([step]));
+                                    fragmentBlock.classList.add("fragment");
+                                    code.parentNode.appendChild(fragmentBlock);
+                                    highlightCodeBlock(fragmentBlock);
 
-                                            }
-                                        )
-                                    } */
-                    highlightSteps[0].forEach(
-                        highlight => {
-                            const preCodeBlock = el.querySelectorAll("pre code");
-                            preCodeBlock.forEach(
-                                codeBlock => {
-                                    // Add expected class on <pre> for reveal CSS
-                                    codeBlock.parentNode.classList.add('code-wrapper');
-
-                                    // Select lines to highlight
-                                    spanToHighlight = [];
-                                    if (typeof highlight.last === 'number') {
-                                        spanToHighlight = [].slice.call(codeBlock.querySelectorAll(':scope > span:nth-child(n+' + highlight.first + '):nth-child(-n+' + highlight.last + ')'));
-                                    } else if (typeof highlight.first === 'number') {
-                                        spanToHighlight = [].slice.call(codeBlock.querySelectorAll(':scope > span:nth-child(' + highlight.first + ')'));
-                                    }
-                                    if (spanToHighlight.length) {
-                                        // Add a class on <code> and <span> to select line to highlight
-                                        spanToHighlight.forEach(
-                                            span => span.classList.add('highlight-line')
-                                        );
-                                        codeBlock.classList.add('has-line-highlights');
+                                    if (typeof fragmentIndex === 'number') {
+                                        fragmentBlock.setAttribute(kFragmentIndex, fragmentIndex);
+                                        fragmentIndex += 1;
+                                    } else {
+                                        fragmentBlock.removeAttribute(kFragmentIndex);
                                     }
 
+                                    // TODO add scrolling animation
                                 }
                             )
+                            code.removeAttribute(kFragmentIndex);
+                            code.setAttribute(kCodeLineNumbersAttr, joinLineNumbers([highlightSteps[0]]));
+                        }
+                        // TODO add scrolling animation: scroll the first highlight into view when the slide
 
-                        });
-                }
+                        highlightCodeBlock(code);
+
+                    });
             }
         }
-    })
+    });
+}
+
+function highlightCodeBlock(codeBlock) {
+
+    const highlightSteps = splitLineNumbers(codeBlock.getAttribute(kCodeLineNumbersAttr));
+
+    if (highlightSteps.length) {
+        // If we have at least one step, we generate fragments
+        highlightSteps[0].forEach(
+            highlight => {
+                // Add expected class on <pre> for reveal CSS
+                codeBlock.parentNode.classList.add('code-wrapper');
+
+                // Select lines to highlight
+                spanToHighlight = [];
+                if (typeof highlight.last === 'number') {
+                    spanToHighlight = [].slice.call(codeBlock.querySelectorAll(':scope > span:nth-child(n+' + highlight.first + '):nth-child(-n+' + highlight.last + ')'));
+                } else if (typeof highlight.first === 'number') {
+                    spanToHighlight = [].slice.call(codeBlock.querySelectorAll(':scope > span:nth-child(' + highlight.first + ')'));
+                }
+                if (spanToHighlight.length) {
+                    // Add a class on <code> and <span> to select line to highlight
+                    spanToHighlight.forEach(
+                        span => span.classList.add('highlight-line')
+                    );
+                    codeBlock.classList.add('has-line-highlights');
+                }
+            }
+        )
+    }
 }
 
 function splitLineNumbers(lineNumbersAttr) {
