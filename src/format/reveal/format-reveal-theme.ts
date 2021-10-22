8d7f1fe1bac60d5fbfc41e97774561b7d944a434
JJ Allaire <jj@rstudio.com>
2021-10-22 07:29:51 -0400

    cleanup reveal theme logic

--- src/format/reveal/format-reveal-theme.ts
+++ src/format/reveal/format-reveal-theme.ts
@@ -41,49 +41,87 @@ export async function revealTheme(format: Format, libDir: string) {
   // metadata override to return
   const metadata: Metadata = {};
 
-  // if there is no revealjs-url provided then use our embedded copy
+  // target revealDir
+  const revealDir = join(libDir, "revealjs");
+
+  // copy local version of reveal if we aren't using a remote version
   const revealJsUrl = format.metadata[kRevealJsUrl] as string | undefined;
-  if ((revealJsUrl === undefined) || isFileRef(revealJsUrl)) {
+  const localReveal = (revealJsUrl === undefined) || isFileRef(revealJsUrl);
+  if (localReveal) {
     // copy reveal dir
     const revealSrcDir = revealJsUrl ||
       formatResourcePath("revealjs", "reveal");
-    const revealDir = join(libDir, "revealjs");
     copyMinimal(revealSrcDir, revealDir);
     metadata[kRevealJsUrl] = pathWithForwardSlashes(
       revealDir,
     );
-
-    // theme could be a reveal theme name or an sccs file
-    // (if no theme is specified then use our built in theme)
-    const theme = (format.metadata![kTheme] ||
-      formatResourcePath("revealjs", "theme.scss")) as string;
-    if (!kRevealThemes.includes(theme)) {
-      const cssThemeDir = join(revealDir, "css", "theme");
-      const cssSourceDir = join(cssThemeDir, "source");
-      const cssTemplateDir = join(cssThemeDir, "template");
-      const themeScss = Deno.readTextFileSync(
-        existsSync(theme) ? theme : join(cssSourceDir, theme + ".scss"),
-      );
-      const scss = await compileWithCache(themeScss, [
-        cssSourceDir,
-        cssTemplateDir,
-      ]);
-      Deno.copyFileSync(
-        scss,
-        join(revealDir, "dist", "theme", "quarto.css"),
-      );
-      metadata[kTheme] = "quarto";
-    }
   }
 
-  // determine light vs. dark highlighting mode
-  const dark = format.metadata[kTheme] &&
-    kRevealDarkThemes.includes(format.metadata[kTheme] as string);
-  const highlightingMode: "light" | "dark" = dark ? "dark" : "light";
+  // are we using a reveal base theme? if we are then just return
+  // as-is with highlighting mode detected
+  const defaultTheme = format.metadata?.[kTheme] === undefined;
+  const revealBaseTheme =
+    (!defaultTheme &&
+      kRevealThemes.includes(format.metadata?.[kTheme] as string)) ||
+    (defaultTheme && !localReveal);
+  if (revealBaseTheme) {
+    return {
+      metadata,
+      [kTextHighlightingMode]: revealBaseThemeHighlightingMode(
+        format.metadata?.[kTheme],
+      ),
+    };
+  }
+
+  // quarto/custom reveal theme -- first verify we have local reveal
+  // (required for sass compilation)
+  if (!localReveal) {
+    throw new Error(
+      "Only built-in reveal themes can be used with a remote revealjs-url",
+    );
+  }
+
+  // theme is either user provided scss or quarto built-in scss
+  const themeFile = (format.metadata?.[kTheme] ||
+    formatResourcePath("revealjs", "theme.scss")) as string;
+  if (!existsSync(themeFile)) {
+    throw new Error(`Theme file '${themeFile}' not found`);
+  }
+  const cssThemeDir = join(revealDir, "css", "theme");
+  const cssSourceDir = join(cssThemeDir, "source");
+  const cssTemplateDir = join(cssThemeDir, "template");
+  const themeScss = Deno.readTextFileSync(themeFile);
+  const scss = await compileWithCache(themeScss, [
+    cssSourceDir,
+    cssTemplateDir,
+  ]);
+  Deno.copyFileSync(
+    scss,
+    join(revealDir, "dist", "theme", "quarto.css"),
+  );
+  metadata[kTheme] = "quarto";
 
   // return
+  const highlightingMode: "light" | "dark" = "light";
   return {
     metadata,
     [kTextHighlightingMode]: highlightingMode,
   };
 }
+
+function revealBaseThemeHighlightingMode(theme?: unknown) {
+  if (theme) {
+    const dark = typeof (theme) === "string" &&
+      kRevealDarkThemes.includes(theme);
+    const highlightingMode: "light" | "dark" = dark ? "dark" : "light";
+    return highlightingMode;
+  } else {
+    return "dark";
+  }
+}
+
+/*
+function revealFrameworkLayer(revealDir: string) : SassLayer {
+  const
+}
+*/
