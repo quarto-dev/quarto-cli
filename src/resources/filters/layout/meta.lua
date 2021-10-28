1df056d1bebb4ebdfc4fa994d8b7663189f82e28
Charles Teague <cteague@gmail.com>
2021-10-27 20:34:37 -0400

    Basic geometry implementation

--- src/resources/filters/layout/meta.lua
+++ src/resources/filters/layout/meta.lua
@@ -6,6 +6,7 @@ function layoutMetaInject()
   return {
     Meta = function(meta)
       
+      -- inject caption, subfig, tikz
       metaInjectLatex(meta, function(inject)
         inject(
           usePackage("caption") .. "\n" ..
@@ -16,6 +17,7 @@ function layoutMetaInject()
         end
       end)
 
+      -- enable column layout (packages and adjust geometry)
       if layoutState.hasColumns and isLatexOutput() then
         -- inject sidenotes package
         metaInjectLatex(meta, function(inject)
@@ -26,12 +28,56 @@ function layoutMetaInject()
             usePackage("marginnote")
           )
         end)
+        local documentclass = pandoc.utils.stringify(readOption(meta, 'documentclass'))
+
+        if documentclass == 'scrartcl' then
+          oneSidedColumnLayout(meta)
+        elseif documentclass == 'scrbook' then
+          twoSidedColumnLayout(meta)
+        elseif documentclass == 'scrreport' then
+          oneSidedColumnLayout(meta)
+        end
       end
-
-
-        
       return meta
     end
   }
+end
+
+function twoSidedColumnLayout(meta)
+  columnGeometry(meta)
+end
+
+function oneSidedColumnLayout(meta)
+  local classoption = readOption(meta, 'classoption')
+  if classoption == nil then
+    classoption = pandoc.MetaList({})
+  end
+
+  -- set one sided if not sidedness not already set
+  local sideoptions = classoption:filter(function(opt) 
+    local text = pandoc.utils.stringify(opt)
+    return text == 'oneside' or text == 'twoside'
+  end)
+  if #sideoptions == 0 then
+    classoption:insert('oneside')
+    meta.classoption = classoption
+  end
+  
+  columnGeometry(meta)
+
+end
+
+function columnGeometry(meta) 
+  -- customize the geometry
+  if not meta.geometry then
+    meta.geometry = pandoc.MetaList({})
+  end  
+  if #meta.geometry == 0 then
+    meta.geometry:insert('left=1in')
+    meta.geometry:insert('marginparwidth=2.15in')
+    meta.geometry:insert('textwidth=4.35in')
+    meta.geometry:insert('marginparsep=.3in')
+  end
+
 end
 
