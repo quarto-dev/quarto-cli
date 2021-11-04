7bf403b99e2919cc0fa4e62c50a17c061a926cc8
Charles Teague <cteague@gmail.com>
2021-11-04 17:26:27 -0400

    Don't enable except for gutter

--- src/resources/filters/layout/cites.lua
+++ src/resources/filters/layout/cites.lua
@@ -6,8 +6,9 @@
 function cites() 
   return {
     Inlines = function(inlines)  
+      local referenceLocation = param('reference-location', 'document')
       local modified = false
-      if isLatexOutput() then
+      if isLatexOutput() and referenceLocation == 'gutter'  then
         for i,inline in ipairs(inlines) do
           if inline.t == 'Cite' then
             local raw = pandoc.RawInline('latex', '\\marginpar{\\begin{footnotesize}{?quarto-cite:'.. inline.citations[1].id .. '}\\vspace{.1in}\\end{footnotesize}}')
