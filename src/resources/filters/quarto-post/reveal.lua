995b083dcb0ece36e7c732a18a3d133787fea0f8
JJ Allaire <jj@rstudio.com>
2021-11-10 06:47:46 -0500

    absolute positioning for elements on reveal slides

---
+++ src/resources/filters/quarto-post/reveal.lua
@@ -0,0 +1,43 @@
+-- reveal.lua
+-- Copyright (C) 2021 by RStudio, PBC
+
+function reveal()
+  if isRevealJsOutput() then
+    return {
+      Div = applyPosition,
+      Span = applyPosition,
+      Image = applyPosition
+    }
+  else
+    return {}
+  end
+end
+
+function applyPosition(el)
+  if el.attr.classes:includes("absolute") then
+    -- translate position attributes into style
+    local style = el.attr.attributes['style']
+    if style == nil then
+      style = ''
+    end
+    local attrs = pandoc.List({ "top", "left", "bottom", "right", "width", "height" })
+    for _, attr in ipairs(attrs) do
+      local value = el.attr.attributes[attr]
+      if value ~= nil then
+        style = style .. attr .. ': ' .. asCssSize(value) .. '; '
+        el.attr.attributes[attr] = nil
+      end
+    end
+    el.attr.attributes['style'] = style
+    return el
+  end
+end
+
+function asCssSize(size)
+  local number = tonumber(size)
+  if number ~= nil then
+    return tostring(number) .. "px"
+  else
+    return size
+  end
+end
