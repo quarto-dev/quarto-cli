30132c387943917b74f8649ab942b07ef0bd18ca
JJ Allaire <jj@rstudio.com>
2021-11-16 07:53:50 -0500

    restart jupyter kernel daemon on any error

--- src/resources/jupyter/jupyter.py
+++ src/resources/jupyter/jupyter.py
@@ -69,6 +69,7 @@ class ExecuteHandler(StreamRequestHandler):
          self.server.request_exit()
       except Exception as e:
          self.message("error", "\n\n" + str(e))
+         self.server.request_exit()
 
    # write a message back to the client      
    def message(self, type, data = ""):
