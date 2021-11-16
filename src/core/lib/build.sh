18085e886841dcba570da1d04b78285464a8f5a8
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2021-11-16 08:48:35 -0700

    remove js build artifacts and old js build scripts

--- src/core/lib/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "// deno-lint-ignore-file" > core-lib.js
-esbuild --bundle index.ts --outfile=/dev/stdout --format=iife >> core-lib.js
-cp core-lib.js ../../resources/editor/tools/yaml
-
