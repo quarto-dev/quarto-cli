1827c7d0d78ea9df9e419c50207c4a2f30164477
Charles Teague <cteague@gmail.com>
2021-11-16 16:51:49 -0500

    Make Quarto Env Variables Available in `quarto-bld` environment

--- package/src/cmd/pkg-cmd.ts
+++ package/src/cmd/pkg-cmd.ts
@@ -5,6 +5,7 @@
 *
 */
 import { Command } from "cliffy/command/mod.ts";
+import { join } from "path/mod.ts";
 import { info } from "log/mod.ts";
 
 import { Configuration, readConfiguration } from "../common/config.ts";
@@ -19,7 +20,18 @@ export function packageCommand(run: (config: Configuration) => Promise<void>) {
   ).action(async (args) => {
     const version = args[kVersion];
 
+    // Read the version and configuration
     const config = readConfiguration(version);
+
+    // Set up the bin and share environment for any downstream code
+    Deno.env.set("QUARTO_BIN_PATH", config.directoryInfo.bin);
+    Deno.env.set(
+      "QUARTO_SHARE_PATH",
+      join(config.directoryInfo.src, "resources"),
+    );
+    Deno.env.set("QUARTO_DEBUG", "true");
+
+    // Run the command
     info("Using configuration:");
     info(config);
     await run(config);
