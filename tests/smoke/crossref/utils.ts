d309f91c8b93dbde7ccbd1f47e8606e28de00187
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2021-11-19 14:43:37 -0700

    minor refactoring to reuse utils in crossref across rest of test suite

--- tests/smoke/crossref/utils.ts
+++ tests/smoke/crossref/utils.ts
@@ -7,13 +7,16 @@
 
 import { join } from "path/mod.ts";
 
-import { docs, outputForInput } from "../../utils.ts";
+import { fileLoader } from "../../utils.ts";
+// import { docs, outputForInput } from "../../utils.ts";
 
-export function crossref(file: string, to: string) {
-  const input = docs(join("crossrefs", file));
-  const output = outputForInput(input, to);
-  return {
-    input,
-    output,
-  };
-}
+// export function crossref(file: string, to: string) {
+//   const input = docs(join("crossrefs", file));
+//   const output = outputForInput(input, to);
+//   return {
+//     input,
+//     output,
+//   };
+// }
+
+export const crossref = fileLoader("crossrefs");
