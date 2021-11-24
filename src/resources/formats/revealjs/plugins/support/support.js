54b4739e5368aac6e2d87a4099d242b17af323c4
JJ Allaire <jj@rstudio.com>
2021-11-24 06:38:54 -0500

    remove auto-animate and hljs styles for printing

--- src/resources/formats/revealjs/plugins/support/support.js
+++ src/resources/formats/revealjs/plugins/support/support.js
@@ -139,9 +139,25 @@ window.QuartoSupport = function () {
     }
   }
 
+  function fixupForPrint(deck) {
+    if (/print-pdf/gi.test(window.location.search)) {
+      const slides = deck.getSlides();
+      slides.forEach(function (slide) {
+        slide.removeAttribute("data-auto-animate");
+      });
+      window.document.querySelectorAll(".hljs").forEach(function (el) {
+        el.classList.remove("hljs");
+      });
+      window.document.querySelectorAll(".hljs-ln-code").forEach(function (el) {
+        el.classList.remove("hljs-ln-code");
+      });
+    }
+  }
+
   return {
     id: "quarto-support",
     init: function (deck) {
+      fixupForPrint(deck);
       applyGlobalStyles(deck);
       addLogoImage(deck);
       addFooter(deck);
