5299e35705525632ad65c471b7a44f5b988f61a2
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2021-12-02 14:14:44 -0700

    skip test that will fail until we bring back front matter schema validation

--- tests/unit/schema-validation/format-execute.test.ts
+++ tests/unit/schema-validation/format-execute.test.ts
@@ -6,28 +6,30 @@ import { asMappedString } from "../../../src/core/mapped-text.ts";
 import { assert } from "testing/asserts.ts";
 
 unitTest("execute-validation", async () => {
-  const good = `
-title: A quarto document
-execute:
-  echo: false
-`;
+  // FIXME we skip validation for now while we're updating the schemas
+  //
+  //   const good = `
+  // title: A quarto document
+  // execute:
+  //   echo: false
+  // `;
 
-  const bad = `
-title: A bad quarto document
-execute: [1, 2, "foo"] 
-`;
-  await ensureAjv();
+  //   const bad = `
+  // title: A bad quarto document
+  // execute: [1, 2, "foo"] 
+  // `;
+  //   await ensureAjv();
 
-  const fm = await getFrontMatterSchema(true);
-  const fmSchema = new YAMLSchema(fm);
+  //   const fm = await getFrontMatterSchema(true);
+  //   const fmSchema = new YAMLSchema(fm);
 
-  // deno-lint-ignore no-explicit-any
-  const fromPlainString = (schema: any, src: string) =>
-    schema.validateParse(asMappedString(src), readAnnotatedYamlFromString(src));
+  //   // deno-lint-ignore no-explicit-any
+  //   const fromPlainString = (schema: any, src: string) =>
+  //     schema.validateParse(asMappedString(src), readAnnotatedYamlFromString(src));
   
-  const goodResult = fromPlainString(fmSchema, good);
-  const badResult = fromPlainString(fmSchema, bad);
+  //   const goodResult = fromPlainString(fmSchema, good);
+  //   const badResult = fromPlainString(fmSchema, bad);
 
-  assert(goodResult.errors.length === 0, "good document should pass");
-  assert(badResult.errors.length !== 0, "bad document should fail");
+  //   assert(goodResult.errors.length === 0, "good document should pass");
+  //   assert(badResult.errors.length !== 0, "bad document should fail");
 });
