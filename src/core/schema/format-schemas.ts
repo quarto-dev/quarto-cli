c65a4a9f72097b0ae82b86cb990cb1b58b33e1a1
JJ Allaire <jj@rstudio.com>
2021-12-06 15:47:04 -0500

    remove unused

--- src/core/schema/format-schemas.ts
+++ src/core/schema/format-schemas.ts
@@ -7,74 +7,54 @@
 *
 */
 
-import {
-  Schema
-} from "../lib/schema.ts";
+import { Schema } from "../lib/schema.ts";
 
-import {
-  getSchemaSchemas
-} from "./yaml-schema-schema.ts";
+import { resourcePath } from "../resources.ts";
 
-import {
-  resourcePath
-} from "../resources.ts";
+import { expandFormatAliases } from "./format-aliases.ts";
 
-import {
-  getExpandedFormatAliases,
-  expandFormatAliases
-} from "./format-aliases.ts";
-
-import {
-  convertFromYaml
-} from "./from-yaml.ts";
+import { convertFromYaml } from "./from-yaml.ts";
 
 import {
   readAndValidateYamlFromFile,
-  ValidationError
+  ValidationError,
 } from "./validated-yaml.ts";
 
-import {
-  error
-} from "log/mod.ts";
+import { error } from "log/mod.ts";
 
 import {
-  StringSchema as StringS,
-  BooleanSchema as BooleanS,
-  arraySchema as arrayOfS,
-  oneOfSchema as oneOfS,
-  anyOfSchema as anyOfS,
-  objectSchema as objectS,
-  enumSchema as enumS,
-  idSchema as withId,
   documentSchema,
+  enumSchema as enumS,
+  objectSchema as objectS,
+  oneOfSchema as oneOfS,
 } from "./common.ts";
 
 interface SchemaEntry {
   name: string;
   schema: Schema;
   hidden?: boolean;
+  // deno-lint-ignore no-explicit-any
   "default"?: any;
   alias?: string;
-  disabled?: string[],
-  enabled?: string[],
+  disabled?: string[];
+  enabled?: string[];
   description: string | {
-    short: string,
-    long: string
-  },
+    short: string;
+    long: string;
+  };
 }
 
-export async function getFormatSchema(format: string): Promise<Schema>
-{
-  const aliases = getExpandedFormatAliases();
-  const pandocFormats = aliases["pandoc-all"];
+export async function getFormatSchema(format: string): Promise<Schema> {
   const entries: SchemaEntry[] = [];
 
   try {
-    entries.push(...((await readAndValidateYamlFromFile(
-      resourcePath("schema/format-pandoc.yml"),
-      { $id: "good" }, // schemaEntryFileSchema is killing ajv currently :(
-      "schema entry file validation failed."
-    )) as SchemaEntry[]));
+    entries.push(
+      ...((await readAndValidateYamlFromFile(
+        resourcePath("schema/format-pandoc.yml"),
+        { $id: "good" }, // schemaEntryFileSchema is killing ajv currently :(
+        "schema entry file validation failed.",
+      )) as SchemaEntry[]),
+    );
   } catch (e) {
     error("\n");
     error(e);
@@ -95,19 +75,19 @@ export async function getFormatSchema(format: string): Promise<Schema>
       return expandFormatAliases(entry.enabled).indexOf(format) !== -1;
     }
     return true;
-  }
+  };
 
   const properties: Record<string, Schema> = {};
-  
+
   for (const entry of entries) {
     if (!useEntry(entry)) {
       continue;
     }
     let schema = convertFromYaml(entry.schema);
-    
+
     // pick documentation from entry (the in-schema documentation has
     // already been handled by convertFromYaml)
-    
+
     if (entry.description) {
       if (typeof entry.description === "string") {
         schema = documentSchema(schema, entry.description);
@@ -119,8 +99,8 @@ export async function getFormatSchema(format: string): Promise<Schema>
   }
   return oneOfS(
     objectS({
-      properties
+      properties,
     }),
-    enumS("default")
+    enumS("default"),
   );
 }
