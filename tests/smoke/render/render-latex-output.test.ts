3a886685819e849c1418d1cd522447c95e29da65
Charles Teague <cteague@gmail.com>
2021-12-13 12:58:53 -0500

    Improve Extended Figure Processing
    We previously always did extended figure processing on figure divs, but in commit 1a3697b961cb57ba0318ba09ccb3dbf1d94c78f5 we introduced a change to permit sometimes processing bare image figures without captions (which we previously didn't process). In this change, we ensnared the figure div processing in the same set of conditions, which broke the processing of figure divs (in a lot of cases).
    
    This change reverts the behavior for figure divs to always on unless explicitly turned off, and introduces a more sophisticated filter for bare figures that will process captionless images if fig attributes are present or if column data is provided.

---
+++ tests/smoke/render/render-latex-output.test.ts
@@ -0,0 +1,26 @@
+/*
+* render.latext-output.test.ts
+*
+* Copyright (C) 2020 by RStudio, PBC
+*
+*/
+
+import { docs, outputForInput } from "../../utils.ts";
+import { ensureFileRegexMatches } from "../../verify.ts";
+import { testRender } from "./render.ts";
+
+const renderVerifyLatexOutput = (input: string, regexes: RegExp[]) => {
+  const teXOutput = outputForInput(input, "latex");
+  testRender(input, "latex", true, [
+    ensureFileRegexMatches(teXOutput.outputPath, regexes),
+  ]);
+};
+
+renderVerifyLatexOutput(docs("latex-output/captionless-margin-image.qmd"), [
+  /\\begin{marginfigure}/,
+]);
+renderVerifyLatexOutput(docs("latex-output/figure-div.qmd"), [
+  /{\\centering/,
+  /\\caption{\\label{fig-foo}This is the figure}/,
+  /See fig\.~\\ref{fig-foo} for more\./,
+]);
