c64c597e766dfcbb5f73aea8933e4ea952e801fb
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2021-12-14 18:46:34 -0700

    add define() and defineCached() to facilitate schema reuse

--- src/core/schema/definitions.ts
+++ src/core/schema/definitions.ts
@@ -7,13 +7,40 @@
 
 import { withValidator } from "../lib/validator-queue.ts";
 import { convertFromYaml } from "./from-yaml.ts";
-import { idSchema } from "./common.ts";
+import { idSchema, refSchema } from "./common.ts";
 import { readYaml } from "../yaml.ts";
 import { ensureAjv } from "./yaml-schema.ts";
-import { normalizeSchema, Schema, setSchemaDefinition } from "../lib/schema.ts";
+import { normalizeSchema, Schema, getSchemaDefinition, setSchemaDefinition } from "../lib/schema.ts";
 import { error } from "log/mod.ts";
 import { schemaPath } from "./utils.ts";
 import { buildSchemaResources } from "./from-yaml.ts";
+
+export function defineCached(thunk: () => Promise<Schema>, schemaId: string): (() => Promise<Schema>)
+{
+  let schema: Schema;
+  return async () => {
+    await ensureAjv();
+    const result = getSchemaDefinition(schemaId);
+    if (result === undefined) {
+      schema = await thunk();
+      if (schemaId !== schema!.$id as string) {
+        schema = idSchema(schema, schemaId);
+      }
+      await define(schema);
+    }
+    return refSchema(schema!.$id as string, (schema!.description as string) || `be a {schema['$id']}`);
+  };
+}
+
+export async function define(schema: Schema)
+{
+  await ensureAjv();
+  if (getSchemaDefinition(schema.$id) === undefined) {
+    await withValidator(normalizeSchema(schema), (_validator) => {
+      setSchemaDefinition(schema);
+    });
+  }
+}
 
 export async function loadDefaultSchemaDefinitions()
 {
