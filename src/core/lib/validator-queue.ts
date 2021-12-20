d67807feef67c9663bf18998511c5daf0e5d9f5f
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2021-12-20 15:17:01 -0700

    improve error messages when 'yes', 'no', 'on', 'off', etc is used in a schema that expects booleans

--- src/core/lib/validator-queue.ts
+++ src/core/lib/validator-queue.ts
@@ -8,9 +8,47 @@
 import { YAMLSchema, LocalizedError, AnnotatedParse } from "./yaml-schema.ts";
 import { PromiseQueue } from "./promise.ts";
 import { Schema } from "./schema.ts";
+import { tidyverseInfo, tidyverseError } from "../log.ts";
 
 const yamlValidators: Record<string, YAMLSchema> = {};
 const validatorQueues: Record<string, PromiseQueue<void>> = {};
+
+function checkForBadBoolean(
+  error: LocalizedError, parse: AnnotatedParse, schema: Schema
+)
+{
+  schema = error.error.params.schema;
+  if (!(typeof error.violatingObject.result === "string" &&
+    error.error.keyword === "type" &&
+    schema?.type === "boolean")) {
+    return error;
+  }
+  const strValue = error.violatingObject.result;
+  const verbatimInput = error.source.value.substring(
+    error.violatingObject.start,
+    error.violatingObject.end);
+
+  // from https://yaml.org/type/bool.html
+  const yesses = new Set("y|Y|yes|Yes|YES|true|True|TRUE|on|On|ON".split("|"));
+  const nos = new Set("n|N|no|No|NO|false|False|FALSE|off|Off|OFF".split("|"));
+  let fix;
+  if (yesses.has(strValue)) {
+    fix = true;
+  } else if (nos.has(strValue)) {
+    fix = false;
+  } else {
+    return error;
+  }
+
+  const errorHeading = `${error.location}: \`${verbatimInput}\` must be a boolean`;
+  const errorMessage = tidyverseError(`\`${verbatimInput}\` is a string.`);
+  const suggestion1 = tidyverseInfo(`Quarto uses YAML 1.2, which interprets booleans strictly.`)
+  const suggestion2 = tidyverseInfo(`Did you mean \`${fix}\` instead?`);
+  return {
+    ...error,
+    message: [errorHeading, errorMessage, suggestion1, suggestion2].join("\n")
+  };
+}
 
 function getSchemaName(schema: Schema): string {
   const schemaName = schema["$id"] || schema["$ref"];
@@ -33,6 +71,10 @@ function getValidator(schema: Schema): YAMLSchema {
   const validator = new YAMLSchema(schema);
 
   yamlValidators[schemaName] = validator;
+
+  // FIXME where do we declare all of the standard validator error handlers?
+  validator.addHandler(checkForBadBoolean);
+  
   return validator;
 }
 
