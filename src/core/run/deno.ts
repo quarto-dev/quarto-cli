1343af61321add4235551ae310b3e58ed90b4c1e
JJ Allaire <jj@rstudio.com>
2021-12-24 12:24:55 -0500

    copy deno_std to cache dir

--- src/core/run/deno.ts
+++ src/core/run/deno.ts
@@ -5,22 +5,28 @@
 *
 */
 
-import { extname, join } from "path/mod.ts";
+import { info } from "log/mod.ts";
+import { existsSync } from "fs/mod.ts";
+import { copySync } from "fs/copy.ts";
+import { dirname, extname, join } from "path/mod.ts";
+import { quartoCacheDir } from "../appdirs.ts";
 import { execProcess } from "../process.ts";
 import { binaryPath, resourcePath } from "../resources.ts";
 import { RunHandler, RunHandlerOptions } from "./run.ts";
+import { removeIfExists } from "../path.ts";
 
 export const denoRunHandler: RunHandler = {
   canHandle: (script: string) => {
     return [".js", ".ts"].includes(extname(script).toLowerCase());
   },
   run: async (script: string, args: string[], options?: RunHandlerOptions) => {
-    // add deno std library
+    // add deno std library (one time initialize cache on demand)
+    const denoDir = initDenoCache();
     options = {
       ...options,
       env: {
         ...options?.env,
-        DENO_DIR: resourcePath(join("deno_std", "cache")),
+        DENO_DIR: denoDir,
       },
     };
     const importMap = resourcePath(join("deno_std", "import_map.json"));
@@ -41,3 +47,23 @@ export const denoRunHandler: RunHandler = {
     });
   },
 };
+
+function initDenoCache() {
+  // see if we need to create the cache
+  const distDenoStd = resourcePath("deno_std");
+  const distLock = join(distDenoStd, "deno_std.lock");
+  const cacheDenoStd = quartoCacheDir("deno_std");
+  const cacheLock = join(cacheDenoStd, "deno_std.lock");
+  if (
+    !existsSync(cacheLock) ||
+    Deno.readTextFileSync(cacheLock) != Deno.readTextFileSync(distLock)
+  ) {
+    removeIfExists(cacheDenoStd);
+    copySync(distDenoStd, cacheDenoStd, { overwrite: true });
+    info("Initializing deno_dir cache");
+  } else {
+    info("Using cached deno_dir");
+  }
+
+  return join(cacheDenoStd, "cache");
+}
