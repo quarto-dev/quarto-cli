fb72be8bfdb96a7f618e25353ae5e0631bbe1e93
Charles Teague <cteague@gmail.com>
2022-01-03 10:59:52 -0500

    Add test that compares figure syntax output

--- tests/smoke/crossref/syntax.test.ts
+++ tests/smoke/crossref/syntax.test.ts
@@ -8,6 +8,15 @@
 import { ensureFileRegexMatches } from "../../verify.ts";
 import { testRender } from "../render/render.ts";
 import { crossref } from "./utils.ts";
+import {
+  ExecuteOutput,
+  test,
+  TestContext,
+  TestDescriptor,
+  Verify,
+} from "../../test.ts";
+import { assert, fail } from "testing/asserts.ts";
+import { quarto } from "../../../src/quarto.ts";
 
 const syntaxQmd = crossref("syntax.qmd", "html");
 testRender(syntaxQmd.input, "html", false, [
@@ -20,3 +29,52 @@ testRender(syntaxQmd.input, "html", false, [
     /\?@fig-/,
   ]),
 ]);
+
+// Test that two different syntaxes for a basic figure produce
+// the same figure output
+const imgQmd = crossref("figure-syntax-img.qmd", "html");
+const divQmd = crossref("figure-syntax-div.qmd", "html");
+const verify: Verify = {
+  name: "Compare output",
+  verify: (_outputs: ExecuteOutput[]) => {
+    const extractBodyRegex = /<body>((.|\n)*)<\/body>/;
+
+    const imgText = Deno.readTextFileSync(imgQmd.output.outputPath);
+    const imgBody = extractBodyRegex.exec(imgText);
+
+    const divText = Deno.readTextFileSync(divQmd.output.outputPath);
+    const divBody = extractBodyRegex.exec(divText);
+
+    if (imgBody && divBody) {
+      assert(
+        imgBody[0] === divBody[0],
+        "Contents of HTML generated for figures do not match.",
+      );
+    } else {
+      fail("Unable to extract html body from output.");
+    }
+
+    return Promise.resolve();
+  },
+};
+const context: TestContext = {
+  teardown: () => {
+    Deno.removeSync(imgQmd.output.outputPath);
+    Deno.removeSync(imgQmd.output.supportPath, { recursive: true });
+
+    Deno.removeSync(divQmd.output.outputPath);
+    Deno.removeSync(divQmd.output.supportPath, { recursive: true });
+    return Promise.resolve();
+  },
+};
+const testDesc: TestDescriptor = {
+  name: "test html produced by different figure syntax",
+  context,
+  execute: async () => {
+    await quarto(["render", imgQmd.input]);
+    await quarto(["render", divQmd.input]);
+  },
+  verify: [verify],
+  type: "smoke",
+};
+test(testDesc);
