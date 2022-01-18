e6111a30257c875a8fc64e442eddad95abc3e36e
Charles Teague <cteague@gmail.com>
2022-01-18 18:10:11 -0500

    Category filtering and pagination will modify the URL / history

--- src/resources/projects/website/listing/quarto-listing.js
+++ src/resources/projects/website/listing/quarto-listing.js
@@ -1,72 +1,132 @@
-const kCategoryHashPrefix = "category:";
-
 window["quarto-listing-loaded"] = () => {
-  // Read the category out of the hash and filter to it
-  const category = getCategoryHash();
-  if (category) {
-    filterListingCategory(category);
+  // Process any existing hash
+  const hash = getHash();
+  if (hash) {
+    if (hash.category) {
+      activateCategory(hash.category);
+    }
+
+    // Filter the page number
+    if (hash.page) {
+      showPage(hash.page);
+    }
   }
-};
-
-window.document.addEventListener("DOMContentLoaded", function (_event) {
-  const categoryEls = window.document.querySelectorAll(
-    ".quarto-listing-category .category"
-  );
-
-  // Attach click handlers to categories
-  for (const categoryEl of categoryEls) {
-    const category = categoryEl.getAttribute("data-category");
-    categoryEl.onclick = () => {
-      activateCategory(category);
-    };
-  }
-});
-
-function getCategoryHash() {
-  const currentUrl = new URL(window.location);
-  const hash = currentUrl.hash ? currentUrl.hash.slice(1) : undefined;
-  if (hash !== undefined && hash.startsWith(kCategoryHashPrefix)) {
-    return hash.slice(kCategoryHashPrefix.length);
-  } else {
-    return undefined;
-  }
-}
-
-function setCategoryHash(category) {
-  const newUrl = new URL(window.location);
-  newUrl.hash = `#${kCategoryHashPrefix}${category}`;
-  window.history.replaceState({}, "", newUrl);
-}
-
-function filterListingCategory(category) {
-  setCategoryHash(category);
+  refreshPaginationHandlers();
 
   const listingIds = Object.keys(window["quarto-listings"]);
   for (const listingId of listingIds) {
     const list = window["quarto-listings"][listingId];
-    if (list) {
-      if (category === "") {
-        // resets the filter
-        list.filter();
-      } else {
-        // filter to this category
-        list.filter(function (item) {
-          const itemValues = item.values();
-          if (itemValues.categories !== null) {
-            const categories = itemValues.categories.split(",");
 
-            // TODO: update url hash
-            // TODO: NEED TO GET CATEGORIES INTO LIST!
-            // TODO: CONDITIONAL FILTERING (EG YAML PARAM)
-            // TODO: MOVING CATS CODE INTO BETTER ORGANIZATION OR LOCATION
-            // TODO: LOCALIZE CATEGORY TITLE
-            // TODO: SUPPORT WORD CLOUD STYLE CATEGORIES
-            return categories.includes(category);
-          } else {
-            return false;
-          }
-        });
+    list.on("updated", function () {
+      setTimeout(refreshPaginationHandlers, 100);
+    });
+  }
+};
+
+window.addEventListener(
+  "hashchange",
+  function () {
+    console.log(window.location);
+  },
+  false
+);
+
+window.document.addEventListener("DOMContentLoaded", function (_event) {
+  // Attach click handlers to categories
+  const categoryEls = window.document.querySelectorAll(
+    ".quarto-listing-category .category"
+  );
+
+  for (const categoryEl of categoryEls) {
+    const category = categoryEl.getAttribute("data-category");
+    categoryEl.onclick = () => {
+      activateCategory(category);
+      setCategoryHash(category);
+    };
+  }
+});
+
+function setCategoryHash(category) {
+  setHash({ category });
+}
+
+function setPageHash(page) {
+  const currentHash = getHash() || {};
+  currentHash.page = page;
+  setHash(currentHash);
+}
+
+function refreshPaginationHandlers() {
+  // Attach click handlers to pagination
+  const paginationEls = window.document.querySelectorAll(
+    ".pagination li.page-item:not(.disabled) .page.page-link"
+  );
+
+  for (const paginationEl of paginationEls) {
+    paginationEl.onclick = (sender) => {
+      setPageHash(sender.target.getAttribute("data-i"));
+      showPage(sender.target.getAttribute("data-i"));
+      setTimeout(refreshPaginationHandlers);
+      return false;
+    };
+  }
+}
+
+function getHash() {
+  // Hashes are of the form
+  // #name:value|name1:value1|name2:value2
+  const currentUrl = new URL(window.location);
+  const hashRaw = currentUrl.hash ? currentUrl.hash.slice(1) : undefined;
+  return parseHash(hashRaw);
+}
+
+const kAnd = "&";
+const kEquals = "=";
+
+function parseHash(hash) {
+  if (!hash) {
+    return undefined;
+  }
+  const hasValuesStrs = hash.split(kAnd);
+  const hashValues = hasValuesStrs
+    .map((hashValueStr) => {
+      const vals = hashValueStr.split(kEquals);
+      if (vals.length === 2) {
+        return { name: vals[0], value: vals[1] };
+      } else {
+        return undefined;
       }
+    })
+    .filter((value) => {
+      return value !== undefined;
+    });
+
+  const hashObj = {};
+  hashValues.forEach((hashValue) => {
+    hashObj[hashValue.name] = hashValue.value;
+  });
+  return hashObj;
+}
+
+function makeHash(obj) {
+  return Object.keys(obj)
+    .map((key) => {
+      return `${key}${kEquals}${obj[key]}`;
+    })
+    .join(kAnd);
+}
+
+function setHash(obj) {
+  const hash = makeHash(obj);
+  window.history.pushState(null, null, `#${hash}`);
+}
+
+function showPage(page) {
+  const listingIds = Object.keys(window["quarto-listings"]);
+  for (const listingId of listingIds) {
+    const list = window["quarto-listings"][listingId];
+    if (list) {
+      list.show((page - 1) * list.page + 1, list.page);
     }
   }
 }
@@ -84,11 +144,34 @@ function activateCategory(category) {
   const categoryEl = window.document.querySelector(
     `.quarto-listing-category .category[data-category='${category}'`
   );
-  console.log(categoryEl);
   if (categoryEl) {
     categoryEl.classList.add("active");
   }
 
   // Filter the listings to this category
   filterListingCategory(category);
+}
+
+function filterListingCategory(category) {
+  const listingIds = Object.keys(window["quarto-listings"]);
+  for (const listingId of listingIds) {
+    const list = window["quarto-listings"][listingId];
+    if (list) {
+      if (category === "") {
+        // resets the filter
+        list.filter();
+      } else {
+        // filter to this category
+        list.filter(function (item) {
+          const itemValues = item.values();
+          if (itemValues.categories !== null) {
+            const categories = itemValues.categories.split(",");
+            return categories.includes(category);
+          } else {
+            return false;
+          }
+        });
+      }
+    }
+  }
 }
