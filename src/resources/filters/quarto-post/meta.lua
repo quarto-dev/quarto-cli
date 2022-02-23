ff3cdfd06877ac935a23cf47d2c66338e79c63ce
Charles Teague <cteague@gmail.com>
2022-02-23 09:21:51 -0500

    Be sure to check for nil :-/

--- src/resources/filters/quarto-post/meta.lua
+++ src/resources/filters/quarto-post/meta.lua
@@ -15,7 +15,9 @@ function quartoPostMetaInject()
       -- Citeproc interprets this as a cite, and provides
       -- a warning of unresolve citation. But we know
       -- that a citation is never allowed in the creator field
-      meta.website['twitter-card'].creator = nil
+      if meta.website ~= nil and meta.website['twitter-card'] ~= nil then
+        meta.website['twitter-card'].creator = nil
+      end
 
       return meta
     end
