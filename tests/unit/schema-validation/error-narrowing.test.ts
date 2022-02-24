dc6521ddd3ca3516b2b27dfc7d805b262a63f316
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-02-24 11:29:04 -0700

    add error narrowing unit tests

---
+++ tests/unit/schema-validation/error-narrowing.test.ts
@@ -0,0 +1,44 @@
+/*
+* error-narrowing.test.ts
+*
+* Unit tests regarding the YAML validation error narrowing heuristics
+*
+* Copyright (C) 2022 by RStudio, PBC
+*
+*/
+
+import {
+  assertYamlValidationFails,
+  expectValidationError,
+  schemaFromString,
+  yamlValidationUnitTest,
+} from "./utils.ts";
+import { assertRejects } from "testing/asserts.ts";
+import {
+  readAndValidateYamlFromMappedString,
+  ValidationError,
+} from "../../../src/core/schema/validated-yaml.ts";
+import { asMappedString } from "../../../src/core/lib/mapped-text.ts";
+
+// deno-lint-ignore require-await
+yamlValidationUnitTest("schema-narrowing", async () => {
+  const schema = schemaFromString(`
+id: schema-test-1
+record:
+  baz: number
+  bar: string
+`);
+  const ymlStr = `
+baz: 3
+`;
+  assertYamlValidationFails(async () => {
+    await readAndValidateYamlFromMappedString(
+      asMappedString(ymlStr),
+      schema,
+      "This should reject",
+    );
+  }, (e: ValidationError) =>
+    expectValidationError(e)
+      .toHaveLength(1)
+      .forSchemaPathToEndWith("required"));
+});
--- tests/unit/schema-validation/utils.ts
+++ tests/unit/schema-validation/utils.ts
@@ -14,6 +14,11 @@ import {
   setInitializer,
 } from "../../../src/core/lib/yaml-validation/state.ts";
 import { ensureAjv } from "../../../src/core/schema/yaml-schema.ts";
+import { convertFromYAMLString } from "../../../src/core/schema/from-yaml.ts";
+import { setSchemaDefinition } from "../../../src/core/lib/yaml-validation/schema.ts";
+import { ValidationError } from "../../../src/core/schema/validated-yaml.ts";
+import { isEqual } from "../../../src/core/lodash.ts";
+import { assertRejects } from "https://deno.land/std@0.122.0/testing/asserts.ts";
 
 export const schemaTestFile = fileLoader("schema-validation");
 
@@ -21,6 +26,73 @@ export async function fullInit() {
   await initPrecompiledModules();
   await initTreeSitter();
   await ensureAjv();
+}
+
+export function schemaFromString(schemaStr: string) {
+  const schema = convertFromYAMLString(schemaStr);
+  setSchemaDefinition(schema);
+  return schema;
+}
+
+// deno-lint-ignore no-explicit-any
+export function expectValidationError(e: any) {
+  let willThrow = e instanceof ValidationError;
+  const goodE = e as ValidationError;
+  const result = {
+    toHaveLength(l: number) {
+      if (!willThrow) return result;
+      if (goodE.validationErrors.length !== l) {
+        willThrow = false;
+      }
+      return result;
+    },
+    forSchemaPathToEndWith(spec: string | string[], which?: number) {
+      if (!willThrow) return result;
+      if (typeof spec === "string") {
+        spec = [spec];
+      }
+      if (which === undefined) {
+        which = 0;
+      }
+      if (goodE.validationErrors[which] === undefined) {
+        willThrow = false;
+        return result;
+      }
+      if (
+        !isEqual(
+          spec,
+          goodE.validationErrors[which].schemaPath.slice(-spec.length),
+        )
+      ) {
+        willThrow = false;
+        return result;
+      }
+      return result;
+    },
+    go() {
+      if (willThrow) {
+        throw e;
+      }
+    },
+  };
+  return result;
+}
+
+export type ValidationChecker = ReturnType<typeof expectValidationError>;
+
+export async function assertYamlValidationFails(
+  fun: () => Promise<unknown>,
+  checker: (e: ValidationError) => ValidationChecker,
+) {
+  await assertRejects(async () => {
+    try {
+      await fun();
+    } catch (e) {
+      if (e instanceof ValidationError) {
+        checker(e).go();
+      }
+    }
+  });
 }
 
 // deno-lint-ignore require-await
