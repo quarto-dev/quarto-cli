3d46b0e6bcd1a07836c516a742324b52f1abc2d5
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-03-02 04:36:19 -0700

    add missing file

---
+++ src/core/lib/polyfills.ts
@@ -0,0 +1,21 @@
+/*
+* polyfills.ts
+*
+* some polyfills we can't depend on in older execution environments.
+*
+* Copyright (C) 2022 by RStudio, PBC
+*
+* (Note that code here is derived from other open-source libraries. We include a link and respective copyright notice)
+*
+*/
+
+// based on https://raw.githubusercontent.com/feross/fromentries/master/index.js
+// MIT License. Feross Aboukhadijeh <https://feross.org/opensource>
+export function fromEntries<T>(
+  iterable: [string, T][],
+): Record<string, T> {
+  return [...iterable].reduce((obj, [key, val]) => {
+    obj[key] = val;
+    return obj;
+  }, {} as Record<string, T>);
+}
