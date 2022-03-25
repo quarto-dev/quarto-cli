19dc282cc8df1f51f5fdf978ca573e044d66e513
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-03-25 14:41:26 +0100

    [revealjs] tests for aside, footnotes and speaker notes

--- tests/smoke/render/render-reveal.test.ts
+++ tests/smoke/render/render-reveal.test.ts
@@ -5,7 +5,25 @@
 *
 */
 
-import { docs } from "../../utils.ts";
+import { docs, outputForInput  } from "../../utils.ts";
+import { ensureHtmlElements } from "../../verify.ts";
 import { testRender } from "./render.ts";
 
+// demo file renders ok
 testRender(docs("reveal/index.qmd"), "revealjs", false);
+
+// aside and footnotes correctly moved with speaker notes ignored
+const input = docs("reveal/aside-footnotes.qmd");
+const output = outputForInput(input, "revealjs");
+testRender(input, "revealjs", false, [
+  ensureHtmlElements(output.outputPath, [
+    // speaker notes are left not moved
+    "section#slide-with-speaker-notes > aside.notes:last-child",
+    // div of class aside are moved
+    "section#slide-with-footnotes > aside:last-child > div > p",
+    // footnotes are put in aside
+    "section#slide-with-footnotes > aside:last-child > ol.aside-footnotes > li",
+    // footnotes back are removed
+    "section#slide-with-footnotes > aside:last-child > ol.aside-footnotes > li > :not(.footnote-back)",
+  ]),
+]);
