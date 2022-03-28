c519a1e4dac81acf3e2e9e9b6a8df48f520de665
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-03-28 16:10:01 +0200

    Add test for formatting preserved in callout caption

--- tests/smoke/render/render-callout.test.ts
+++ tests/smoke/render/render-callout.test.ts
@@ -19,12 +19,16 @@ const htmlOutput = outputForInput(input, "html");
 
 testRender(input, "html", false, [
   ensureHtmlElements(htmlOutput.outputPath, [
+    // callout environments are created
     "div.callout-warning",
     "div.callout-important",
     "div.callout-note",
     "div.callout-tip",
     "div.callout-caution",
     "div.callout.no-icon",
+    // formatting is kept in caption
+    "div.callout-tip > div.callout-header > div.callout-caption-container > strong",
+    "div.callout-tip > div.callout-header > div.callout-caption-container > code"
   ]),
 ]);
 
@@ -33,21 +37,27 @@ testRender(input, "latex", true, [
   ensureFileRegexMatches(teXOutput.outputPath, [
     requireLatexPackage("fontawesome5"),
     requireLatexPackage("tcolorbox", "many"),
+    // callout environments are created
     /quarto-callout-warning/,
     /quarto-callout-important/,
     /quarto-callout-note/,
     /quarto-callout-tip/,
     /quarto-callout-caution/,
+    // formatting is kept in caption,
+    /{Caption with \\textbf{formatted} text, like \\texttt{function\\_name\(\)}/,
   ]),
 ]);
 
 const docXoutput = outputForInput(input, "docx");
 testRender(input, "docx", true, [
   ensureDocxRegexMatches(docXoutput.outputPath, [
+    // callout environments are created
     /<pic:cNvPr.*warning\.png".*?\/>/,
     /<pic:cNvPr.*important\.png".*?\/>/,
     /<pic:cNvPr.*note\.png".*?\/>/,
     /<pic:cNvPr.*tip\.png".*?\/>/,
     /<pic:cNvPr.*caution\.png".*?\/>/,
+    // formatting is kept in caption,
+    /Caption with.*<w:bCs.*formatted.*text, like.*<w:rStyle w:val="VerbatimChar".*function_name\(\)/
   ]),
 ]);
