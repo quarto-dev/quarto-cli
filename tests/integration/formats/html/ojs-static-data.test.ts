2379f2fa722b32cbdca779ca05f9316866d63759
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-04-14 08:53:11 -0700

    clean up comments and unneeded code

--- tests/integration/formats/html/ojs-static-data.test.ts
+++ tests/integration/formats/html/ojs-static-data.test.ts
@@ -12,12 +12,6 @@ import { testRender } from "../../../smoke/render/render.ts";
 import { localFileURL } from "../../../puppeteer.ts";
 import { verifyOjsValue } from "./ojs-utils.ts";
 
-// this appeases the TypeScript typechecker
-// who doesn't like what it sees inside the inPuppeteer closures
-// deno-lint-ignore no-explicit-any no-unused-vars
-const window = (undefined as any);
-
-
 const filename = "docs/ojs/test-ojs-static-data.qmd";
 testRender(filename, "html", false, [
   verifyOjsValue(localFileURL(filename), "v2", 6.9),
@@ -27,4 +21,3 @@ testRender(filename, "html", false, [
     return Promise.resolve();
   },
 });
-
