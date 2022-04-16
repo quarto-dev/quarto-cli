7cff67f4dd8748f9a5d4aed594cdcb2d1b20038f
JJ Allaire <jj@rstudio.com>
2022-04-16 15:22:41 -0400

    Revert "break core/run cyclic dependency"
    This reverts commit ed773f2e35f31a8cba0bd8ba89eadc337a662338.

--- src/core/run/run.ts
+++ src/core/run/run.ts
@@ -5,10 +5,29 @@
 *
 */
 
+import { ProcessResult } from "../../core/process.ts";
 import { denoRunHandler } from "./deno.ts";
 import { luaRunHandler } from "./lua.ts";
 import { pythonRunHandler } from "./python.ts";
 import { rRunHandler } from "./r.ts";
+
+export interface RunHandlerOptions {
+  cwd?: string;
+  env?: {
+    [key: string]: string;
+  };
+  stdout?: "inherit" | "piped" | "null" | number;
+}
+
+export interface RunHandler {
+  canHandle: (script: string) => boolean;
+  run: (
+    script: string,
+    args: string[],
+    stdin?: string,
+    options?: RunHandlerOptions,
+  ) => Promise<ProcessResult>;
+}
 
 export const kRunHandlers = [
   denoRunHandler,
