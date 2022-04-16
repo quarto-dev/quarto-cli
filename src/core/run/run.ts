d42f467b8c816cf14e0546723d49c158d787a786
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-04-16 13:05:21 -0700

    break cyclic dependency on run handlers

--- src/core/run/run.ts
+++ src/core/run/run.ts
@@ -5,17 +5,23 @@
 *
 */
 
-import { denoRunHandler } from "./deno.ts";
-import { luaRunHandler } from "./lua.ts";
-import { pythonRunHandler } from "./python.ts";
-import { rRunHandler } from "./r.ts";
+import { RunHandler } from "./types.ts";
 
-export const kRunHandlers = [
-  denoRunHandler,
-  luaRunHandler,
-  pythonRunHandler,
-  rRunHandler,
+//import { denoRunHandler } from "./deno.ts";
+//import { luaRunHandler } from "./lua.ts";
+//import { pythonRunHandler } from "./python.ts";
+//import { rRunHandler } from "./r.ts";
+
+const kRunHandlers: RunHandler[] = [
+  //  denoRunHandler,
+  //  luaRunHandler,
+  //  pythonRunHandler,
+  //  rRunHandler,
 ];
+
+export function installRunHandler(handler: RunHandler) {
+  kRunHandlers.push(handler);
+}
 
 export function handlerForScript(script: string) {
   return kRunHandlers.find((handler) => handler.canHandle(script));
