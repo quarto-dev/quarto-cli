1afc228c288977028ae77b62e764cab41016c33c
JJ Allaire <jj@rstudio.com>
2022-04-18 07:29:32 -0400

    disable schema validation for project files

--- src/project/project-context.ts
+++ src/project/project-context.ts
@@ -114,11 +114,14 @@ export async function projectContext(
       const configFiles = [configFile];
 
       const errMsg = "Project _quarto.yml validation failed.";
+      let projectConfig = readYaml(configFile) as ProjectConfig;
+      /*
       let projectConfig = (await readAndValidateYamlFromFile(
         configFile,
         configSchema,
         errMsg,
       )) as ProjectConfig;
+      */
       projectConfig.project = projectConfig.project || {};
       const includedMeta = await includedMetadata(
         dir,
