b4070316e217bd6e1f084699a944aef0d29553a4
Charles Teague <cteague@gmail.com>
2022-04-19 18:24:21 -0400

    Listings should always filter inputs

--- src/project/types/website/listing/website-listing-read.ts
+++ src/project/types/website/listing/website-listing-read.ts
@@ -837,9 +837,9 @@ function expandGlob(
     if (Deno.statSync(globOrPathAsPath).isDirectory) {
       return { glob: join(globOrPath, "**"), inputs: true };
     } else {
-      return { glob: globOrPath, inputs: false };
+      return { glob: globOrPath, inputs: true };
     }
   } catch {
-    return { glob: globOrPath, inputs: false };
+    return { glob: globOrPath, inputs: true };
   }
 }
