8c2d86da66a5aeb54cc07db4545d4dcf5dfffb8e
Charles Teague <cteague@gmail.com>
2022-04-20 13:02:18 -0400

    Use getElementById when possible
    Sections with names containing periods such as
    
    `# SDT Kennzahlen für alle VPn berechnen.`
    
    cause problems when used with querySelector. Use getelementByID which is more permissive.

--- src/resources/formats/html/quarto.js
+++ src/resources/formats/html/quarto.js
@@ -59,7 +59,11 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
 
   const sections = tocLinks.map((link) => {
     const target = link.getAttribute("data-scroll-target");
-    return window.document.querySelector(decodeURI(`${target}`));
+    if (target.startsWith("#")) {
+      return window.document.getElementById(decodeURI(`${target}`));
+    } else {
+      return window.document.querySelector(decodeURI(`${target}`));
+    }
   });
 
   const sectionMargin = 200;
