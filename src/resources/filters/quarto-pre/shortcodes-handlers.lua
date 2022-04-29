8e4a282d001def2a2128fc34e48fe469c9300a9c
JJ Allaire <jj@rstudio.com>
2022-04-29 14:33:00 -0400

    scope down the functions available to shortcodes

--- src/resources/filters/quarto-pre/shortcodes-handlers.lua
+++ src/resources/filters/quarto-pre/shortcodes-handlers.lua
@@ -3,20 +3,80 @@
 
 -- handlers process shortcode into either a list of inlines or into a list of blocks
    
-local handlers = setmetatable({}, {__index=_G})
+
+
+local function createHandlerEnv(scriptFile) 
+  return setmetatable({}, {__index= {
+    -- https://www.lua.org/manual/5.3/manual.html#6.1
+    assert = assert,
+    collectgarbage = collectgarbage,
+    dofile = dofile,
+    error = error,
+    getmetatable = getmetatable,
+    ipairs = ipairs,
+    load = load,
+    loadfile = loadfile,
+    next = next,
+    pairs = pairs,
+    pcall = pcall,
+    print = print,
+    rawequal = rawequal,
+    rawget = rawget,
+    rawlen = rawlen,
+    rawset = rawset,
+    select = select,
+    setmetatable = setmetatable,
+    tonumber = tonumber,
+    tostring = tostring,
+    type = type,
+    _VERSION = _VERSION,
+    xpcall = xpcall,
+    coroutine = coroutine,
+    require = require,
+    package = package,
+    string = string,
+    utf8 = utf8,
+    table = table,
+    math = math,
+    io = io,
+    file = file,
+    os = os,
+    debug = debug,
+    -- https://pandoc.org/lua-filters.html
+    FORMAT = FORMAT,
+    PANDOC_READER_OPTIONS = PANDOC_READER_OPTIONS,
+    PANDOC_WRITER_OPTIONS = PANDOC_WRITER_OPTIONS,
+    PANDOC_VERSION = PANDOC_VERSION,
+    PANDOC_API_VERSION = PANDOC_API_VERSION,
+    PANDOC_SCRIPT_FILE = scriptFile,
+    PANDOC_STATE = PANDOC_STATE,
+    pandoc = pandoc,
+    lpeg = lpeg,
+    re = re,
+    -- quarto functions
+    dump = dump,
+  }})
+end
+
+local handlers = {}
 
 function initShortcodeHandlers()
+
   -- user provided handlers
   local shortcodeFiles = pandoc.List(param("shortcodes", {}))
   for _,shortcodeFile in ipairs(shortcodeFiles) do
-    local chunk, err = loadfile(shortcodeFile, "bt", handlers)
+    local env = createHandlerEnv(shortcodeFile)
+    local chunk, err = loadfile(shortcodeFile, "bt", env)
     if not err then
       local result = chunk()
       if result then
-        handlers = setmetatable({}, {__index=_G})
+        tclear(env)
         for k,v in pairs(result) do
-          handlers[k] = v
+          env[k] = v
         end
+      end
+      for k,v in pairs(env) do
+        handlers[k] = v
       end
     else
       error(err)
