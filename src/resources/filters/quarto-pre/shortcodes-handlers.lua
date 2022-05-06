4cfbf7744855ff841d8b4b53e9c116179b5264c6
JJ Allaire <jj@rstudio.com>
2022-05-06 16:04:41 -0400

    provide access to PANDOC_SCRIPT_FILE for shortcodes

--- src/resources/filters/quarto-pre/shortcodes-handlers.lua
+++ src/resources/filters/quarto-pre/shortcodes-handlers.lua
@@ -3,60 +3,62 @@
 
 -- handlers process shortcode into either a list of inlines or into a list of blocks
    
-local shortcodeMetatable = {
-  -- https://www.lua.org/manual/5.3/manual.html#6.1
-  assert = assert,
-  collectgarbage = collectgarbage,
-  dofile = dofile,
-  error = error,
-  getmetatable = getmetatable,
-  ipairs = ipairs,
-  load = load,
-  loadfile = loadfile,
-  next = next,
-  pairs = pairs,
-  pcall = pcall,
-  print = print,
-  rawequal = rawequal,
-  rawget = rawget,
-  rawlen = rawlen,
-  rawset = rawset,
-  select = select,
-  setmetatable = setmetatable,
-  tonumber = tonumber,
-  tostring = tostring,
-  type = type,
-  _VERSION = _VERSION,
-  xpcall = xpcall,
-  coroutine = coroutine,
-  require = require,
-  package = package,
-  string = string,
-  utf8 = utf8,
-  table = table,
-  math = math,
-  io = io,
-  file = file,
-  os = os,
-  debug = debug,
-  -- https://pandoc.org/lua-filters.html
-  FORMAT = FORMAT,
-  PANDOC_READER_OPTIONS = PANDOC_READER_OPTIONS,
-  PANDOC_WRITER_OPTIONS = PANDOC_WRITER_OPTIONS,
-  PANDOC_VERSION = PANDOC_VERSION,
-  PANDOC_API_VERSION = PANDOC_API_VERSION,
-  PANDOC_SCRIPT_FILE = PANDOC_SCRIPT_FILE,
-  PANDOC_STATE = PANDOC_STATE,
-  pandoc = pandoc,
-  lpeg = lpeg,
-  re = re,
-  -- quarto functions
-  quarto = {
-    utils = {
-      dump = dump
+local function shortcodeMetatable(scriptFile) 
+  return {
+    -- https://www.lua.org/manual/5.3/manual.html#6.1
+    assert = assert,
+    collectgarbage = collectgarbage,
+    dofile = dofile,
+    error = error,
+    getmetatable = getmetatable,
+    ipairs = ipairs,
+    load = load,
+    loadfile = loadfile,
+    next = next,
+    pairs = pairs,
+    pcall = pcall,
+    print = print,
+    rawequal = rawequal,
+    rawget = rawget,
+    rawlen = rawlen,
+    rawset = rawset,
+    select = select,
+    setmetatable = setmetatable,
+    tonumber = tonumber,
+    tostring = tostring,
+    type = type,
+    _VERSION = _VERSION,
+    xpcall = xpcall,
+    coroutine = coroutine,
+    require = require,
+    package = package,
+    string = string,
+    utf8 = utf8,
+    table = table,
+    math = math,
+    io = io,
+    file = file,
+    os = os,
+    debug = debug,
+    -- https://pandoc.org/lua-filters.html
+    FORMAT = FORMAT,
+    PANDOC_READER_OPTIONS = PANDOC_READER_OPTIONS,
+    PANDOC_WRITER_OPTIONS = PANDOC_WRITER_OPTIONS,
+    PANDOC_VERSION = PANDOC_VERSION,
+    PANDOC_API_VERSION = PANDOC_API_VERSION,
+    PANDOC_SCRIPT_FILE = scriptFile,
+    PANDOC_STATE = PANDOC_STATE,
+    pandoc = pandoc,
+    lpeg = lpeg,
+    re = re,
+    -- quarto functions
+    quarto = {
+      utils = {
+        dump = dump
+      }
     }
   }
-}
+end
 
 local handlers = {}
 
@@ -65,7 +67,7 @@ function initShortcodeHandlers()
   -- user provided handlers
   local shortcodeFiles = pandoc.List(param("shortcodes", {}))
   for _,shortcodeFile in ipairs(shortcodeFiles) do
-    local env = setmetatable({}, {__index = shortcodeMetatable})
+    local env = setmetatable({}, {__index = shortcodeMetatable(shortcodeFile)})
     local chunk, err = loadfile(shortcodeFile, "bt", env)
     if not err then
       local result = chunk()
