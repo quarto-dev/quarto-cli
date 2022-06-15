5394582a6e0508b9603faa5d89d774de0c3d0a18
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-06-15 14:37:15 -0700

    polyfill replaceAll so that RStudio Desktop preview pane works

--- src/resources/formats/html/mermaid/mermaid-init.js
+++ src/resources/formats/html/mermaid/mermaid-init.js
@@ -1,3 +1,23 @@
+/**
+ * String.prototype.replaceAll() polyfill
+ * https://gomakethings.com/how-to-replace-a-section-of-a-string-with-another-one-with-vanilla-js/
+ * @author Chris Ferdinandi
+ * @license MIT
+ */
+if (!String.prototype.replaceAll) {
+  String.prototype.replaceAll = function (str, newStr) {
+    // If a regex pattern
+    if (
+      Object.prototype.toString.call(str).toLowerCase() === "[object regexp]"
+    ) {
+      return this.replace(str, newStr);
+    }
+
+    // If a string
+    return this.replace(new RegExp(str, "g"), newStr);
+  };
+}
+
 mermaid.initialize({ startOnLoad: false });
 
 const _quartoMermaid = {
