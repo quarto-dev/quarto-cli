43da47f2110307d0937c7101360d8d06109bc4f9
Charles Teague <cteague@gmail.com>
2022-06-16 10:24:20 -0400

    Stub in quarto use command

---
+++ src/command/use/cmd.ts
@@ -0,0 +1,64 @@
+/*
+* cmd.ts
+*
+* Copyright (C) 2021 by RStudio, PBC
+*
+*/
+import { Command } from "cliffy/command/mod.ts";
+import { initYamlIntelligenceResourcesFromFilesystem } from "../../core/schema/utils.ts";
+import { createTempContext } from "../../core/temp.ts";
+
+import { error } from "log/mod.ts";
+import { templateHandler } from "./handlers/template.ts";
+
+export const useCommand = new Command()
+  .hidden()
+  .name("use")
+  .arguments("<type:string> [target:string]")
+  .option(
+    "--no-prompt",
+    "Do not prompt to confirm actions",
+  )
+  .description(
+    "Automate document or project setup tasks.",
+  )
+  .example(
+    "Use a template from Github",
+    "quarto use template <gh-org>/<gh-repo>",
+  )
+  .action(
+    async (options: Options, type: string, target?: string) => {
+      await initYamlIntelligenceResourcesFromFilesystem();
+      const temp = createTempContext();
+      try {
+        const typeHandler = typeHandlers.find((typeHandler) => {
+          return typeHandler.type === type;
+        });
+        if (typeHandler) {
+          typeHandler.handler(options, target);
+        } else {
+          const types = typeHandlers.map((t) => {
+            return `       - ${t.type}`;
+          });
+          error(
+            `Unknown type ${type}. Please use one of the following:\n${
+              types.join("\n")
+            }`,
+          );
+        }
+      } finally {
+        temp.cleanup();
+      }
+    },
+  );
+
+export type Options = {
+  prompt?: boolean;
+};
+
+export interface TypeHandler {
+  type: string;
+  handler: (options: Options, target?: string) => void;
+}
+
+const typeHandlers: TypeHandler[] = [templateHandler];
