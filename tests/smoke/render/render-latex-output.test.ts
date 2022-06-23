292292ec2ea790982d82756860e8d73fdeac2cf9
Charles Teague <cteague@gmail.com>
2022-06-23 16:23:56 -0400

    fix test

--- tests/smoke/render/render-latex-output.test.ts
+++ tests/smoke/render/render-latex-output.test.ts
@@ -18,8 +18,12 @@ renderVerifyLatexOutput(docs("latex-output/figure-div.qmd"), [
 ]);
 
 // All captions should be placed at the bottom
+// Note that the 'D' or '\\' are present after the caption command because one of the
+// test tables purposely msisses the caption handling to be sure that we don't
+// completely destory the document. Be sure that any test document captions that must be
+// moved started with D or have a label.
 renderVerifyLatexOutput(docs("latex-output/table-captions.qmd"), [
   /\\end{document}/,
 ], [
-  /\\begin{longtable}.*\s\\caption/,
+  /\\begin{longtable}.*\s\\caption{[D\\]/,
 ]);
