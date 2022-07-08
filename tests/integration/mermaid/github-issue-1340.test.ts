14d53b29193546b14405cc84f4e0fce5c4d211fd
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-07-08 15:03:11 -0700

    add cleanup

--- tests/integration/mermaid/github-issue-1340.test.ts
+++ tests/integration/mermaid/github-issue-1340.test.ts
@@ -34,9 +34,9 @@ testRender(input, "html", false, [{
 }, {
   name: "manual cleanup for book project",
   verify: (_outputs: ExecuteOutput[]): Promise<void> => {
-    /*Deno.removeSync(output, { recursive: true });
+    Deno.removeSync(output, { recursive: true });
     Deno.removeSync(join(input, ".quarto"), { recursive: true });
-    Deno.removeSync(join(input, ".gitignore"));*/
+    Deno.removeSync(join(input, ".gitignore"));
 
     return Promise.resolve();
   },
