dc37601e74fcd1f754ac440419041d879237db62
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-07-08 14:05:06 -0700

    add failing test

---
+++ tests/integration/mermaid/github-issue-1340.test.ts
@@ -0,0 +1,40 @@
+/*
+* github-issue-1340.test.ts
+*
+* Copyright (C) 2022 by RStudio, PBC
+*
+*/
+
+import { docs, fileLoader, outputForInput } from "../../utils.ts";
+import {
+  ensureHtmlElements,
+  ensureHtmlSelectorSatisfies,
+  verifyPath,
+} from "../../verify.ts";
+import { testRender } from "../../smoke/render/render.ts";
+import { ExecuteOutput } from "../../test.ts";
+import { join } from "path/mod.ts";
+
+const input = docs("bug-repros/issue-1340/");
+const output = join(input, "_book");
+// const output = outputForInput(join(input, "index.qmd"), "html"); <- doesn't work for book projects, right.
+testRender(input, "html", false, [{
+  name: "file exists",
+  verify: (_outputs: ExecuteOutput[]): Promise<void> => {
+    verifyPath(
+      join(
+        output,
+        "index_files",
+        "figure-html",
+        "unnamed-chunk-2-1.png",
+      ),
+    );
+    return Promise.resolve();
+  },
+}, {
+  name: "manual cleanup for book project",
+  verify: (_outputs: ExecuteOutput[]): Promise<void> => {
+    Deno.removeSync(output, { recursive: true });
+    return Promise.resolve();
+  },
+}]);
