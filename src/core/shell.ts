79b7dfc4ca88e55a49d8e9d6020309e606b7d3e6
Charles Teague <charles@rstudio.com>
2022-07-22 13:27:31 -0400

    Add additional windows protection

--- src/core/shell.ts
+++ src/core/shell.ts
@@ -22,24 +22,33 @@ export async function openUrl(url: string) {
   // Because URLs may contain characters like '&' that need to be escaped
   // on Windoww, we need to check whether the url is one of those
   // and use our special windows indirection in that case
-  const safeArgs = requireQuoting([url]);
-  if (safeArgs.status) {
-    const result = await safeWindowsExec(
-      cmd,
-      safeArgs.args,
-      (cmd: string[]) => {
-        return execProcess({
-          cmd,
-          stdout: "piped",
-          stderr: "piped",
-        });
-      },
-    );
-  } else {
-    // The traditional and simple way to run, which usually works
-    if (await which(cmd)) {
-      Deno.run({ cmd: [cmd, url] });
+  if (Deno.build.os === "windows") {
+    const safeArgs = requireQuoting([url]);
+    if (safeArgs.status) {
+      const result = await safeWindowsExec(
+        cmd,
+        safeArgs.args,
+        (cmd: string[]) => {
+          return execProcess({
+            cmd,
+            stdout: "piped",
+            stderr: "piped",
+          });
+        },
+      );
+    } else {
+      // The traditional and simple way to run, which usually works
+      if (await which(cmd)) {
+        Deno.run({ cmd: [cmd, url] });
+      }
     }
+  } else {
+ // The traditional and simple way to run, which always 
+ // works outside of windows
+ if (await which(cmd)) {
+  Deno.run({ cmd: [cmd, url] });
+}
   }
+  
   
 }
