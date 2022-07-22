6f8e66342765ce3c2c4d58b74a096d99dd0a0486
Charles Teague <charles@rstudio.com>
2022-07-22 13:01:06 -0400

    Use safe exec for problematic urls

--- src/core/shell.ts
+++ src/core/shell.ts
@@ -6,6 +6,9 @@
 */
 
 import { which } from "./path.ts";
+import { requireQuoting, safeWindowsExec } from "./windows.ts";
+import { execProcess, ProcessResult } from "./process.ts";
+
 
 export async function openUrl(url: string) {
   const shellOpen = {
@@ -13,8 +16,30 @@ export async function openUrl(url: string) {
     darwin: "open",
     linux: "xdg-open",
   };
+  
   const cmd = shellOpen[Deno.build.os];
-  if (await which(cmd)) {
-    Deno.run({ cmd: [cmd, url] });
+   
+  // Because URLs may contain characters like '&' that need to be escaped
+  // on Windoww, we need to check whether the url is one of those
+  // and use our special windows indirection in that case
+  const safeArgs = requireQuoting([url]);
+  if (safeArgs.status) {
+    const result = await safeWindowsExec(
+      cmd,
+      safeArgs.args,
+      (cmd: string[]) => {
+        return execProcess({
+          cmd,
+          stdout: "piped",
+          stderr: "piped",
+        });
+      },
+    );
+  } else {
+    // The traditional and simple way to run, which usually works
+    if (await which(cmd)) {
+      Deno.run({ cmd: [cmd, url] });
+    }
   }
+  
 }
