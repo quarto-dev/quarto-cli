a18fadedc3eb1ecacd79f92f25043b83e359409e
JJ Allaire <jj@rstudio.com>
2022-08-04 11:37:55 -0400

    use on-disk cache for filtered jupyter notebooks

---
+++ src/core/jupyter/filtered-notebook-cache.ts
@@ -0,0 +1,139 @@
+/*
+* filtered-notebook-cache.ts
+*
+* Copyright (C) 2022 by RStudio, PBC
+*
+*/
+
+import { existsSync } from "fs/exists.ts";
+import { dirname, isAbsolute, join } from "path/mod.ts";
+import { generate as generateUuid } from "uuid/v4.ts";
+
+import { parseShellRunCommand } from "../../core/run/shell.ts";
+import { quartoCacheDir } from "../appdirs.ts";
+import { which } from "../path.ts";
+import { ensureDirSync } from "../../vendor/deno.land/std@0.150.0/fs/ensure_dir.ts";
+
+// get cached copy of filtered notebook
+export async function filteredNotebookFromCache(
+  file: string,
+  filters: string[],
+) {
+  file = normalizeNotebookPath(file);
+  const invalidation = await notebookInvalidation(file, filters);
+  const notebook = getCachedNotebook(file);
+  if (notebook?.invalidation === invalidation) {
+    return Deno.readTextFileSync(notebook.fullPath);
+  } else {
+    return undefined;
+  }
+}
+
+// cache a filtered notebook
+export async function cacheFilteredNotebook(
+  file: string,
+  filters: string[],
+  json: string,
+) {
+  file = normalizeNotebookPath(file);
+  const invalidation = await notebookInvalidation(file, filters);
+  cacheNotebook(file, { invalidation, json });
+}
+
+function normalizeNotebookPath(file: string) {
+  return isAbsolute(file) ? file : join(Deno.cwd(), file);
+}
+
+// generate a notebook invalidation key
+async function notebookInvalidation(file: string, filters: string[]) {
+  return [file, ...(await filterScriptPaths(file, filters || []))].reduce(
+    (hash, file) => {
+      try {
+        return hash + Deno.statSync(file).mtime?.toUTCString();
+      } catch {
+        return hash;
+      }
+    },
+    "",
+  );
+}
+
+// get filesystem paths of filters
+async function filterScriptPaths(file: string, filters: string[]) {
+  const filterPaths: string[] = [];
+  for (const filter of filters) {
+    const args = parseShellRunCommand(filter);
+    const script = args[0];
+    const scriptPath = join(dirname(file), script);
+    if (existsSync(scriptPath)) {
+      filterPaths.push(scriptPath);
+    } else if (isAbsolute(script) && existsSync(script)) {
+      filterPaths.push(script);
+    } else {
+      const scriptPath = await which(script);
+      if (scriptPath && existsSync(scriptPath)) {
+        filterPaths.push(scriptPath);
+      }
+    }
+  }
+  return filterPaths;
+}
+
+type CachedNotebookContents = {
+  invalidation: string;
+  json: string;
+};
+
+function getCachedNotebook(
+  file: string,
+): { invalidation: string; fullPath: string } | undefined {
+  const contents = readNotebookIndex()[file];
+  if (contents) {
+    return {
+      invalidation: contents.invalidation,
+      fullPath: notebookCachePath(contents.path),
+    };
+  }
+}
+
+function cacheNotebook(file: string, contents: CachedNotebookContents) {
+  // either use existing path or create a new (unique) one
+  const index = readNotebookIndex();
+  const path = index[file]?.path || generateUuid();
+
+  // write the notebook json to the cache
+  Deno.writeTextFileSync(notebookCachePath(path), contents.json);
+
+  // update the index
+  index[file] = { invalidation: contents.invalidation, path };
+  writeNotebookIndex(index);
+}
+
+function readNotebookIndex(): Record<
+  string,
+  { invalidation: string; path: string }
+> {
+  const index = notebookIndexFile();
+  if (existsSync(index)) {
+    return JSON.parse(Deno.readTextFileSync(index));
+  }
+
+  return {};
+}
+
+function writeNotebookIndex(
+  index: Record<string, { invalidation: string; path: string }>,
+) {
+  Deno.writeTextFileSync(
+    notebookIndexFile(),
+    JSON.stringify(index, undefined, 2),
+  );
+}
+
+const notebookIndexFile = () => notebookCachePath("INDEX");
+
+function notebookCachePath(file?: string) {
+  const cacheDir = join(quartoCacheDir(), "filtered-notebooks");
+  ensureDirSync(cacheDir);
+  return file ? join(cacheDir, file) : cacheDir;
+}
--- src/core/jupyter/jupyter-filters.ts
+++ src/core/jupyter/jupyter-filters.ts
@@ -13,7 +13,10 @@ import { Format } from "../../config/types.ts";
 import { execProcess } from "../../core/process.ts";
 import { handlerForScript } from "../../core/run/run.ts";
 import { parseShellRunCommand } from "../../core/run/shell.ts";
-import { which } from "../path.ts";
+import {
+  cacheFilteredNotebook,
+  filteredNotebookFromCache,
+} from "./filtered-notebook-cache.ts";
 import { JupyterNotebook } from "./types.ts";
 
 export async function markdownFromNotebookFile(file: string, format?: Format) {
@@ -44,8 +47,7 @@ export async function jupyterNotebookFiltered(
   let json = Deno.readTextFileSync(file);
   if (filters && filters.length > 0) {
     // see if we can satisfy the request from the cache
-    const invalidation = await notebookInvalidation(file, filters);
-    const cached = filteredNotebookFromCache(file, invalidation);
+    const cached = await filteredNotebookFromCache(file, filters);
     if (cached) {
       return cached;
     }
@@ -81,69 +83,9 @@ export async function jupyterNotebookFiltered(
       }
       json = result.stdout || json;
     }
-    cacheFilteredNotebook(file, invalidation, json);
+    await cacheFilteredNotebook(file, filters, json);
     return json;
   } else {
     return json;
   }
-}
-
-// cache of filtered notebooks
-const filteredNotebooks = new Map<
-  string,
-  { invalidation: string; json: string }
->();
-
-// get cached copy of filtered notebook
-function filteredNotebookFromCache(file: string, invalidation: string) {
-  const notebook = filteredNotebooks.get(file);
-  if (notebook?.invalidation === invalidation) {
-    return notebook.json;
-  } else {
-    return undefined;
-  }
-}
-
-// cache a filtered notebook
-function cacheFilteredNotebook(
-  file: string,
-  invalidation: string,
-  json: string,
-) {
-  filteredNotebooks.set(file, { invalidation, json });
-}
-
-// generate a notebook invalidation key
-async function notebookInvalidation(file: string, filters: string[]) {
-  return [file, ...(await filterScriptPaths(file, filters || []))].reduce(
-    (hash, file) => {
-      try {
-        return hash + Deno.statSync(file).mtime?.toUTCString();
-      } catch {
-        return hash;
-      }
-    },
-    "",
-  );
-}
-
-// get filesystem paths of filters
-async function filterScriptPaths(file: string, filters: string[]) {
-  const filterPaths: string[] = [];
-  for (const filter of filters) {
-    const args = parseShellRunCommand(filter);
-    const script = args[0];
-    const scriptPath = join(dirname(file), script);
-    if (existsSync(scriptPath)) {
-      filterPaths.push(scriptPath);
-    } else if (isAbsolute(script) && existsSync(script)) {
-      filterPaths.push(script);
-    } else {
-      const scriptPath = await which(script);
-      if (scriptPath && existsSync(scriptPath)) {
-        filterPaths.push(scriptPath);
-      }
-    }
-  }
-  return filterPaths;
 }
