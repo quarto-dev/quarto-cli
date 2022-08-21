f0ecdc91a59384f565810b05c069d1d53f22156a
Mike Sarahan <msarahan@gmail.com>
2022-08-21 12:41:53 -0500

    add configure script changes after rename

--- configure.cmd
+++ configure.cmd
@@ -1,112 +1,97 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+REM Batch script reminder: variables between % % are evaluated as parse time, not at runtime.
+REM    Variables set dynamically (including being read from a file) should be evaluated
+REM    using ! ! instead of % %. However, this only allows one level of expansion. Do not
+REM    try to create a variable that derives from a derived variable. It will be empty.
+
 REM First Check that Deno isn't running since this causes weird and confusing errors
 tasklist /fi "ImageName eq deno.exe" /fo csv 2>NUL | find /I "deno.exe">NUL
 if "%ERRORLEVEL%"=="0" goto :denoRunning
 
+call package\src\store_win_configuration.bat
+call win_configuration.bat
 
-REM Reads the configuration file line by line
-REM and convert any export statements into set statements
-REM (allows reuse of variables)
-FOR /F "tokens=*" %%A IN (configuration) DO CALL :convertExportToSet %%A 
-
-IF NOT EXIST %QUARTO_PACKAGE_DIR% (
-	MKDIR %QUARTO_PACKAGE_DIR%
-) 
-PUSHD %QUARTO_PACKAGE_DIR%
-
-RMDIR /S /Q %QUARTO_DIST_DIR%
-IF NOT EXIST %QUARTO_DIST_DIR% (
-	MKDIR %QUARTO_DIST_DIR%
-)
-PUSHD %QUARTO_DIST_DIR%
-
-IF NOT EXIST %QUARTO_BIN_DIR% (
-	MKDIR %QUARTO_BIN_DIR% 
-)
-PUSHD %QUARTO_BIN_DIR%
-MKDIR tools
-PUSHD tools
-
-ECHO Bootstrapping Deno...
-ECHO Deno
-REM Download Deno
-SET DENO_FILE="deno-x86_64-pc-windows-msvc.zip"
-SET DENO_URL="https://github.com/denoland/deno/releases/download/%DENO%/%DENO_FILE%"
-CURL --fail -L %DENO_URL% -o %DENO_FILE%
-REM Windows doesn't have unzip installed by default, but starting in Windows 10 build 17063 they did 
-REM include a build in 'tar' command. Windows 10 build 17063 was released in 2017.
-tar -xf %DENO_FILE%
-
-REM If tar failed, try unzipping it.
-IF %ERRORLEVEL% NEQ 0 ( 
-	ECHO tar failed; trying to unzip...
-	unzip %DENO_FILE%	
+if NOT DEFINED QUARTO_VENDOR_BINARIES (
+  set "QUARTO_VENDOR_BINARIES=true"
 )
 
-REM If both failed, exit with error.
-REM These blocks aren't nested because of the way Windows evaluates variables in control blocks;
-REM %ERRORLEVEL% won't update without jumping through more hoops in a nested if.
-IF %ERRORLEVEL% NEQ 0 (
-	ECHO Unable to decompress %DENO_FILE%
-	exit 1
+if "%QUARTO_VENDOR_BINARIES%" == "true" (
+  RMDIR /S /Q "!QUARTO_DIST_PATH!"
+  MKDIR !QUARTO_BIN_PATH!\tools
+  PUSHD !QUARTO_BIN_PATH!\tools
+
+  ECHO Bootstrapping Deno...
+  REM Download Deno
+  SET "DENO_FILE=deno-x86_64-pc-windows-msvc.zip"
+  CURL --fail -L "https://github.com/denoland/deno/releases/download/!DENO!/!DENO_FILE!" -o "!DENO_FILE!"
+  REM Windows doesn't have unzip installed by default, but starting in Windows 10 build 17063 they did 
+  REM include a build in 'tar' command. Windows 10 build 17063 was released in 2017.
+  tar -xf !DENO_FILE!
+
+  REM If tar failed, try unzipping it.
+  IF %ERRORLEVEL% NEQ 0 ( 
+    ECHO tar failed; trying to unzip...
+    unzip !DENO_FILE!	
+  )
+
+  REM If both failed, exit with error.
+  REM These blocks aren't nested because of the way Windows evaluates variables in control blocks;
+  REM %ERRORLEVEL% won't update without jumping through more hoops in a nested if.
+  IF %ERRORLEVEL% NEQ 0 (
+    ECHO Unable to decompress !DENO_FILE!
+    exit 1
+  )
+
+  DEL !DENO_FILE!
+
+  ECHO .
+  REM  Update to deno canary commit if it is set
+  IF DEFINED DENO_CANARY_COMMIT  (
+    deno upgrade --canary --version %DENO_CANARY_COMMIT%
+  )
+
+  SET QUARTO_DENO=!QUARTO_BIN_PATH!\tools\deno.exe
+  POPD
 )
 
-DEL %DENO_FILE%
-
-
-ECHO .
-REM  Update to deno canary commit if it is set
-IF NOT "%DENO_CANARY_COMMIT%"=="" (
-	deno upgrade --canary --version %DENO_CANARY_COMMIT%
-)
+PUSHD !QUARTO_PACKAGE_PATH!\src
+ECHO Configuring Quarto from !cd!
+CALL quarto-bld.cmd configure --log-level info
+echo Configuration done
 
 POPD
 
-SET FINAL_BIN_PATH=%cd%
+if "%CI%" NEQ "true" (
+	echo Revendoring quarto dependencies
 
-POPD
-POPD
-POPD
-
-PUSHD %QUARTO_PACKAGE_DIR%\src
-ECHO Configuring Quarto
-CALL quarto-bld configure --log-level info
-
-POPD
-
-REM TODO: Convert this to .cmd
-REM if [[ "$CI" != "true" && ( ( "./src/import_map.json" -nt "./src/dev_import_map.json" ) || ( "./src/vendor/import_map.json" -nt "./src/dev_import_map.json" ) ) ]]; then
-REM 	echo [Revendoring quarto dependencies]
-REM 
-REM   today=`date +%Y-%m-%d`
-REM 	mv ./src/vendor ./src/vendor-${today}
-REM 	pushd src
-REM 	set +e
-REM 	../package/dist/bin/tools/deno vendor quarto.ts ../tests/test-deps.ts --importmap=./import_map.json
-REM 	return_code="$?"
-REM 	set -e
-REM 	if [[ ${return_code} -ne 0 ]]; then
-REM 	  echo deno vendor failed (likely because of a download error). Please run the configure script again.
-REM 		rm -rf vendor
-REM 		mv vendor-${today} vendor
-REM 		exit 1
-REM 	fi
-REM 	popd
-REM 	./package/dist/bin/tools/deno run --unstable --allow-all --importmap=./src/import_map.json package/src/common/create-dev-import-map.ts
-REM fi
-
-
+  @REM for /F "tokens=2" %%i in ('date /t') do set today=%%i
+	@REM RENAME src\vendor src\vendor-%today%
+	@REM pushd src
+  @REM echo Vendor phase 1
+	@REM %QUARTO_DENO% vendor quarto.ts %QUARTO_ROOT%\tests\test-deps.ts --importmap=import_map.json
+	@REM if ERRORLEVEL NEQ 0 (
+	@REM   popd
+	@REM   echo deno vendor failed (likely because of a download error). Please run the configure script again.
+	@REM 	RMDIR vendor
+	@REM 	RENAME vendor-${today} vendor
+	@REM 	exit 1
+  @REM )
+	@REM popd
+	@REM %QUARTO_DENO% run --unstable --allow-all --importmap=src\import_map.json package\src\common\create-dev-import-map.ts
+)
 
 ECHO Downloading Deno Stdlib
-CALL package\scripts\deno_std\download.bat
+CALL !QUARTO_PACKAGE_PATH!\scripts\deno_std\download.bat
 
 SET QUARTO_DENO_EXTRA_OPTIONS="--reload"
-CALL "%FINAL_BIN_PATH%\quarto" --version
+IF EXIST !QUARTO_BIN_PATH!\quarto.bat (
+  CALL "!QUARTO_BIN_PATH!\quarto" --version
+)
 
 ECHO NOTE: To use quarto please use quarto-cmd (located in this folder) or add the following path to your PATH
-ECHO %FINAL_BIN_PATH%
+ECHO !QUARTO_BIN_PATH!
 
 
 GOTO :eof
@@ -116,18 +101,3 @@ GOTO :eof
 echo Please ensure no instances of `deno.exe` are running before configuring.
 echo (Deno might be running as an LSP if you have Visual Studio Code open.)
 GOTO :eof
-
-REM Reads each line of a file and converts any exports into SETs
-:convertExportToSet
-SET LINE=%*
-SET FIND=export 
-SET REPLACE=
-
-IF "%LINE:~0,7%"=="%FIND%" (
-	CALL SET LINE=%%LINE:!FIND!=!REPLACE!%%
-	CALL SET %%LINE%%
-)
-
-
-
-:eof
--- configure.sh
+++ configure.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-set -e
+set -ex
 source configuration
+
+# Defaults are set in configuration file, but can be overridden here.
+#  We can't put these overrides in the configuration file because it is parsed
+#  very naively on windows.
+export DENO=${DENO_VERSION=$DENO}
+export DENO_DOM=${DENO_DOM_VERSION=$DENO_DOM}
+export PANDOC=${PANDOC_VERSION=$PANDOC}
+export DARTSASS=${DART_SASS_VERSION=$DARTSASS}
+export ESBUILD=${ESBUILD_VERSION=$ESBUILD}
+
+source package/src/set_package_paths.sh
 
 # Selects curl or wget based on availability and https support.
 function download() {
@@ -14,60 +25,64 @@ function download() {
 	fi
 }
 
-# Ensure directory is there for Deno
-echo "Bootstrapping Deno..."
+QUARTO_VENDOR_BINARIES=${QUARTO_VENDOR_BINARIES=true}
 
-pushd $QUARTO_PACKAGE_DIR
-rm -rf $QUARTO_DIST_DIR
+DENO_BIN=${QUARTO_DENO=$QUARTO_BIN_PATH/tools/deno}
 
-if [ ! -d "$QUARTO_DIST_DIR" ]; then
-	mkdir -p $QUARTO_DIST_DIR
+if [[ "${QUARTO_VENDOR_BINARIES}" = "true" ]]; then
+  DENO_VERSION_NO_V=$(echo $DENO | sed 's/v//')
+  if [[ (! -f $DENO_BIN) || $($DENO_BIN --version | grep $DENO_VERSION_NO_V) == "" ]]; then
+    # Ensure directory is there for Deno
+    echo "Bootstrapping Deno..."
+
+    rm -rf $QUARTO_DIST_PATH
+
+    ## Binary Directory
+    mkdir -p "$QUARTO_BIN_PATH/tools"
+    cd $QUARTO_BIN_PATH/tools
+
+    # Download Deno
+    DENOURL=https://github.com/denoland/deno/releases/download
+    if [[ $OSTYPE == 'darwin'* ]]; then
+      DENOFILE=deno-x86_64-apple-darwin.zip
+    else
+      DENOFILE=deno-x86_64-unknown-linux-gnu.zip
+    fi
+    download "$DENOURL/$DENO/$DENOFILE" "$DENOFILE"
+    unzip -o $DENOFILE
+    rm $DENOFILE
+
+    # If a canary commit is provided, upgrade to that
+    if [ ! -z "$DENO_CANARY_COMMIT" ]; then
+      echo [Upgrading Deno to Canary]
+      ./deno upgrade --canary --version $DENO_CANARY_COMMIT
+    fi
+  fi
+  export DENO_BIN_PATH=$QUARTO_BIN_PATH/tools/deno
+else
+  if [ -z "$DENO_BIN_PATH" ]; then
+    echo "DENO_BIN_PATH is not set. You either need to allow QUARTO_VENDOR_BINARIES or set DENO_BIN_PATH to the path of a deno binary."
+    exit 1
+  fi
 fi
-pushd $QUARTO_DIST_DIR
 
-## Binary Directory
-if [ ! -d "$QUARTO_BIN_DIR" ]; then
-	mkdir -p "$QUARTO_BIN_DIR"
-fi
+echo "Downloading Deno Stdlib"
+${QUARTO_PACKAGE_PATH}/scripts/deno_std/download.sh
 
-pushd $QUARTO_BIN_DIR
-
-mkdir tools
-pushd tools
-
-# Download Deno
-DENOURL=https://github.com/denoland/deno/releases/download
-DENOFILE=deno-x86_64-unknown-linux-gnu.zip
-download "$DENOURL/$DENO/$DENOFILE" "$DENOFILE"
-unzip -o $DENOFILE
-rm $DENOFILE
-
-# If a canary commit is provided, upgrade to that
-if [ ! -z "$DENO_CANARY_COMMIT" ]; then
-	echo [Upgrading Deno to Canary]
-	./deno upgrade --canary --version $DENO_CANARY_COMMIT
-fi
-
-popd
-popd
-popd
-popd
-
-pushd $QUARTO_PACKAGE_DIR/src/
+pushd $QUARTO_PACKAGE_PATH/src/
 
 # Run the configure command to bootstrap installation
 ./quarto-bld configure --log-level info
 
-popd
 
-if [[ "$CI" != "true" && ( ( "./src/import_map.json" -nt "./src/dev_import_map.json" ) || ( "./src/vendor/import_map.json" -nt "./src/dev_import_map.json" ) ) ]]; then
+if [[ "$CI" != "true" && ( ( "$QUARTO_SRC_PATH/import_map.json" -nt "$QUARTO_SRC_PATH/dev_import_map.json" ) || ( "$QUARTO_SRC_PATH/vendor/import_map.json" -nt "$QUARTO_SRC_PATH/dev_import_map.json" ) ) ]]; then
 	echo [Revendoring quarto dependencies]
 
+	pushd ${QUARTO_SRC_PATH}
 	today=`date +%Y-%m-%d`
-	mv ./src/vendor ./src/vendor-${today}
-	pushd src
+	mv vendor vendor-${today}
 	set +e
-	../package/dist/bin/tools/deno vendor quarto.ts ../tests/test-deps.ts --importmap=./import_map.json
+	$DENO_BIN_PATH vendor quarto.ts $QUARTO_ROOT/tests/test-deps.ts --importmap=$QUARTO_SRC_PATH/import_map.json
 	return_code="$?"
 	set -e
 	if [[ ${return_code} -ne 0 ]]; then
@@ -78,18 +93,14 @@ if [[ "$CI" != "true" && ( ( "./src/import_map.json" -nt "./src/dev_import_map.j
 	else
 	  rm -rf vendor-${today}
 	fi
-	popd
-	./package/dist/bin/tools/deno run --unstable --allow-all --importmap=./src/import_map.json package/src/common/create-dev-import-map.ts
+	$DENO_BIN_PATH run --unstable --allow-all --importmap=$QUARTO_SRC_PATH/import_map.json $QUARTO_PACKAGE_PATH/src/common/create-dev-import-map.ts
 fi
-
-echo "Downloading Deno Stdlib"
-./package/scripts/deno_std/download.sh
 
 # Run the quarto command with 'reload', which will force the import_map dependencies
 # to be reloaded
 if ! quarto_loc="$(type -p quarto)" || [[ -z $quarto_loc ]]; then
   echo "Quarto symlink doesn't appear to be configured."
-else 
+else
   export QUARTO_DENO_EXTRA_OPTIONS="--reload"
 	quarto --version
 fi
