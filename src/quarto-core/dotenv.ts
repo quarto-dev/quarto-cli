7645776e469438b718d9f5e308e92a1aa3b49021
JJ Allaire <jj@rstudio.com>
2022-09-17 06:44:11 -0400

    environment: use .required rather than .example

--- src/quarto-core/dotenv.ts
+++ src/quarto-core/dotenv.ts
@@ -15,7 +15,7 @@ import { activeProfiles, kQuartoProfile } from "./profile.ts";
 
 const kQuartoEnv = "_environment";
 const kQuartoEnvLocal = `${kQuartoEnv}.local`;
-const kQuartoEnvExample = `${kQuartoEnv}.example`;
+const kQuartoEnvRequired = `${kQuartoEnv}.required`;
 
 // read the QUARTO_PROFILE from dotenv if it's there
 export async function dotenvQuartoProfile(projectDir: string) {
@@ -70,8 +70,8 @@ export async function dotenvSetVariables(projectDir: string) {
   }
 
   // validate against example if it exists
-  const dotenvExample = join(projectDir, kQuartoEnvExample);
-  if (existsSync(dotenvExample)) {
+  const dotenvRequired = join(projectDir, kQuartoEnvRequired);
+  if (existsSync(dotenvRequired)) {
     const definedEnvTempPath = globalTempContext().createFile({
       suffix: ".yml",
     });
@@ -81,7 +81,7 @@ export async function dotenvSetVariables(projectDir: string) {
     );
     await config({
       path: definedEnvTempPath,
-      example: dotenvExample,
+      example: dotenvRequired,
       safe: true,
       allowEmptyValues: true,
     });
