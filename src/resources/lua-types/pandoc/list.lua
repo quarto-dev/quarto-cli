ede5ea21f127bba721ab943cc1e102299af4a285
JJ Allaire <jj@rstudio.com>
2022-09-17 15:55:36 -0400

    some additional list methods

--- src/resources/lua-types/pandoc/list.lua
+++ src/resources/lua-types/pandoc/list.lua
@@ -1,9 +1,7 @@
 ---@meta
 
-
 ---@class pandoc.List
 pandoc.List = {}
-
 
 --[[
  Create a new List 
@@ -42,6 +40,35 @@ function pandoc.List:find(needle, init) end
 
 
 --[[
+Returns the value and index of the first element for which the predicate holds true.
+]]
+---@param pred fun(x: any): boolean # Condition items must satisfy
+---@param init? integer (Optional) Index at which the search is started
+---@return any|nil First item for which `pred` succeeds, or `nil` if no such item exists.
+function pandoc.List:find_if(pred, init) end
+
+
+--[[
+Returns a new list containing all items satisfying a given condition.
+]]
+---@param pred fun(x: any): boolean # Condition items must satisfy
+---@return pandoc.List
+function pandoc.List:filter(pred) end
+
+
+
+--[[
+Checks if the list has an item equal to the given needle.
+]]
+---@param needle any Item to search for
+---@param init? integer (Optional) Index at which the search is started
+---@return boolean  # `true` if a list item is equal to the `needle`, `false` otherwise
+function pandoc.List:includes(needle, init) end
+
+
+
+
+--[[
 Inserts element at end of list
 ]]
 ---@param value any Value to insert into the list
@@ -55,12 +82,6 @@ Inserts element value at position `pos` in list, shifting elements to the next-g
 ---@param value any Value to insert into the list
 function pandoc.List:insert(pos, value) end
 
---[[
-Returns a new list containing all items satisfying a given condition.
-]]
----@param pred fun(x: any): boolean # Condition itemms must satisfy
----@return pandoc.List
-function pandoc.List:filter(pred) end
 
 --[[
 Returns a copy of the current list by applying the given function to all elements.
@@ -68,3 +89,26 @@ Returns a copy of the current list by applying the given function to all element
 ---@param fn fun(x: any): any # Function which is applied to all list items.
 ---@return pandoc.List
 function pandoc.List:map(fn) end
+
+--[[
+Removes the element at position `pos`, returning the value of the removed element.
+]]
+---@param pos? integer Position of the list value that will be removed; defaults to the index of the last element
+---@return any The removed element
+function pandoc.List:remove(pos) end
+
+--[[
+Sorts list elements in a given order, in-place. If comp is given, then it must be a function that receives
+two list elements and returns true when the first element must come before the second in the final order
+(so that, after the sort, i < j implies not comp(list[j],list[i])). If comp is not given, then the standard
+Lua operator < is used instead.
+
+Note that the comp function must define a strict partial order over the elements in the list; that is, it
+must be asymmetric and transitive. Otherwise, no valid sort may be possible.
+
+The sort algorithm is not stable: elements considered equal by the given order may have their relative
+positions changed by the sort.
+]]
+---@param comp? fun(a, b): boolean 
+function pandoc.List:sort(comp) end
+
