3ed633fb79f4af27192cb806fca55070ac18c8ab
JJ Allaire <jj@rstudio.com>
2022-09-21 10:12:01 -0400

    correct newlnes for publish help

--- src/command/publish/cmd.ts
+++ src/command/publish/cmd.ts
@@ -53,8 +53,8 @@ export const publishCommand =
       "Publish a document or project. Available providers include:\n\n" +
         " - Quarto Pub (quarto-pub)\n" +
         " - GitHub Pages (gh-pages)\n" +
-        " - RStudio Connect (connect)\n\n" +
-        " - Netlify (netlify)\n" +
+        " - RStudio Connect (connect)\n" +
+        " - Netlify (netlify)\n\n" +
         "Accounts are configured interactively during publishing.\n" +
         "Manage/remove accounts with: quarto publish accounts",
     )
