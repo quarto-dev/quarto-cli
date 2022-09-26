abf5ed42eeb33160431605457bcb14daa65738d2
JJ Allaire <jj@rstudio.com>
2022-09-26 19:13:51 -0400

    improved figure handling for docusaurus

--- src/resources/extensions/quarto/docusaurus/docusaurus.lua
+++ src/resources/extensions/quarto/docusaurus/docusaurus.lua
@@ -1,9 +1,17 @@
 
 
+-- strip image attributes (which may result from
+-- fig-format: retina) as they will result in an
+-- img tag which won't hit the asset pipeline
+function Image(el)
+  el.attr = pandoc.Attr()
+  return el
+end
+
 -- transform pandoc "title" to docusaures title
 function CodeBlock(el)
   local lang = el.attr.classes[1]
-  local title = el.attr.attributes["title"]
+  local title = el.attr.attributes["filename"] or el.attr.attributes["title"]  
   if lang and title then
     -- docusaures code block attributes don't conform to any syntax
     -- that pandoc natively understands, so return the CodeBlock as
