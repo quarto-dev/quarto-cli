9e6c7a4f9c0ce8bbb05848742ac39d555045964c
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-09-28 16:58:26 -0700

    more cleanup

--- src/resources/pandoc/datadir/_utils.lua
+++ src/resources/pandoc/datadir/_utils.lua
@@ -76,7 +76,7 @@ function tdump (tbl, raw)
       end
 
       if tbl["-is-extended-ast-"] then
-        printInner(typeIndent .. string.format("{ [quarto-extended-ast:%s:%s]%s", tbl["-quarto-internal-type-"], address, endOfOpen))
+        printInner(typeIndent .. string.format("{ [quarto-extended-ast:%s:%s]%s", tbl.t, address, endOfOpen))
       -- elseif tbl["-quarto-internal-type-"] then
       --   printInner(typeIndent .. string.format("{ [quarto-pandoc:%s:%s]%s", tbl["-quarto-internal-type-"], address, endOfOpen))
       elseif tisarray(tbl) then
