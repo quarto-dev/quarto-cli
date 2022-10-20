5e6d6a67ab0accb60e90cc3f3a1ca2666ca1edda
Charles Teague <cteague@gmail.com>
2022-10-20 14:16:52 -0400

    Fix regression caused by listing warnings
    This re-fixes #367
    
    My implementation of warning regarding empty qmd files was way too aggressive, scale it back.

--- src/project/types/website/listing/website-listing-read.ts
+++ src/project/types/website/listing/website-listing-read.ts
@@ -6,7 +6,7 @@
 *
 */
 import { debug, warning } from "log/mod.ts";
-import { basename, dirname, join, relative } from "path/mod.ts";
+import { basename, dirname, extname, join, relative } from "path/mod.ts";
 import { cloneDeep, orderBy } from "../../../../core/lodash.ts";
 import { existsSync } from "fs/mod.ts";
 
@@ -725,84 +725,83 @@ async function listItemFromFile(
   );
 
   const docRawMetadata = target?.markdown.yaml;
-  if (docRawMetadata) {
-    const directoryMetadata = await directoryMetadataForInputFile(
-      project,
-      dirname(input),
-    );
-    const documentMeta = mergeConfigs(
-      directoryMetadata,
-      docRawMetadata,
-    ) as Metadata;
+  const directoryMetadata = await directoryMetadataForInputFile(
+    project,
+    dirname(input),
+  );
+  const documentMeta = mergeConfigs(
+    directoryMetadata,
+    docRawMetadata,
+  ) as Metadata;
 
-    if (documentMeta?.draft) {
-      // This is a draft, don't include it in the listing
-      return undefined;
-    } else {
-      // See if we have a max desc length
-      const maxDescLength = listing[kMaxDescLength] as number ||
-        kDefaultMaxDescLength;
-
-      // Create the item
-      const filename = basename(projectRelativePath);
-      const filemodified = fileModifiedDate(input);
-      const description = documentMeta?.description as string ||
-        documentMeta?.abstract as string ||
-        descriptionPlaceholder(inputTarget?.outputHref, maxDescLength);
-
-      const imageRaw = documentMeta?.image as string ||
-        findPreviewImgMd(target?.markdown.markdown);
-      const image = imageRaw !== undefined
-        ? pathWithForwardSlashes(
-          listingItemHref(imageRaw, dirname(projectRelativePath)),
-        )
-        : undefined;
-
-      const imageAlt = documentMeta?.[kImageAlt] as string | undefined;
-
-      const date = documentMeta?.date
-        ? parsePandocDate(resolveDate(input, documentMeta?.date) as string)
-        : undefined;
-
-      const authors = parseAuthor(documentMeta?.author);
-      const author = authors ? authors.map((auth) => auth.name) : [];
-
-      const readingtime = target?.markdown
-        ? estimateReadingTimeMinutes(target.markdown.markdown)
-        : undefined;
-
-      const categories = documentMeta?.categories
-        ? Array.isArray(documentMeta?.categories)
-          ? documentMeta?.categories
-          : [documentMeta?.categories]
-        : undefined;
-
-      const item: ListingItem = {
-        ...documentMeta,
-        path: `/${projectRelativePath}`,
-        [kFieldTitle]: target?.title,
-        [kFieldDate]: date,
-        [kFieldAuthor]: author,
-        [kFieldCategories]: categories,
-        [kFieldImage]: image,
-        [kFieldImageAlt]: imageAlt,
-        [kFieldDescription]: description,
-        [kFieldFileName]: filename,
-        [kFieldFileModified]: filemodified,
-        [kFieldReadingTime]: readingtime,
-      };
-      return {
-        item,
-        source: target !== undefined
-          ? ListingItemSource.document
-          : ListingItemSource.rawfile,
-      };
-    }
-  } else {
-    warning(
-      `File ${input} was not included in the listing '${listing.id}' because there is no file metadata.`,
-    );
+  if (documentMeta?.draft) {
+    // This is a draft, don't include it in the listing
     return undefined;
+  } else {
+    if (!docRawMetadata && extname(input) === ".qmd") {
+      warning(
+        `File ${input} in the listing '${listing.id}' contains no metadata.`,
+      );
+    }
+
+    // See if we have a max desc length
+    const maxDescLength = listing[kMaxDescLength] as number ||
+      kDefaultMaxDescLength;
+
+    // Create the item
+    const filename = basename(projectRelativePath);
+    const filemodified = fileModifiedDate(input);
+    const description = documentMeta?.description as string ||
+      documentMeta?.abstract as string ||
+      descriptionPlaceholder(inputTarget?.outputHref, maxDescLength);
+
+    const imageRaw = documentMeta?.image as string ||
+      findPreviewImgMd(target?.markdown.markdown);
+    const image = imageRaw !== undefined
+      ? pathWithForwardSlashes(
+        listingItemHref(imageRaw, dirname(projectRelativePath)),
+      )
+      : undefined;
+
+    const imageAlt = documentMeta?.[kImageAlt] as string | undefined;
+
+    const date = documentMeta?.date
+      ? parsePandocDate(resolveDate(input, documentMeta?.date) as string)
+      : undefined;
+
+    const authors = parseAuthor(documentMeta?.author);
+    const author = authors ? authors.map((auth) => auth.name) : [];
+
+    const readingtime = target?.markdown
+      ? estimateReadingTimeMinutes(target.markdown.markdown)
+      : undefined;
+
+    const categories = documentMeta?.categories
+      ? Array.isArray(documentMeta?.categories)
+        ? documentMeta?.categories
+        : [documentMeta?.categories]
+      : undefined;
+
+    const item: ListingItem = {
+      ...documentMeta,
+      path: `/${projectRelativePath}`,
+      [kFieldTitle]: target?.title,
+      [kFieldDate]: date,
+      [kFieldAuthor]: author,
+      [kFieldCategories]: categories,
+      [kFieldImage]: image,
+      [kFieldImageAlt]: imageAlt,
+      [kFieldDescription]: description,
+      [kFieldFileName]: filename,
+      [kFieldFileModified]: filemodified,
+      [kFieldReadingTime]: readingtime,
+    };
+    return {
+      item,
+      source: target !== undefined
+        ? ListingItemSource.document
+        : ListingItemSource.rawfile,
+    };
   }
 }
 
