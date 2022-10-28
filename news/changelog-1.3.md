9062a1f870c7ba5275c1c9428528571f65823e7e
Charles Teague <cteague@gmail.com>
2022-10-28 12:25:45 -0400

    Properly support language fallback for date locale (#3078)
    * soft reset to trigger CI
    
    * clean up commit for main merge
    
    * more cleanup
    
    * make cwd an AbsolutePath, fixes the other --execute-dir issue in command/render/flags.ts
    
    * squish changes
    
    * wip
    
    * meta denormalization
    
    * cleanup and denormalize work
    
    * cleanup
    
    * meta denormalization
    
    * norm and denorm tables
    
    * wip
    
    * wip
    
    * merge
    
    * wip walker progress
    
    * bottomup filtering, now slow :(
    
    * track script file in wrapped scripts
    
    * broken commit for bisecting
    
    * remove bad code
    
    * restore non-debug
    
    * better constructor for Inlines and Blocks
    
    * remove print
    
    * concat overload
    
    * emulate equality
    
    * wip
    
    * wip
    
    * emulate pandoc.utils.type
    
    * ordered list
    
    * make attr a metafield of fixed type
    
    * cleanup
    
    * more cleanup
    
    * more cleanup
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * more cleanup
    
    * partial work
    
    * correct unmeta in the presence of empty objects
    
    * cleanup
    
    * more cleanup
    
    * more cleanup
    
    * more cleanup
    
    * .. you guessed it. cleanup
    
    * filename cleanup
    
    * scope cleanup
    
    * more scope cleanup
    
    * naming cleanup: normalize->emulate, extended node -> custom node
    
    * more scope and pairs/ipairs cleanup
    
    * single main.lua filter
    
    * more naming and file cleanup
    
    * file header cleanup
    
    * attempting to bundle new lua filters
    
    * shuffle global value init to make it work on bundled lua
    
    * move init away from import zone
    
    * more file headers
    
    * MetaBlocks and MetaInlines emulation
    
    * definitionlist emulation, and emulation during loaders
    
    * from_emulated uses regular names for table parts, rows is fixed-type required
    
    * table parts in from_emulated need both names to be supported
    
    * splice inlines and blocks correctly
    
    * type() emulation for emulated nodes
    
    * override utils directly to work with filters that 'require' pandoc.utils
    
    * starting lua emulation test
    
    * more ast nodes for the tests
    
    * handle author and crossref conditions
    
    * test for author processing
    
    * custom node test
    
    * custom writers in node emulation
    
    * cleanup
    
    * add crossref activation test
    
    * let dump peek into emulated nodes again
    
    * init ast handlers in time for custom nodes in our code
    
    * fix comment
    
    * callout is now a custom node
    
    * fix comments and clean
    
    * add customwriter to package-dist, preserve import order
    
    * fix broken git merge
    
    * upgrade to deno 1.26.1
    
    * remove unneeded comments, upgrade deno_std
    
    * new package lock
    
    * revert qualified-path-2 merge
    
    * remove references to stdlib specific versions
    
    * port to cliffy v0.25.2
    
    * explicitly chmod script
    
    * remove references to stdlib specific versions
    
    * don't import from version-specific stdlib
    
    * respect Writer() interface in custom writers
    
    * Add placeholder changelog
    
    * Properly support language fallback for date locale
    
    Co-authored-by: Carlos Scheidegger <carlos.scheidegger@rstudio.com>
    Co-authored-by: Carlos Scheidegger <285675+cscheid@users.noreply.github.com>

--- news/changelog-1.3.md
+++ news/changelog-1.3.md
@@ -1,5 +1,9 @@
 ## Changes
 
+## Dates
+
+- Properly fall back to language only locale when a supported language-region locale isn't available. (#3059)
+
 ## About Pages
 
 - Add support for `image-alt` which provides alternate text for the about page image. (#3010)
--- tests/smoke/render/render-date.test.ts
+++ tests/smoke/render/render-date.test.ts
@@ -11,14 +11,19 @@ import { testRender } from "./render.ts";
 const tests = [
   { input: docs("date/today.qmd"), noMatch: />today</ },
   { input: docs("date/lastmodified.qmd"), noMatch: />last-modified</ },
+  { input: docs("date/fr.qmd"), match: /octobre/, noMatch: /October/ },
+  { input: docs("date/fr-FR.qmd"), match: /octobre/, noMatch: /October/ },
+  { input: docs("date/fr-CA.qmd"), match: /octobre/, noMatch: /October/ },
 ];
 
 tests.forEach((test) => {
   const to = "html";
   const output = outputForInput(test.input, "html");
+
+  const noMatch = test.noMatch ? [test.noMatch] : [];
+  const match = test.match ? [test.match] : [];
+
   testRender(test.input, to, false, [
-    ensureFileRegexMatches(output.outputPath, [], [
-      test.noMatch,
-    ]),
+    ensureFileRegexMatches(output.outputPath, match, noMatch),
   ]);
 });
