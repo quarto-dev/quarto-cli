8ff946e83cdf96a926af537f145c80aef25e70fa
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-10-31 10:43:49 -0700

    rename to use underscore_case

--- tests/docs/smoke-all/2022/09/30/custom-nodes/createcustomnode.lua
+++ tests/docs/smoke-all/2022/09/30/custom-nodes/createcustomnode.lua
@@ -16,7 +16,7 @@ quarto.ast.add_handler({
   -- end
 
   -- the name of the ast node, used as a key in extended ast filter tables
-  astName = "MyCustomNode",
+  ast_name = "MyCustomNode",
 
   -- a function that takes the extended ast node as supplied in user markdown
   -- and returns a new Pandoc node (use quarto.ast.pandoc instead of pandoc if
