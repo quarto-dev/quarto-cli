b96ba512c1bafd5c51127263e58af684ca167312
Charles Teague <cteague@gmail.com>
2022-11-16 09:17:11 -0500

    Add support for echo param in embeds

--- src/core/handlers/embed.ts
+++ src/core/handlers/embed.ts
@@ -23,6 +23,7 @@ interface EmbedHandler {
   name: string;
   handle(
     filename: string,
+    params: Record<string, unknown>,
     handlerContext: LanguageCellHandlerContext,
   ): Promise<{
     handled: boolean;
@@ -34,16 +35,21 @@ interface EmbedHandler {
 const kHandlers: EmbedHandler[] = [
   {
     name: "Jupyter Notebook Embed",
-    handle(filename: string, _handlerContext: LanguageCellHandlerContext) {
+    handle(
+      filename: string,
+      params: Record<string, unknown>,
+      _handlerContext: LanguageCellHandlerContext,
+    ) {
       const markdownFragments: EitherString[] = [];
 
       // Resolve the filename into a path
       const notebookAddress = parseNotebookAddress(filename);
       if (notebookAddress) {
         const placeHolder = notebookMarkdownPlaceholder(filename, {
-          echo: false,
+          echo: params.echo !== undefined ? params.echo as boolean : false,
           warning: false,
           asis: true,
+          eval: false,
         });
 
         markdownFragments.push(placeHolder);
@@ -86,7 +92,11 @@ const embedHandler: LanguageHandler = {
 
     // Go through handlers until one handles the embed by returning markdown
     for (const handler of kHandlers) {
-      const result = await handler.handle(filename, handlerContext);
+      const result = await handler.handle(
+        filename,
+        directive.shortcode.namedParams,
+        handlerContext,
+      );
       if (result.handled) {
         textFragments.push(...result.markdownFragments);
         if (result.resources) {
