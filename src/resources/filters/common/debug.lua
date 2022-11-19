07c2945ab2d32700997b446f673c865eea414a7d
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-11-19 14:36:13 -0500

    shortcode kwargs return Inlines() for everything. be more robust

--- src/resources/filters/common/debug.lua
+++ src/resources/filters/common/debug.lua
@@ -11,7 +11,7 @@ function tdump (tbl, indent, refs)
     return
   end
 
-  if tbl.t then
+  if tbl.t and type(t) == "string" then
     print(string.rep("  ", indent) .. tbl.t)
   end
   local empty = true
