f869918fe2749598d13805d937a95431bb62a99b
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-11-19 14:33:06 -0500

    add support for `kbd` shortcode. Closes #3394

---
+++ src/resources/extensions/quarto/kbd/kbd.lua
@@ -0,0 +1,88 @@
+-- todo: i18n :(
+return {
+  ['kbd'] = function(args, kwargs, meta)
+    local function osname(v)
+      if v == "win" then return "Windows" end
+      if v == "mac" then return "macOS" end
+      if v == "linux" then return "Linux" end       
+    end
+    if quarto.doc.is_format("html:js") then
+      quarto.doc.add_html_dependency({
+        name = 'kbd',
+        scripts = { 'resources/kbd.js' },
+        stylesheets = { 'resources/kbd.css' }
+      })
+      local kwargs_strs = {}
+      for k, v in pairs(kwargs) do
+        table.insert(kwargs_strs, string.format('data-%s="%s"', osname(k), pandoc.utils.stringify(v)))
+      end
+      local kwargs_str = table.concat(kwargs_strs)
+
+      local default_arg_str
+      if #args == 0 then
+        default_arg_str = ""
+      else
+        default_arg_str = pandoc.utils.stringify(args[1])
+      end
+
+      return pandoc.RawInline('html', '<kbd ' .. kwargs_str .. '>' .. default_arg_str .. '</kbd>')
+    else
+      -- example shortcodes
+      -- {{< kbd Shift-Ctrl-P >}}
+      -- {{< kbd Shift-Ctrl-P mac=Shift-Command-P >}}
+      -- {{< kbd mac=Shift-Command-P win=Shift-Control-S linux=Shift-Ctrl-S >}}
+      local result = {};
+      local n_kwargs = 0
+      for k, v in pairs(kwargs) do
+        n_kwargs = n_kwargs + 1
+      end
+      if #args == 1 then
+        table.insert(result, pandoc.Code(pandoc.utils.stringify(args[1])))
+        if n_kwargs > 0 then
+          table.insert(result, pandoc.Str(' ('))
+          for k, v in pairs(kwargs) do
+            table.insert(result, pandoc.Str('on '))
+            table.insert(result, pandoc.Str(osname(k)))
+            table.insert(result, pandoc.Str(': '))
+            table.insert(result, pandoc.Code(pandoc.utils.stringify(v)))
+            n_kwargs = n_kwargs - 1
+            if n_kwargs > 0 then
+              table.insert(result, pandoc.Str('; '))
+            end
+          end
+          table.insert(result, pandoc.Str(')'))
+        end
+      else
+        -- all kwargs
+        if n_kwargs == 0 then
+          error("kbd requires at least one argument")
+        elseif n_kwargs == 1 then
+          for k, v in pairs(kwargs) do
+            result = {
+              pandoc.Code(pandoc.utils.stringify(v)),
+              pandoc.Str(" (on "),
+              pandoc.Str(osname(k)),
+              pandoc.Str(")")
+            }
+          end
+        else
+          for k, v in pairs(kwargs) do
+            table.insert(result, pandoc.Code(pandoc.utils.stringify(v)))
+            table.insert(result, pandoc.Str(' on '))
+            table.insert(result, pandoc.Str(osname(k)))
+            n_kwargs = n_kwargs - 1
+            if n_kwargs > 1 then
+              table.insert(result, pandoc.Str(', '))
+            elseif n_kwargs == 1 then
+              table.insert(result, pandoc.Str(', and '))
+            end
+          end
+        end
+      end
+
+
+      
+      return result
+    end
+  end
+}
