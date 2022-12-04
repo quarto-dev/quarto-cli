8e77e30310d12feae8a1f6902d2ed31193bc1ea0
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-12-04 09:07:04 -0700

    use vendored libraries for deno.ts's quarto run handler

---
+++ tests/smoke/run/stdlib-run-version.test.ts
@@ -0,0 +1,21 @@
+/*
+ * stdlib-run-version.test.ts
+ *
+ * Copyright (C) 2022 Posit Software, PBC
+ *
+ */
+
+import { execProcess } from "../../../src/core/process.ts";
+import { assert } from "testing/asserts.ts";
+import { unitTest } from "../../test.ts";
+
+unitTest("yaml-intelligence-unit-regression", async () => {
+  const result = await execProcess({
+    cmd: [
+      "quarto",
+      "run",
+      "docs/run/test-stdlib.ts",
+    ],
+  });
+  assert(result.success);
+});
