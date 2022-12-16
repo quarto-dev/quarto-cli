45995541b0c11e894f0cee7f2d1ea71a6095bf84
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-12-16 13:48:13 -0700

    handle inner content, pull attrs and classes

--- src/resources/filters/quarto-pre/content-hidden.lua
+++ src/resources/filters/quarto-pre/content-hidden.lua
@@ -8,11 +8,81 @@ local kWhenFormat = "when-format"
 local kUnlessFormat = "unless-format"
 local kWhenProfile = "when-profile"
 local kUnlessProfile = "unless-profile"
+local kConditions = pandoc.List({kWhenFormat, kUnlessFormat, kWhenProfile, kUnlessProfile})
+
+_quarto.ast.add_handler({
+  class_name = { kContentVisible, kContentHidden },
+  
+  ast_name = "ConditionalBlock",
+
+  kind = "Block",
+
+  parse = function(div)
+    local behavior = div.classes:find(kContentVisible) or div.classes:find(kContentHidden)
+    local condition = pandoc.List({})
+    local remaining_attributes = pandoc.List({})
+    for i, v in ipairs(div.attributes) do
+      if kConditions:find(v[1]) ~= nil then
+        condition:insert(v)
+      else
+        remaining_attributes:insert(v)
+      end
+    end
+    div.attributes = remaining_attributes
+    div.classes = div.classes:filter(function(k) return k ~= kContentVisible and k ~= kContentHidden end)
+    return quarto.ConditionalBlock({
+      node = div,
+      behavior = behavior,
+      condition = condition
+    })
+  end,
+
+  render = function(node)
+    local el = node.node
+    local profiles = pandoc.List(param("quarto_profile", {}))
+    if node.behavior == kContentVisible then
+      return handleVisible(el, profiles)
+    elseif node.behavior == kContentHidden then 
+      return handleHidden(el, profiles)
+    else
+      return el
+    end
+  end,
+
+  constructor = function(tbl)
+    local result = {
+      node = tbl.node,
+      behavior = tbl.behavior,
+      condition = pandoc.List({})
+    };
+    for i, v in ipairs(tbl.condition) do
+      if kConditions:find(v[1]) == nil then
+        error("Ignoring invalid condition in conditional block: " .. v[1])
+      else
+        result.condition:insert(v)
+      end
+    end
+
+    return tbl
+  end,
+
+  inner_content = function(tbl)
+    return {
+      content = tbl.node.content,
+    }
+  end,
+
+  set_inner_content = function(tbl, content)
+    if content.content ~= nil then
+      tbl.node.content = content.content
+    end
+  end
+})
 
 function contentHidden()
   local profiles = pandoc.List(param("quarto_profile", {}))
   return {
-    Div = handleHiddenVisible(profiles),
+    -- Div = handleHiddenVisible(profiles),
     CodeBlock = handleHiddenVisible(profiles),
     Span = handleHiddenVisible(profiles)
   }
