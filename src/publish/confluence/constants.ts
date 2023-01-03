a31ebc6a2a46d5eb4e58538ce7f2255a449ec6e0
Al Manning <al.manning@rstudio.com>
2023-01-03 14:07:47 +0100

    add in deleting

--- src/publish/confluence/constants.ts
+++ src/publish/confluence/constants.ts
@@ -1,4 +1,4 @@
-export const DELETE_DISABLED = true;
+export const DELETE_DISABLED = false;
 
 export const DESCENDANT_LIMIT = 500; // Render time would be the big worry here, we can consider paging in the future
 
