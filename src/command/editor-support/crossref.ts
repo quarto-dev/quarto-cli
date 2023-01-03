5dad8f8e2bc96bb2074268fd978fa41730e309e8
J.J. Allaire <jj.allaire@gmail.com>
2023-01-03 07:37:57 -0500

    use direct to pandoc crossref index interface

--- src/command/editor-support/crossref.ts
+++ src/command/editor-support/crossref.ts
@@ -8,46 +8,72 @@
 import { join } from "path/posix.ts";
 import { readAll } from "streams/mod.ts";
 import { error } from "log/mod.ts";
+import { encode as base64Encode } from "encoding/base64.ts";
 
 import { Command } from "cliffy/command/mod.ts";
 
+import { execProcess } from "../../core/process.ts";
+import { pandocBinaryPath, resourcePath } from "../../core/resources.ts";
+import { globalTempContext } from "../../core/temp.ts";
 import { exitWithCleanup } from "../../core/cleanup.ts";
-import { render } from "../render/render-shared.ts";
-import { renderServices } from "../render/render-shared.ts";
 
 export const crossrefCommand = new Command()
   .description("Index cross references for content")
   .action(async () => {
     // read input
-    const input = await readAll(Deno.stdin);
-
-    // get render services
-    const services = renderServices();
+    const stdinContent = await readAll(Deno.stdin);
+    const input = new TextDecoder().decode(stdinContent);
 
     // create directory for indexing and write input into it
-    const indexingDir = services.temp.createDir();
-    const inputFile = join(indexingDir, "input.qmd");
-    Deno.writeFile(inputFile, input);
+    const indexingDir = globalTempContext().createDir();
 
     // setup index file and input type
-    const indexFile = "index.json";
+    const indexFile = join(indexingDir, "index.json");
     Deno.env.set("QUARTO_CROSSREF_INDEX_PATH", indexFile);
     Deno.env.set("QUARTO_CROSSREF_INPUT_TYPE", "qmd");
 
-    // render
-    const result = await render(inputFile, {
-      services,
-      progress: false,
-      flags: {
-        quiet: true,
-      },
-    });
+    // build command
+    const cmd = [pandocBinaryPath(), "+RTS", "-K512m", "-RTS"];
+    cmd.push(...[
+      "--from",
+      "markdown",
+      "--to",
+      "native",
+      "--data-dir",
+      resourcePath("pandoc/datadir"),
+      "--lua-filter",
+      resourcePath("filters/quarto-init/quarto-init.lua"),
+      "--lua-filter",
+      resourcePath("filters/crossref/crossref.lua"),
+    ]);
 
-    if (result.error) {
-      error("Error running Pandoc: " + result.error.message);
+    // create filter params
+    const filterParams = base64Encode(
+      JSON.stringify({
+        ["crossref-index-file"]: "index.json",
+        ["crossref-input-type"]: "qmd",
+      }),
+    );
+
+    // run pandoc
+    const result = await execProcess(
+      {
+        cmd,
+        cwd: indexingDir,
+        env: {
+          "QUARTO_FILTER_PARAMS": filterParams,
+          "QUARTO_SHARE_PATH": resourcePath(),
+        },
+      },
+      input,
+    );
+
+    // check for error
+    if (!result.success) {
+      error("Error running Pandoc: " + result.stderr);
       exitWithCleanup(1);
     }
 
     // write back the index
-    Deno.stdout.writeSync(Deno.readFileSync(join(indexingDir, indexFile)));
+    Deno.stdout.writeSync(Deno.readFileSync(indexFile));
   });
