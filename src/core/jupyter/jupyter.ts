36ad00d65d3091ffc2e63c48d7d6288bca8ce267
Charles Teague <cteague@gmail.com>
2023-01-12 15:00:14 -0500

    Ensure that cell id makes it through pandoc rendering

--- src/core/jupyter/jupyter.ts
+++ src/core/jupyter/jupyter.ts
@@ -1096,6 +1096,11 @@ async function mdFromCodeCell(
   // write div enclosure
   const divMd: string[] = [`::: {`];
 
+  // add the id
+  if (cell.id) {
+    divMd.push(`#${cell.id} `);
+  }
+
   // metadata to exclude from cell div attributes
   const kCellOptionsFilter = kJupyterCellInternalOptionKeys.concat(
     kJupyterCellStandardMetadataKeys,
