033860d4857fab683cb629d701b87223657825ee
Charles Teague <cteague@gmail.com>
2023-01-18 15:31:29 -0500

    add another book test

--- tests/smoke/book/render-book.test.ts
+++ tests/smoke/book/render-book.test.ts
@@ -5,9 +5,8 @@ import { existsSync } from "fs/mod.ts";
 import { join } from "path/mod.ts";
 import { docs } from "../../utils.ts";
 
+// Test a simple book
 const input = docs("books/simple");
-
-// Run the command
 testQuartoCmd(
   "render",
   [input],
@@ -15,6 +14,22 @@ testQuartoCmd(
   {
     teardown: async () => {
       const bookDir = join(input, "_book");
+      if (existsSync(bookDir)) {
+        await Deno.remove(bookDir, { recursive: true });
+      }
+    },
+  },
+);
+
+// Test a more complex book render
+const vizInput = docs("books/visualization-curriculum");
+testQuartoCmd(
+  "render",
+  [vizInput],
+  [noErrorsOrWarnings],
+  {
+    teardown: async () => {
+      const bookDir = join(vizInput, "docs");
       if (existsSync(bookDir)) {
         await Deno.remove(bookDir, { recursive: true });
       }
