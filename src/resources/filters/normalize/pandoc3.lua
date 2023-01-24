d10c98350381f3e989f160ac61bf59d9a77e5c77
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-01-24 08:49:44 -0700

    normalize fig->image attributes correctly

--- src/resources/filters/normalize/pandoc3.lua
+++ src/resources/filters/normalize/pandoc3.lua
@@ -7,7 +7,13 @@ function parse_pandoc3_figures()
       if (#fig.content == 1 and fig.content[1].t == "Plain" and 
           #fig.content[1].content == 1 and fig.content[1].content[1].t == "Image") then
         local image = fig.content[1].content[1]
-        image.attr = fig.attr
+        image.classes:extend(fig.classes)
+        for k, v in pairs(fig.attributes) do
+          image.attributes[k] = v
+        end
+        if fig.identifier ~= "" then
+          image.identifier = fig.identifier
+        end
         
         return pandoc.Para(image)
       else
