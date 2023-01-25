8d261a5981a1c8f8019477f028e3edbbf2e8b52e
Carlos Scheidegger <285675+cscheid@users.noreply.github.com>
2023-01-25 09:20:43 -0700

    Bugfix/issue 4073 (#4088)
    * support more html content where a table is present
    * fix falsy check

--- src/resources/filters/normalize/parsehtml.lua
+++ src/resources/filters/normalize/parsehtml.lua
@@ -9,8 +9,16 @@ function parse_html_tables()
       if _quarto.format.isRawHtml(el) then
         -- if we have a raw html table in a format that doesn't handle raw_html
         -- then have pandoc parse the table into a proper AST table block
-        local tableBegin,tableBody,tableEnd = el.text:match(htmlTablePattern())
+        local pat = htmlTablePattern()
+        local i, j = string.find(el.text, pat)
+        if i == nil then
+          return nil
+        end
+
+        local tableBegin,tableBody,tableEnd = el.text:match(pat)
         if tableBegin then
+          local before_table = string.sub(el.text, 1, i - 1)
+          local after_table = string.sub(el.text, j + 1)
           local tableHtml = tableBegin .. "\n" .. tableBody .. "\n" .. tableEnd
           local tableDoc = pandoc.read(tableHtml, "html")
           local skip = false
@@ -24,7 +32,15 @@ function parse_html_tables()
           if skip then
             return nil
           end
-          return tableDoc.blocks
+          local blocks = pandoc.Blocks({})
+          if before_table ~= "" then
+            blocks:insert(pandoc.RawBlock(el.format, before_table))
+          end
+          blocks:extend(tableDoc.blocks)
+          if after_table ~= "" then
+            blocks:insert(pandoc.RawBlock(el.format, after_table))
+          end
+          return blocks
         end
       end
       return el
