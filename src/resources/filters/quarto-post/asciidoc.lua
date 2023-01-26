1f745f6d4ab4983ed9fd39ffd54b512f68e9edac
Charles Teague <cteague@gmail.com>
2023-01-26 11:54:20 -0500

    Asccidoc callout / admonition support

--- src/resources/filters/quarto-post/asciidoc.lua
+++ src/resources/filters/quarto-post/asciidoc.lua
@@ -14,6 +14,50 @@ function asciidoc()
         end)
         return pandoc.RawInline("asciidoc", citesStr);
       end
+    end,
+    Callout = function(el) 
+      if quarto.doc.isFormat("asciidoc") then
+
+        -- types map cleanly
+        local admonitionType = el.type:upper();
+        local admonitionCaption = el.caption;
+        local admonitionContents = el.content;
+
+        if admonitionCaption then
+
+          -- a captioned admonition
+          admonitionCaption:insert(1, pandoc.RawInline("asciidoc", ".") )
+          admonitionCaption:insert(1, pandoc.RawInline("asciidoc", "[" .. admonitionType .. "]\n"))
+          admonitionCaption:insert(pandoc.RawInline("asciidoc", "\n====\n"))
+          tprepend(admonitionContents, {admonitionCaption})
+                    
+          tappend(admonitionContents, {pandoc.RawInline("asciidoc", "====")})
+
+        else
+          -- A captionless admonition
+          if #admonitionContents == 1 then
+
+            -- If there is only a single child, we can use a simple decoration of that child
+            local typePrefix = {pandoc.RawInline("asciidoc", admonitionType .. ": ") };
+            if admonitionContents[1].content then
+              tprepend(admonitionContents[1].content, typePrefix)
+            else
+              tprepend(admonitionContents, typePrefix)
+            end
+          else
+
+            -- If there are more then one child, we need to wrap all the children
+            -- with the more complex syntax
+            tprepend(admonitionContents[1].content, {pandoc.RawInline("asciidoc", "====\n")})
+            tprepend(admonitionContents[1].content, {pandoc.RawInline("asciidoc", "[" .. admonitionType .. "]\n")})
+            tappend(admonitionContents[#admonitionContents].content, {pandoc.RawInline("asciidoc", "\n====")})
+          end
+
+        end
+
+        local admonition = pandoc.Div(admonitionContents);
+        return admonition
+      end
     end
   }
 end
