7ae48caf853a450227143a5676e884b90ed2d50b
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-01-26 11:40:40 +0100

    Correctly put quarto to PATH for Unix and Windows

--- configure.cmd
+++ configure.cmd
@@ -95,6 +95,7 @@ IF EXIST !QUARTO_BIN_PATH!\quarto.cmd (
 ECHO NOTE: To use quarto please use quarto.cmd (located in this folder) or add the following path to your PATH
 ECHO !QUARTO_BIN_PATH!
 
+endlocal & set QUARTO_BIN_DEV=%QUARTO_BIN_PATH%
 
 GOTO :eof
 
