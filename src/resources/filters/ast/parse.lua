535442d9fdaab0b7033665c024154abccc7751ab
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-02-01 15:52:42 -0800

    partial work

--- src/resources/filters/ast/parse.lua
+++ src/resources/filters/ast/parse.lua
@@ -4,12 +4,14 @@
 -- Copyright (C) 2022 by RStudio, PBC
 
 local function parse(node)
-  local tag = pandoc.utils.stringify(node.attr.classes)
-  local handler = _quarto.ast.resolve_handler(tag)
-  if handler == nil then
-    return node
+  for _, class in ipairs(node.attr.classes) do
+    local tag = pandoc.utils.stringify(class)
+    local handler = _quarto.ast.resolve_handler(tag)
+    if handler ~= nil then
+      return handler.parse(node)
+    end
   end
-  return handler.parse(node)
+  return node
 end
 
 function parseExtendedNodes() 
