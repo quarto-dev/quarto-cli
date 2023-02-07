8534c21fcdad3203393e9e6596d3c12521fe0e14
Charles Teague <cteague@gmail.com>
2023-02-07 13:59:41 -0500

    add hack

--- configure.sh
+++ configure.sh
@@ -47,6 +47,10 @@ if [[ "${QUARTO_VENDOR_BINARIES}" = "true" ]]; then
       download "$DENOURL/$DENO/$DENOFILE" "$DENOFILE"
       unzip -o $DENOFILE
       DENO_ARCH_DIR=$(basename $DENOFILE .zip)
+      if [[ "${DENO_ARCH_DIR}" = "deno-linux-arm64"]]; then
+        DENO_ARCH_DIR="deno-aarch64-unknown-linux-gnu"
+      fi
+      
       mkdir $DENO_ARCH_DIR
       mv deno $DENO_ARCH_DIR
       rm $DENOFILE
