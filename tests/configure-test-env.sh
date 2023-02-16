dabc2533c6fd77afec80c3fb53efa64ac99cb8d4
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-02-16 16:55:40 +0100

    [test environment] Don't use which to find if a command is available

--- tests/configure-test-env.sh
+++ tests/configure-test-env.sh
@@ -1,7 +1,7 @@
 # Check R environment ---
 
 echo ">>>> Configuring R environment"
-r_exists=$(which Rscript)
+r_exists=$(command -v Rscript)
 
 if [ -z $r_exists ] 
 then 
@@ -13,25 +13,25 @@ fi
 
 # Check python test environment ---
 echo ">>>> Configuring Python environment"
-python_exists=$(which python)
+python_exists=$(command -v python)
 if [ -z $python_exists ] 
 then 
   echo "No python found in PATH - Install python."
 else
-  pipenv_exist=$(which pipenv)
+  pipenv_exist=$(command -v pipenv)
   if [ -z $pipenv_exist ] 
   then
     python -m pip install pipenv
   fi
   # specific for pyenv shim
-  [ -n $(which pyenv) ] && pyenv rehash
+  [ -n $(command -v pyenv) ] && pyenv rehash
   echo "Setting up python environnement with pipenv"
   pipenv install
 fi
 
 # Check Julia environment ---
 echo ">>>> Configuring Julia environment"
-julia_exists=$(which julia)
+julia_exists=$(command -v julia)
 if [ -z $julia_exists ] 
 then 
   echo "No julia found in PATH - Install Julia."
@@ -42,13 +42,13 @@ fi
 
 # Update tinytex
 echo ">>>> Configuring TinyTeX environment"
-if [ -z $GH_TOKEN ] && [ -n $(which gh) ]
+if [ -z $GH_TOKEN ] && [ -n $(command -v gh) ]
 then 
   echo "Setting GH_TOKEN env var for Github Download."
   export GH_TOKEN=$(gh auth token)
 fi
 
-if [ -n $(which quarto) ] 
+if [ -n $(command -v quarto) ] 
 then
   quarto install tinytex
 fi
