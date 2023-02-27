2e20f6340d3850406feaa15b3c1d568c59032a6b
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-02-27 14:09:14 -0700

    'rebreak' branch by reintroducing puppeteer first

--- src/core/puppeteer.ts
+++ src/core/puppeteer.ts
@@ -5,4 +5,273 @@
 *
 */
 
-// temporarily removed until we get things in order in deno 1.28.*
+import { readRegistryKey } from "./windows.ts";
+import { which } from "./path.ts";
+import { error, info } from "log/mod.ts";
+import { fetcher } from "../tools/impl/chromium.ts";
+import { existsSync } from "fs/mod.ts";
+
+// deno-lint-ignore no-explicit-any
+let puppeteerImport: any = undefined;
+// deno-lint-ignore prefer-const
+let puppeteerUrl = "puppeteer/mod.ts";
+export async function getPuppeteer() {
+  if (puppeteerImport !== undefined) {
+    return puppeteerImport;
+  }
+  puppeteerImport = (await import(puppeteerUrl)).default;
+  return puppeteerImport;
+}
+
+/**
+ * Extracts images and elements from an URL
+ *
+ * @param urlOrOptions webpage url or page options
+ * @param selector css selector
+ * @param filenames filenames to write screenshots to
+ * @returns html content of selected results
+ */
+export async function extractImagesFromElements(
+  urlOrOptions: string | PageOptions,
+  selector: string,
+  filenames: string[],
+): Promise<string[]> {
+  return await withPuppeteerBrowserAndPage(
+    urlOrOptions,
+    // deno-lint-ignore no-explicit-any
+    async (_browser: any, page: any) => {
+      const elements = await page.$$(selector);
+      if (elements.length !== filenames.length) {
+        throw new Error(
+          `extractImagesFromElements was given ${filenames.length} filenames, but selector yielded ${elements.length} elements.`,
+        );
+      }
+      for (let i = 0; i < elements.length; ++i) {
+        await elements[i].screenshot({ path: filenames[i] });
+      }
+
+      // deno-lint-ignore no-explicit-any
+      const document = (undefined as any);
+      const clientSideResult = await page.evaluate((selector: string) => {
+        // deno-lint-ignore no-explicit-any
+        return Array.from(document.querySelectorAll(selector)).map((n: any) =>
+          n.outerHTML
+        );
+      }, selector);
+      return clientSideResult;
+    },
+  );
+}
+
+export function extractHtmlFromElements(
+  url: string,
+  selector: string,
+): Promise<string[]> {
+  // deno-lint-ignore no-explicit-any
+  const document = (undefined as any);
+  return inPuppeteer(url, (selector: string) => {
+    // deno-lint-ignore no-explicit-any
+    return Array.from(document.querySelectorAll(selector)).map((n: any) =>
+      n.outerHTML
+    );
+  }, selector);
+}
+
+export interface PageOptions {
+  url: string;
+  viewport?: {
+    // https://github.com/puppeteer/puppeteer/blob/v0.12.0/docs/api.md#pagesetviewportviewport
+    width: number;
+    height: number;
+    deviceScaleFactor?: number;
+    isMobile?: boolean;
+    hasTouch?: boolean;
+    isLandscape?: boolean;
+  };
+}
+export async function withPuppeteerBrowserAndPage<T>(
+  url: string | PageOptions,
+  // deno-lint-ignore no-explicit-any
+  f: (b: any, p: any) => Promise<T>,
+): Promise<T> {
+  const allowedErrorMessages = [
+    "Navigation failed because browser has disconnected!",
+    "Navigation timeout of 30000 ms exceeded",
+    "Evaluation failed: undefined",
+  ];
+
+  let attempts = 0;
+  const maxAttempts = 5;
+  while (attempts++ < maxAttempts) {
+    try {
+      let finished = false;
+      let result: T;
+      // deno-lint-ignore no-explicit-any
+      await withHeadlessBrowser(async (browser: any) => {
+        const page = await browser.newPage();
+        if (typeof url === "string") {
+          await page.goto(url);
+        } else {
+          if (url.viewport) {
+            page.setViewport(url.viewport);
+          }
+          await page.goto(url.url);
+        }
+        result = await f(browser, page);
+        finished = true;
+      });
+      if (finished) {
+        return result!;
+      }
+    } catch (error) {
+      if (
+        (allowedErrorMessages.indexOf(error.message) !== -1) &&
+        (attempts < maxAttempts)
+      ) {
+        console.log(
+          `\nEncountered a bad error message from puppeteer: "${error.message}"\n Retrying ${attempts}/${maxAttempts}`,
+        );
+      } else {
+        throw error;
+      }
+    }
+  }
+  throw new Error("Internal Error - shouldn't have arrived here.");
+}
+
+export async function inPuppeteer(
+  url: string,
+  // deno-lint-ignore no-explicit-any
+  f: any,
+  // deno-lint-ignore no-explicit-any
+  ...params: any[]
+  // deno-lint-ignore no-explicit-any
+): Promise<any> {
+  const allowedErrorMessages = [
+    "Navigation failed because browser has disconnected!",
+    "Navigation timeout of 30000 ms exceeded",
+    "Evaluation failed: undefined",
+  ];
+
+  let attempts = 0;
+  const maxAttempts = 5;
+  while (attempts++ < maxAttempts) {
+    try {
+      // deno-lint-ignore no-explicit-any
+      return await withHeadlessBrowser(async (browser: any) => {
+        const page = await browser.newPage();
+        await page.goto(url);
+        const clientSideResult = await page.evaluate(f, ...params);
+        return clientSideResult;
+      });
+    } catch (error) {
+      if (
+        (allowedErrorMessages.indexOf(error.message) !== -1) &&
+        (attempts < maxAttempts)
+      ) {
+        console.log(
+          `\nEncountered a bad error message from puppeteer: "${error.message}"\n Retrying ${attempts}/${maxAttempts}`,
+        );
+      } else {
+        throw error;
+      }
+    }
+  }
+  throw new Error("Internal Error - shouldn't have arrived here.");
+}
+
+export async function withHeadlessBrowser<T>(
+  // deno-lint-ignore no-explicit-any
+  fn: (browser: any) => Promise<T>,
+) {
+  const browser = await fetchBrowser();
+  if (browser !== undefined) {
+    try {
+      const result = await fn(browser);
+      await browser.close();
+      return result;
+    } catch (e) {
+      // we can't try ... finally here because it plays badly with async
+      // and return values.
+      await browser.close();
+      throw e;
+    }
+  }
+}
+
+async function findChrome(): Promise<string | undefined> {
+  let path;
+  if (Deno.build.os === "darwin") {
+    path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+  } else if (Deno.build.os === "linux") {
+    path = await which("google-chrome");
+    if (!path) {
+      path = await which("chromium-browser");
+    }
+  } else if (Deno.build.os === "windows") {
+    // Try the HKLM key
+    const programs = ["chrome.exe", "msedge.exe"];
+    for (let i = 0; i < programs.length; i++) {
+      path = await readRegistryKey(
+        "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\" +
+          programs[i],
+        "(Default)",
+      );
+      if (path && existsSync(path)) break;
+    }
+
+    // Try the HKCR key
+    if (!path) {
+      const regKeys = ["ChromeHTML", "MSEdgeHTM"];
+      for (let i = 0; i < regKeys.length; i++) {
+        path = await readRegistryKey(
+          `HKCR\\${regKeys[i]}\\shell\\open\\command`,
+          "(Default)",
+        );
+        path = path?.match(/"(.*)"/);
+        path = path ? path[1] : undefined;
+        if (path && existsSync(path)) break;
+      }
+    }
+  }
+  return path;
+}
+
+export async function getBrowserExecutablePath() {
+  // Cook up a new instance
+  const browserFetcher = await fetcher();
+  const availableRevisions = await browserFetcher.localRevisions();
+
+  let executablePath: string | undefined = undefined;
+
+  if (availableRevisions.length > 0) {
+    // get the latest available revision
+    availableRevisions.sort((a: string, b: string) => Number(b) - Number(a));
+    const revision = availableRevisions[0];
+    const revisionInfo = browserFetcher.revisionInfo(revision);
+    executablePath = revisionInfo.executablePath;
+  }
+
+  if (executablePath === undefined) {
+    executablePath = await findChrome();
+  }
+
+  if (executablePath === undefined) {
+    error("Chrome not found");
+    info(
+      "\nNo Chrome or Chromium installation was detected.\n\nPlease run 'quarto tools install chromium' to install Chromium.\n",
+    );
+    throw new Error();
+  }
+
+  return executablePath;
+}
+
+async function fetchBrowser() {
+  const executablePath = await getBrowserExecutablePath();
+  const puppeteer = await getPuppeteer();
+  return await puppeteer.launch({
+    product: "chrome",
+    executablePath,
+  });
+}
--- src/tools/impl/chromium.ts
+++ src/tools/impl/chromium.ts
@@ -5,4 +5,130 @@
  *
  */
 
-// temporarily removed until we get things in order in deno 1.28.*
+import { getPuppeteer } from "../../core/puppeteer.ts";
+import {
+  clearLine,
+  progressBar,
+  spinner,
+  withSpinner,
+} from "../../core/console.ts";
+
+import { InstallableTool, InstallContext, PackageInfo } from "../types.ts";
+import { quartoDataDir } from "../../core/appdirs.ts";
+
+export function chromiumInstallDir(): string | undefined {
+  return quartoDataDir("chromium");
+}
+
+async function installDir() {
+  if (await installed()) {
+    return Promise.resolve(chromiumInstallDir());
+  } else {
+    return Promise.resolve(undefined);
+  }
+}
+
+export const chromiumInstallable: InstallableTool = {
+  name: "Chromium",
+  prereqs: [],
+  installed,
+  installedVersion,
+  latestRelease,
+  preparePackage,
+  install,
+  afterInstall,
+  uninstall,
+  installDir,
+};
+
+export async function installed(): Promise<boolean> {
+  const fetcherObj = await fetcher();
+  const version = await supportedRevision();
+  const localRevisions = await fetcherObj.localRevisions();
+  return localRevisions.includes(version);
+}
+
+async function installedVersion(): Promise<string | undefined> {
+  const fetcherObj = await fetcher();
+  const localRevisions = await fetcherObj.localRevisions();
+  if (localRevisions.length) {
+    return localRevisions[localRevisions.length - 1];
+  }
+}
+
+async function latestRelease() {
+  const fetcherObj = await fetcher();
+  const version = await supportedRevision();
+  const revisionInfo = await fetcherObj.revisionInfo(version);
+
+  return Promise.resolve({
+    url: revisionInfo.url,
+    version,
+    assets: [{ name: "", url: "" }],
+  });
+}
+
+async function preparePackage(_ctx: InstallContext): Promise<PackageInfo> {
+  const revision = await supportedRevision();
+
+  const progress = progressBar(
+    100,
+    `Downloading Chromium ${revision}`,
+  );
+  //  const spin = spinner("Installing");
+  let spinnerStatus: ((() => void) | undefined);
+  const fetcherObj = await fetcher();
+  const revisionInfo = await fetcherObj.download(
+    revision,
+    (x: number, total: number) => {
+      const percent = x / total * 100;
+      if (percent < 100) {
+        progress.update(percent);
+      } else {
+        progress.complete(true);
+        clearLine();
+        spinnerStatus = spinner(`Installing Chromium ${revision}`);
+      }
+    },
+  );
+  if (spinnerStatus !== undefined) {
+    spinnerStatus();
+  }
+
+  return {
+    filePath: revisionInfo.folderPath,
+    version: revisionInfo.revision,
+  };
+}
+
+function install(_pkg: PackageInfo, _ctx: InstallContext): Promise<void> {
+  return Promise.resolve();
+}
+
+function afterInstall(_ctx: InstallContext): Promise<boolean> {
+  return Promise.resolve(false);
+}
+
+async function uninstall(_ctx: InstallContext): Promise<void> {
+  await withSpinner({
+    message: "Removing Chromium...",
+  }, async () => {
+    const fetcherObj = await fetcher();
+    const version = await supportedRevision();
+    return fetcherObj.remove(version);
+  });
+  return Promise.resolve();
+}
+
+export async function fetcher() {
+  const options = {
+    path: chromiumInstallDir(),
+  };
+  const fetcher = (await getPuppeteer()).createBrowserFetcher(options);
+  return fetcher;
+}
+
+// TODO: https://github.com/puppeteer/puppeteer/blob/main/versions.js
+async function supportedRevision(): Promise<string> {
+  return (await getPuppeteer())._preferredRevision;
+}
