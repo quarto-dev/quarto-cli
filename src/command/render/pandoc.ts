e11148547ae15770281f62198b57e62a1a1628ab
J.J. Allaire <jj.allaire@gmail.com>
2023-03-08 08:58:50 -0500

    Revert "remove confluence workaround since it's not needed anymore" (this regressed confluence publishing)
    This reverts commit 789162faedc2a98091ff5221e5ac834324d96427.

--- src/command/render/pandoc.ts
+++ src/command/render/pandoc.ts
@@ -707,6 +707,14 @@ export async function runPandoc(
     formatFilterParams["custom-writer"] = allDefaults.to;
     allDefaults.to = resourcePath("filters/customwriter/customwriter.lua");
   }
+  if (Deno.env.get("QUARTO_ALLENMANNING_WORKAROUND_CONFLUENCE") === undefined) {
+    if (allDefaults.writer?.match(/[.]lua$/)) {
+      formatFilterParams["custom-writer"] = allDefaults.writer;
+      allDefaults.writer = resourcePath(
+        "filters/customwriter/customwriter.lua",
+      );
+    }
+  }
 
   // set up the custom .qmd reader
   if (allDefaults.from) {
