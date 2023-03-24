c81cb8f5d98a8e34fbea25f07fe5ba5baeb011e4
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-03-24 13:51:37 -0700

    add missing change from prev commit

--- src/command/render/render-shared.ts
+++ src/command/render/render-shared.ts
@@ -129,32 +129,6 @@ export async function render(
   };
 }
 
-export function renderServices() {
-  const temp = createTempContext();
-  const extension = createExtensionContext();
-
-  if (getNamedLifetime("render-services")) {
-    return {
-      temp,
-      extension,
-      lifetime: getNamedLifetime("render-services"),
-      cleanup: () => {},
-    };
-  } else {
-    const lifetime = createNamedLifetime("render-services");
-    lifetime.attach(temp);
-
-    return {
-      temp,
-      extension,
-      lifetime,
-      cleanup: () => {
-        lifetime.cleanup();
-      },
-    };
-  }
-}
-
 export function printWatchingForChangesMessage() {
   info("Watching files for changes", { format: colors.green });
 }
