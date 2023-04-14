fb9c2b3ed099f578fd7ff6e685bbbdf11637d055
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-04-14 07:54:31 -0700

    make execution conditional

--- src/resources/filters/quarto-post/jats.lua
+++ src/resources/filters/quarto-post/jats.lua
@@ -5,21 +5,25 @@
 
 
 function jats()
-  return {
-    -- clear out divs
-    Div = function(div)
-      if _quarto.format.isJatsOutput() then
-        -- unroll blocks contained in divs
-        local blocks = pandoc.List()
-        for _, childBlock in ipairs(div.content) do
-          if childBlock.t == "Div" then
-            tappend(blocks, childBlock.content)
-          else
-            blocks:insert(childBlock)
+  if _quarto.format.isJatsOutput() then
+    return {}
+  else 
+    return {
+      -- clear out divs
+      Div = function(div)
+        if _quarto.format.isJatsOutput() then
+          -- unroll blocks contained in divs
+          local blocks = pandoc.List()
+          for _, childBlock in ipairs(div.content) do
+            if childBlock.t == "Div" then
+              tappend(blocks, childBlock.content)
+            else
+              blocks:insert(childBlock)
+            end
           end
+          return blocks
         end
-        return blocks
       end
-    end
-  }
+    }
+  end
 end
