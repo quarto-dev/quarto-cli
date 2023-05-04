4e3a4b9a503f1a25b1c4bec6ce5e30468a1422b1
Charles Teague <cteague@gmail.com>
2023-05-04 09:52:51 -0400

    Don’t position all footnotes in the margin
    Fixes #5338
    
    the new version of Pandoc that we use places footnotes in aside tags, which are getting moved to the margin even when they shouldn’t be moved. this narrows the targeting of asides to sidestep footnotes.

--- src/format/html/format-html-bootstrap.ts
+++ src/format/html/format-html-bootstrap.ts
@@ -218,7 +218,7 @@ export function boostrapExtras(
 // Find any elements that are using fancy layouts (columns)
 const getColumnLayoutElements = (doc: Document) => {
   return doc.querySelectorAll(
-    '[class^="column-"], [class*=" column-"], aside, [class*="margin-caption"], [class*=" margin-caption"], [class*="margin-ref"], [class*=" margin-ref"]',
+    '[class^="column-"], [class*=" column-"], aside:not(.footnotes), [class*="margin-caption"], [class*=" margin-caption"], [class*="margin-ref"], [class*=" margin-ref"]',
   );
 };
 
@@ -1023,7 +1023,7 @@ const processMarginElsInCallouts = (doc: Document) => {
     }
 
     const marginNodes = calloutEl.querySelectorAll(
-      ".callout-body-container .column-margin, .callout-body-container aside, .callout-body-container .aside",
+      ".callout-body-container .column-margin, .callout-body-container aside:not(.footnotes), .callout-body-container .aside:not(.footnotes)",
     );
 
     if (marginNodes.length > 0) {
@@ -1062,7 +1062,7 @@ const processMarginElsInTabsets = (doc: Document) => {
       const tabId = tabEl.id;
 
       const marginNodes = tabEl.querySelectorAll(
-        ".column-margin, aside, .aside",
+        ".column-margin, aside:not(.footnotes), .aside:not(.footnotes)",
       );
 
       if (tabId && marginNodes.length > 0) {
@@ -1224,7 +1224,9 @@ const isContainer = (el: Element | null) => {
 
 const isAlreadyInMargin = (el: Element): boolean => {
   const elInMargin = el.classList.contains("column-margin") ||
-    el.classList.contains("aside") || el.classList.contains("margin-caption");
+    (el.classList.contains("aside") &&
+      !el.classList.contains("footnotes")) ||
+    el.classList.contains("margin-caption");
   if (elInMargin) {
     return true;
   } else if (el.parentElement !== null) {
