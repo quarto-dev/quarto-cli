086cb9ce7c8e75af4171d604e5960d1412c9d203
Charles Teague <cteague@gmail.com>
2023-05-08 13:02:25 -0400

    Allow project types to specify render formats for a file

--- src/project/types/types.ts
+++ src/project/types/types.ts
@@ -38,6 +38,11 @@ export interface ProjectType {
     format: Format,
     project?: ProjectContext,
   ) => Format;
+  formatsForFile?: (
+    formats: string[],
+    file: RenderFile,
+    project?: ProjectContext,
+  ) => string[];
   formatExtras?: (
     context: ProjectContext,
     input: string,
