18bb92ecc6d1729a2ee35d435b5f1c9d431a4231
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-05-09 14:12:56 -0700

    use setTimeout as nextTick workaround instead of bad deno internal call

--- src/core/deno/next-tick.ts
+++ src/core/deno/next-tick.ts
@@ -4,6 +4,11 @@
  * Copyright (C) 2023 Posit Software, PBC
  */
 
-export const nextTick: () => void =
-  // deno-lint-ignore no-explicit-any
-  (Deno as any)[(Deno as any).internal].core.setNextTickCallback;
+// we currently hack nextTick through setTimeout because we don't
+// want to have to import from node and the first hack attempt didn't work.
+//   (Deno as any)[(Deno as any).internal].core.setNextTickCallback;
+
+export const nextTick = (callback: () => void) => {
+  // use setTimeout to emulate nextTick
+  setTimeout(callback, 16); // 16ms is 1 frame at 60fps
+};
