c073873bc0d4dd7ff095a8d956e15f753b8e76b2
Charles Teague <cteague@gmail.com>
2023-05-11 09:56:09 -0400

    Only produce MECA if either explicitly specified or if JATS is in the project

--- src/project/types/manuscript/manuscript.ts
+++ src/project/types/manuscript/manuscript.ts
@@ -50,6 +50,7 @@ import {
 } from "./manuscript-types.ts";
 import { createMecaBundle } from "./manuscript-meca.ts";
 import { readLines } from "io/mod.ts";
+import { info } from "log/mod.ts";
 
 // TODO: Localize
 const kMecaFileLabel = "MECA Archive";
@@ -78,8 +79,8 @@ export const manuscriptProjectType: ProjectType = {
     config: ProjectConfig,
     _flags?: RenderFlags,
   ): Promise<ProjectConfig> => {
-    const manuscriptConfig = config[kManuscriptType] as ManuscriptConfig ||
-      undefined;
+    const manuscriptConfig =
+      (config[kManuscriptType] || {}) as ManuscriptConfig;
 
     // If the manuscript has resources, add those in
     if (manuscriptConfig[kResources]) {
@@ -232,7 +233,11 @@ export const manuscriptProjectType: ProjectType = {
         ?.[kManuscriptType] as ResolvedManuscriptConfig;
 
       if (isArticle(source, project, manuscriptConfig)) {
-        if (manuscriptConfig && manuscriptConfig[kMecaArchive] !== false) {
+        const formats = project.config?.format
+          ? Object.values(project.config?.format)
+          : [];
+
+        if (shouldMakeMecaBundle(formats, manuscriptConfig)) {
           // Add an alternate link to a MECA bundle
           if (format.render[kFormatLinks] !== false) {
             const links: Array<string | FormatLink> = [];
@@ -334,7 +339,13 @@ export const manuscriptProjectType: ProjectType = {
   ) => {
     const manuscriptConfig = context.config
       ?.[kManuscriptType] as ResolvedManuscriptConfig;
-    if (manuscriptConfig && manuscriptConfig[kMecaArchive] !== false) {
+    if (
+      shouldMakeMecaBundle(
+        outputFiles.map((file) => file.format),
+        manuscriptConfig,
+      )
+    ) {
+      info(`Creating ${kMecaFileLabel}...`);
       const mecaFileName = manuscriptConfig.mecaFile;
       const mecaBundle = await createMecaBundle(
         mecaFileName,
@@ -351,6 +362,7 @@ export const manuscriptProjectType: ProjectType = {
         );
       }
     }
+
     return Promise.resolve();
   },
 };
@@ -364,6 +376,26 @@ const isArticle = (
     ? join(project.dir, manuscriptConfig.article)
     : manuscriptConfig.article;
   return file === articlePath;
+};
+
+const shouldMakeMecaBundle = (
+  formats: Format[],
+  manuConfig?: ManuscriptConfig,
+) => {
+  if (!manuConfig || manuConfig[kMecaArchive] !== false) {
+    // See if it was explicitely on
+    if (manuConfig && manuConfig[kMecaArchive] === true) {
+      return true;
+    }
+
+    // See if we're producing JATS, then enable it
+    return formats.find((format) => {
+      return isJatsOutput(format.pandoc);
+    });
+  } else {
+    // Explicitely turned off
+    return false;
+  }
 };
 
 const articleFile = (projectDir: string, config: ManuscriptConfig) => {
