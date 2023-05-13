a0a42e4025752344e5fa4a66e1a587136aeeb7d2
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-05-13 11:57:58 -0700

    Add line numbers to lua errors. Closes #5242.

--- src/resources/filters/common/log.lua
+++ src/resources/filters/common/log.lua
@@ -5,20 +5,26 @@
 -- could write to named filed (e.g. <docname>.filter.log) and client could read warnings and delete (also delete before run)
 -- always append b/c multiple filters
 
+local function caller_info(offset)
+  offset = offset or 3
+  local caller = debug.getinfo(offset, "lS")
+  return caller.source:sub(2,-1) .. ":" .. tostring(caller.currentline)
+end
+
 function info(message)
   io.stderr:write(message .. "\n")
 end
 
 function warn(message) 
-  io.stderr:write(lunacolors.yellow("WARNING: " .. message .. "\n"))
+  io.stderr:write(lunacolors.yellow("WARNING (" .. caller_info() .. ") " .. message .. "\n"))
 end
 
 function error(message)
-  io.stderr:write(lunacolors.red("ERROR: " .. message .. "\n"))
+  io.stderr:write(lunacolors.red("ERROR (" .. caller_info() .. ") " .. message .. "\n"))
 end
 
 function fatal(message)
-  io.stderr:write(lunacolors.red("ERROR: " .. message .. "\n"))
+  io.stderr:write(lunacolors.red("FATAL (" .. caller_info() .. ") " ..message .. "\n"))
   -- TODO write stack trace into log, and then exit.
   crash_with_stack_trace()  
 end
