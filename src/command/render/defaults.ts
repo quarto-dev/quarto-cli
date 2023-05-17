d0b5ddcf5ab9f63097169b41a21af5add9c61758
Charles Teague <cteague@gmail.com>
2023-05-17 18:22:58 -0400

    Merge pull request #5588 from quarto-dev/fix/citeproc-regression
    Correctly unset citeproc meta if set by user as we use filter

--- src/command/render/defaults.ts
+++ src/command/render/defaults.ts
@@ -50,6 +50,7 @@ export async function generateDefaults(
         ...(allDefaults[kFilters] || []),
       ],
       options,
+      allDefaults,
     );
     if (resolvedFilters) {
       allDefaults[kFilters] = resolvedFilters.quartoFilters;
--- src/command/render/filters.ts
+++ src/command/render/filters.ts
@@ -567,6 +567,7 @@ const kQuartoCiteProcMarker = "citeproc";
 export async function resolveFilters(
   filters: QuartoFilter[],
   options: PandocOptions,
+  pandoc: FormatPandoc,
 ): Promise<QuartoFilterSpec | undefined> {
   // build list of quarto filters
 
@@ -601,8 +602,8 @@ export async function resolveFilters(
     // If we're explicitely adding the citeproc filter, turn off
     // citeproc: true so it isn't run twice
     // See https://github.com/quarto-dev/quarto-cli/issues/2393
-    if (options.format.pandoc.citeproc === true) {
-      delete options.format.pandoc.citeproc;
+    if (pandoc.citeproc === true) {
+      delete pandoc.citeproc;
     }
 
     quartoFilters.push(kQuartoCiteProcMarker);
