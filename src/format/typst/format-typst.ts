321365f67cd518794dcf9a889ada259291e625e3
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-05-17 20:57:28 +0200

    Set typst citations system as a default, and handle the case where `citeproc: true` is set in the YAML header.

--- src/format/typst/format-typst.ts
+++ src/format/typst/format-typst.ts
@@ -44,6 +44,11 @@ export function typstFormat(): Format {
       standalone: true,
       [kDefaultImageExtension]: "svg",
       [kWrap]: "none",
+      [kCiteproc]: false,
+      // TODO: Patch to do what Pandoc > 3.1.2 will do next when `+citations` (the default) is set.
+      [kVariables]: {
+        citations: true,
+      },
     },
     resolveFormat: typstResolveFormat,
     formatExtras: (
@@ -111,17 +116,13 @@ export function typstFormat(): Format {
 function typstResolveFormat(format: Format) {
   // Pandoc citeproc with typst output requires adjustment
   // https://github.com/jgm/pandoc/commit/e89a3edf24a025d5bb0fe8c4c7a8e6e0208fa846
-  if (format.pandoc?.[kCiteproc] === false) {
-    // TODO: Patch to do what Pandoc > 3.1.2 will do next when `+citations` (the default) is set.
-    format.pandoc[kVariables] = format.pandoc[kVariables] || {};
-    format.pandoc[kVariables]["citations"] = true;
-  } else if (
-    format.pandoc?.[kCiteproc] === true || format.metadata?.[kBibliography] ||
-    format.metadata?.[kReferences]
-  ) {
-    // Using citeproc requires `citations` extensions to be disabled (e.g typst-citations)
+  if (format.pandoc?.[kCiteproc] === true) {
+    // citeproc: false is the default, so user setting it to true means they want to use
+    // Pandoc's citeproc which requires `citations` extensions to be disabled (e.g typst-citations)
     format.render[kVariant] = [format.render?.[kVariant], "-citations"].join(
       "",
     );
+    format.pandoc[kVariables] = format.pandoc[kVariables] || {};
+    format.pandoc[kVariables]["citations"] = false;
   }
 }
