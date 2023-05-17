784fb01208ff4ef0ef31c5c7c1700e8329b760c0
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-05-17 13:49:21 -0700

    avoid O(n^2), use table.concat instead

--- src/resources/filters/quarto-pre/table-rawhtml.lua
+++ src/resources/filters/quarto-pre/table-rawhtml.lua
@@ -14,21 +14,21 @@ function table_merge_raw_html()
 
   return {
     Blocks = function(blocks)
-      local pendingRaw = ''
+      local pendingRaw = pandoc.List()
       local merged = pandoc.List()
       for i,el in ipairs(blocks) do
         if _quarto.format.isRawHtml(el) and el.text:find(patterns.html_table_tag_name) then
-          pendingRaw = pendingRaw .. "\n" .. el.text
+          pendingRaw:insert(el.text)
         else
           if #pendingRaw > 0 then
-            merged:insert(pandoc.RawBlock("html", pendingRaw))
-            pendingRaw = ''
+            merged:insert(pandoc.RawBlock("html", table.concat(pendingRaw, "\n")))
+            pendingRaw = pandoc.List()
           end
           merged:insert(el)
         end
       end
       if #pendingRaw > 0 then
-        merged:insert(pandoc.RawBlock("html", pendingRaw))
+        merged:insert(pandoc.RawBlock("html", table.concat(pendingRaw, "\n")))
       end
       return merged
     end
