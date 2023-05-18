5b93eee68b124beccaffa921d15f54493ba6f2c9
Charles Teague <cteague@gmail.com>
2023-05-18 09:08:57 -0400

    Add ability to force filtering of hidden and cleaning of hidden classes

--- src/resources/filters/quarto-pre/hidden.lua
+++ src/resources/filters/quarto-pre/hidden.lua
@@ -2,11 +2,39 @@
 -- Copyright (C) 2020-2022 Posit Software, PBC
 
 function hidden()
-  if param("keep-hidden", false) then
-    return {
-      Div = stripHidden,
-      CodeBlock = stripHidden
-    }
+
+  local function stripHidden(el)
+    if el.attr.classes:find("hidden") then
+      return {}
+    end
+  end
+
+  local function clearHiddenClasses(el) 
+    local _val, idx = el.attr.classes:find("hidden") 
+    if idx then
+      el.attr.classes:remove(idx);
+      return el
+    else
+      return undefined
+    end
+  end
+  
+  local stripHiddenCellFilter = {
+    Div = stripHidden,
+    CodeBlock = stripHidden
+  }
+
+  local clearHiddenClassesFilter = {
+    Div = clearHiddenClasses,
+    CodeBlock = clearHiddenClasses
+  }
+
+  if param("keep-hidden", false) and not _quarto.format.isHtmlOutput() then
+    return stripHiddenCellFilter
+  elseif param("remove-hidden", false) then
+    return stripHiddenCellFilter
+  elseif param("clear-hidden-classes", false) then
+    return clearHiddenClassesFilter
   else
     return {
 
@@ -14,8 +42,5 @@ function hidden()
   end
 end
 
-function stripHidden(el)
-  if not _quarto.format.isHtmlOutput() and el.attr.classes:find("hidden") then
-    return {}
-  end
-end
+
+
