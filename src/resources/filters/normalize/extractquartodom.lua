e9dd822b5e5ecc02cc2d6de8fe87f7cbf351b28e
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-05-30 10:11:17 -0700

    process shortcodes inside data-qmd attributes. Closes #5730

--- src/resources/filters/normalize/extractquartodom.lua
+++ src/resources/filters/normalize/extractquartodom.lua
@@ -1,11 +1,15 @@
+local md_shortcode = require("lpegshortcode")
+
 local function process_quarto_markdown_input_element(el)
-  if el.attributes.qmd ~= nil then
-    return pandoc.read(el.attributes.qmd, "markdown")
-  elseif el.attributes["qmd-base64"] ~= nil then
-    return pandoc.read(quarto.base64.decode(el.attributes["qmd-base64"]), "markdown")
-  else
+  if el.attributes.qmd == nil and el.attributes["qmd-base64"] == nil then
     error("process_quarto_markdown_input_element called with element that does not have qmd or qmd-base64 attribute")
+    return el
   end
+  local text = el.attributes.qmd or quarto.base64.decode(el.attributes["qmd-base64"])
+  local after_shortcodes = md_shortcode.md_shortcode:match(text)
+  local after_reading = pandoc.read(after_shortcodes, "markdown")
+  local after_parsing = after_reading:walk(parse_extended_nodes()):walk(compute_flags())
+  return after_parsing
 end
 
 function parse_md_in_html_rawblocks()
@@ -21,8 +25,6 @@ function parse_md_in_html_rawblocks()
         local doc = process_quarto_markdown_input_element(span)
         if #doc.blocks < 1 then
           return pandoc.Span({})
-          -- error("process_quarto_markdown_input_element called with empty element")
-          -- error(span.attributes.qmd)
         end
         return doc.blocks[1].content
       end
