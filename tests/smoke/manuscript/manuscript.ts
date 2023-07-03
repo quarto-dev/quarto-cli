9eb98dd557c4a18abbb616c389abac4efa8bcbc0
Charles Teague <cteague@gmail.com>
2023-07-03 14:10:09 -0400

    Implement basic JATS validation
    (currently disabled)

--- tests/smoke/manuscript/manuscript.ts
+++ tests/smoke/manuscript/manuscript.ts
@@ -6,9 +6,15 @@
 import { existsSync } from "fs/mod.ts";
 import { basename, dirname, extname, join } from "path/mod.ts";
 import { testQuartoCmd, Verify } from "../../test.ts";
-import { fileExists, noErrorsOrWarnings } from "../../verify.ts";
+import { docs } from "../../utils.ts";
+import {
+  ensureXmlValidatesWithXsd,
+  fileExists,
+  noErrorsOrWarnings,
+} from "../../verify.ts";
 
 export type targetFormat = "html" | "jats" | "docx" | "pdf";
+const enableJatsValidation = false;
 
 export const testManuscriptRender = (
   article: string,
@@ -19,23 +25,33 @@ export const testManuscriptRender = (
 ) => {
   const articleDir = dirname(article);
 
+  const verifications: Verify[] = [];
   const articleOuts = formats.map((fmt) => {
-    return manuscriptOutputForInput(article, fmt);
+    const output = manuscriptOutputForInput(article, fmt);
+    if (enableJatsValidation && fmt === "jats") {
+      const xsdPath = docs(
+        join("jats", "xsd", "JATS-Archiving-1-2-MathML2-DTD"),
+      );
+
+      // Test a basic JATS document that tests a variety of elements
+      verifications.push(ensureXmlValidatesWithXsd(output.outputPath, xsdPath));
+    }
+    return output;
   });
 
-  const verifyFileExists = articleOuts.map((out) => {
-    return fileExists(out.outputPath);
+  articleOuts.forEach((out) => {
+    verifications.push(fileExists(out.outputPath));
   });
 
   expectedOutputs.forEach((out) => {
-    verifyFileExists.push(fileExists(join(articleDir, "_manuscript", out)));
+    verifications.push(fileExists(join(articleDir, "_manuscript", out)));
   });
 
   // Render the manuscript
   testQuartoCmd(
     "render",
     [articleDir, "--to", to],
-    [noErrorsOrWarnings, ...verifyFileExists, ...articleVerify],
+    [noErrorsOrWarnings, ...verifications, ...articleVerify],
     {
       teardown: () => {
         articleOuts.forEach((out) => {
