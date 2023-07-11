76787a38b572a4140d1b4fead0691ef8a7f78771
Charles Teague <cteague@gmail.com>
2023-07-11 13:51:51 -0400

    correct signing path

--- package/src/macos/installer.ts
+++ package/src/macos/installer.ts
@@ -79,7 +79,7 @@ export async function makeInstallerMac(config: Configuration) {
     const signWithoutEntitlements: string[] = [
       join(config.directoryInfo.pkgWorking.bin, "quarto.js"),
       join(config.directoryInfo.pkgWorking.bin, "quarto"),
-      join(config.directoryInfo.pkgWorking.bin, "pandoc"),
+      join(config.directoryInfo.pkgWorking.bin, "tools", "pandoc"),
     ];
 
 
