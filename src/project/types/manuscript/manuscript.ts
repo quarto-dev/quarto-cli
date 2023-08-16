66b7c0ede0050d7fc12ad8c3cc76d4db344f8226
Charles Teague <cteague@gmail.com>
2023-08-16 16:04:11 -0400

    Only keep tex when rendering pdf

--- src/project/types/manuscript/manuscript.ts
+++ src/project/types/manuscript/manuscript.ts
@@ -52,11 +52,7 @@ import {
   kWarning,
 } from "../../../config/constants.ts";
 import { projectOutputDir } from "../../project-shared.ts";
-import {
-  isHtmlOutput,
-  isIpynbOutput,
-  isLatexOutput,
-} from "../../../config/format.ts";
+import { isHtmlOutput, isLatexOutput } from "../../../config/format.ts";
 import {
   PandocInputTraits,
   PandocOptions,
@@ -296,9 +292,6 @@ export const manuscriptProjectType: ProjectType = {
 
     // Default to cosmo theme
     config[kTheme] = "cosmo";
-
-    // By default, keep tex and clean things up ourselves
-    config[kKeepTex] = true;
 
     return config;
   },
@@ -545,6 +538,11 @@ export const manuscriptProjectType: ProjectType = {
         };
       }
       extras[kNotebooks] = Object.values(outputNbs);
+    } else if (isArticle && isLatexOutput(format.pandoc)) {
+      if (isLatexOutput(format.pandoc)) {
+        // By default, keep tex and clean things up ourselves
+        format.render[kKeepTex] = true;
+      }
     }
 
     // Resolve input links
