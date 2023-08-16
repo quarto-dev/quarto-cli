b4e66c8c54a2a7a1c1225a1c17d7608341ffcacb
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-08-16 12:36:58 -0700

    fix tests with new DOM

--- tests/smoke/crossref/tables.test.ts
+++ tests/smoke/crossref/tables.test.ts
@@ -14,9 +14,16 @@ import { docs } from "../../utils.ts";
 const tablesQmd = crossref("tables.qmd", "html");
 testRender(tablesQmd.input, "html", false, [
   ensureHtmlElements(tablesQmd.output.outputPath, [
-    "section#simple-crossref-table > div#tbl-letters > table > caption",
-    "section#sub-tables div.quarto-layout-panel > div.quarto-layout-row > div#tbl-first > table > caption",
-    "section#sub-tables div.quarto-layout-panel > div.quarto-layout-row > div#tbl-second > table > caption",
+    // tables in figure elements
+    "section#simple-crossref-table > div#tbl-letters > figure table",
+    "section#sub-tables div.quarto-layout-panel > figure div.quarto-layout-row div#tbl-first > figure table",
+    "section#sub-tables div.quarto-layout-panel > figure div.quarto-layout-row div#tbl-second > figure table",
+
+    // table captions in figure elements (with and without subfloats)
+    "section#simple-crossref-table > div#tbl-letters > figure.quarto-float-tbl > figcaption.table.quarto-float-caption",
+    "section#sub-tables div#tbl-panel.quarto-layout-panel > figure.quarto-float-tbl > figcaption.table.quarto-float-caption",
+    "section#sub-tables div.quarto-layout-panel div.quarto-layout-row div#tbl-first  > figure.quarto-float-tbl > figcaption.table.quarto-subfloat-caption",
+    "section#sub-tables div.quarto-layout-panel div.quarto-layout-row div#tbl-second > figure.quarto-float-tbl > figcaption.table.quarto-subfloat-caption",
   ]),
   ensureFileRegexMatches(tablesQmd.output.outputPath, [
     /Table&nbsp;1: My Caption/,
@@ -34,8 +41,8 @@ testRender(tablesQmd.input, "html", false, [
 const knitrTablesQmd = crossref("knitr-tables.qmd", "html");
 testRender(knitrTablesQmd.input, "html", false, [
   ensureHtmlElements(knitrTablesQmd.output.outputPath, [
-    "div.quarto-layout-panel > div.quarto-layout-row > div#tbl-cars > table > caption",
-    "div.quarto-layout-panel > div.quarto-layout-row > div#tbl-pressure > table > caption",
+    "div.quarto-layout-panel div.quarto-layout-row div#tbl-cars > figure.quarto-float-tbl >figcaption.table.quarto-subfloat-caption",
+    "div.quarto-layout-panel div.quarto-layout-row div#tbl-pressure > figure.quarto-float-tbl > figcaption.table.quarto-subfloat-caption",
   ]),
   ensureFileRegexMatches(knitrTablesQmd.output.outputPath, [
     /Table&nbsp;1: Tables/,
