e132fb3b33fd3cbb4cb170ffee5b5ceaac444e0a
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-08-23 13:19:16 -0700

    jats panel support

--- src/resources/filters/layout/jats.lua
+++ src/resources/filters/layout/jats.lua
@@ -47,3 +47,60 @@ function jatsPosition(el)
       return "float"
     end
 end
+
+_quarto.ast.add_renderer("PanelLayout", function(layout)
+  return _quarto.format.isJatsOutput()
+end, function(layout)
+
+  if layout.float == nil then
+    fail("don't know how to render a layout without a float")
+    return nil
+  end
+
+  -- empty options by default
+  if not options then
+    options = {}
+  end
+  -- outer panel to contain css and figure panel
+  local attr = pandoc.Attr(layout.identifier or "", layout.classes or {}, layout.attributes or {})
+  local panel_content = pandoc.Blocks({})
+  -- layout
+  for i, row in ipairs(layout.layout) do
+    
+    local aligns = row:map(function(cell) 
+      -- get the align
+      local align = cell.attributes[kLayoutAlign]
+      return layoutTableAlign(align) 
+    end)
+    local widths = row:map(function(cell) 
+      -- propagage percents if they are provided
+      local layoutPercent = horizontalLayoutPercent(cell)
+      if layoutPercent then
+        return layoutPercent / 100
+      else
+        return 0
+      end
+    end)
+
+    local cells = pandoc.List()
+    for _, cell in ipairs(row) do
+      local align = cell.attributes[kLayoutAlign]
+      cells:insert(cell)
+    end
+    
+    -- make the table
+    local panelTable = pandoc.SimpleTable(
+      pandoc.List(), -- caption
+      aligns,
+      widths,
+      pandoc.List(), -- headers
+      { cells }
+    )
+    
+    -- add it to the panel
+    panel_content:insert(pandoc.utils.from_simple_table(panelTable))
+  end
+  prepare_caption(layout.float)
+  -- return panel
+  return pandoc.Figure(panel_content, {layout.float.caption_long}, attr)
+end)
