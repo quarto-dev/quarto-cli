c7b5f0c52f30def719666a8c171d697d7a21c199
Charles Teague <cteague@gmail.com>
2023-10-09 10:33:31 -0400

    die if credentials are invalid

--- package/src/common/archive-binary-dependencies.ts
+++ package/src/common/archive-binary-dependencies.ts
@@ -114,9 +114,16 @@ export async function archiveBinaryDependency(
     ];
     for (const platformDep of platformDeps) {
       if (platformDep) {
+        
         const dependencyAwsPath =
           `${kBucket}/${dependencyBucketPath}/${platformDep.filename}`;
+        info(`Checking ${dependencyAwsPath}`);
         const response = await s3cmd("ls", [dependencyAwsPath]);
+        if (response?.includes('Unable to locate credentials')) {
+          throw new Error("Unable to locate S3 credentials, please try again.");
+        }
+        
+
         if (!response) {
           // This dependency doesn't exist, archive it
           info(
