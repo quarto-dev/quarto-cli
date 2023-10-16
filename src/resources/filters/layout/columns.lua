79c347f8aa48026cdbcfdd80d1edc4cb3f7452f8
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-10-16 13:20:30 -0700

    add missing is_regular_node changes

--- src/resources/filters/layout/columns.lua
+++ src/resources/filters/layout/columns.lua
@@ -117,7 +117,7 @@ function renderDivColumn(el)
           if figure ~= nil then
             applyFigureColumns(columnClasses, figure)
             figOrTable = true
-          elseif contentEl.t == 'Div' and hasTableRef(contentEl) then
+          elseif is_regular_node(contentEl, "Div") and hasTableRef(contentEl) then
             -- wrap table divs
             latexWrapEnvironment(contentEl, latexTableEnv(el), false)
             figOrTable = true
@@ -131,7 +131,7 @@ function renderDivColumn(el)
             latexWrapEnvironment(tableDiv, latexTableEnv(el), false)
             el.content[j] = tableDiv
             figOrTable = true
-          elseif contentEl.t == 'Div' then
+          elseif is_regular_node(contentEl, "Div") then
             -- forward the columns class from the output div
             -- onto the float ref target, which prevents
             -- the general purpose `sidenote` processing from capturing this
