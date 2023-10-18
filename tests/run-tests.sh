dcae8ce2bc920a94f74effdc7507db242371e450
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-10-18 16:18:24 +0200

    Running test for timing needs also to ignore smoke_all starting with _

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -67,7 +67,7 @@ if [ "$QUARTO_TEST_TIMING" != "" ] && [ "$QUARTO_TEST_TIMING" != "false" ]; then
     # For smoke-all.test.ts, each smoke-all document test needs to be timed.
     if [ "$i" == "$SMOKE_ALL_TEST_FILE" ]; then
       echo "> Timing smoke-all tests"
-      SMOKE_ALL_FILES=`find docs/smoke-all/ -type f -name "*.qmd" -o -name "*.ipynb" -o -name "*.md"`
+      SMOKE_ALL_FILES=`find docs/smoke-all/ -type f -regextype "posix-extended" -regex ".*/[^_][^/]*[.]qmd" -o -regex ".*/[^_][^/]*[.]md" -o -regex ".*/[^_][^/]*[.]ipynb"`
       for j in $SMOKE_ALL_FILES; do
         echo "${SMOKE_ALL_TEST_FILE} -- ${j}" >> "$QUARTO_TEST_TIMING"
         /usr/bin/time -f "        %e real %U user %S sys" -a -o ${QUARTO_TEST_TIMING} "${DENO_DIR}/tools/${DENO_ARCH_DIR}/deno" test ${QUARTO_DENO_OPTIONS} ${QUARTO_DENO_EXTRA_OPTIONS} "${QUARTO_IMPORT_ARGMAP}" ${SMOKE_ALL_TEST_FILE} -- ${j}
