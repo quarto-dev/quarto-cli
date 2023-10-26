808688f506d6f6c14a1a1abf18cfaa42f1c2c5cf
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-10-26 12:52:37 -0700

    win32 fix

--- quarto_cli/__init__.py
+++ quarto_cli/__init__.py
@@ -2,10 +2,15 @@ import sys
 import subprocess
 import glob
 from pathlib import Path
+import sys
 
 def find_version():
     g = str((Path(__file__).parent / "quarto-*").resolve())
-    return Path(glob.glob(g)[0]) / "bin" / "quarto"
+    g = str((Path(glob.glob(g)[0]) / "bin" / "quarto").resolve())
+    # if on windows, search for quarto.exe
+    if sys.platform == "win32":
+        g += ".exe"
+    return g
 
 def call_quarto(*args, **kwargs):
     return subprocess.run([find_version(), *sys.argv[1:], *args], **kwargs)
