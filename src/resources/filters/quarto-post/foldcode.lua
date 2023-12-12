54d2a306844cab1cfbef9a5fa4e461a02b698513
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-12-12 11:29:15 -0700

    simplify pass with topdown traverse

--- src/resources/filters/quarto-post/foldcode.lua
+++ src/resources/filters/quarto-post/foldcode.lua
@@ -2,13 +2,10 @@
 -- Copyright (C) 2021-2022 Posit Software, PBC
 
 -- slightly fancy code here to make two operations work in a single pass
-
-local pass_uuid = "8c6b3915-b784-4ce5-8e73-59b368a9f289"
-
 function render_folded_block(block)
   local make_code_fold_html = function(fold, summary)
     local div = pandoc.Div({}, pandoc.Attr("", { 
-      "quarto-scaffold", pass_uuid 
+      "quarto-scaffold" 
     }))
     quarto_global_state.codeFoldingCss = _quarto.format.isHtmlOutput()
     local open = ""
@@ -36,7 +33,7 @@ function render_folded_block(block)
     return div
   end
   local make_code_cell_scaffold = function(div)
-    return pandoc.Div({ block }, pandoc.Attr("", { "quarto-scaffold", pass_uuid }))
+    return pandoc.Div({ block }, pandoc.Attr("", { "quarto-scaffold" }))
   end
   if not block.attr.classes:includes("cell-code") then
     return nil
@@ -63,33 +60,32 @@ function fold_code_and_lift_codeblocks()
   return {
     traverse = "topdown",
     FloatRefTarget = function(float, float_node)
-      -- we need some special case logic to not lift code blocks
-      -- from listing floats that have no other content.
-      local other_content_found = false
-      _quarto.ast.walk(float.content, {
-        Div = function(div)
-          if not div.classes:includes(pass_uuid) then
-            other_content_found = true
-          end
-        end,
-      })
-      if not other_content_found then
+      -- we need to not lift code blocks from listing floats
+      if float.type == "Listing" then
         return nil
       end
 
       local blocks = pandoc.Blocks({})
       -- ok to lift codeblocks
       float.content = _quarto.ast.walk(float.content, {
+        traverse = "topdown",
         Div = function(div)
-          if div.classes:includes(pass_uuid) then
-            blocks:insert(div)
-            return {}
-          end
-        end
+          other_content_found = true
+          return nil
+        end,
+        DecoratedCodeBlock = function(block)
+          -- defer the folding of code blocks to the DecoratedCodeBlock renderer
+          -- so that we can handle filename better
+          return nil, false
+        end,
+        CodeBlock = function(block)
+          blocks:insert(render_folded_block(block))
+          return {}
+        end,
       })
       if #blocks > 0 then
         blocks:insert(float_node)
-        return blocks
+        return blocks, false
       end
     end,
 
