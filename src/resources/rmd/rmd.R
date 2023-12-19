a601b4c4bb029e5375fd81d42634d97817d8fa18
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-12-19 09:37:15 +0100

    Correctly read in R from stdin considering encoding
    stdin comes from deno and is supposed to be utf-8. So we don't re-encode the connection and just read from it, marking it as UTF-8.

--- src/resources/rmd/rmd.R
+++ src/resources/rmd/rmd.R
@@ -170,8 +170,8 @@
   }
 
   # read request from stdin
-  stdin <- file("stdin", "r", encoding = "UTF-8")
-  input <- xfun::read_utf8(stdin, error = FALSE)
+  stdin <- file("stdin", "r", encoding = "")
+  input <- readLines(stdin, warn = FALSE, encoding = "UTF-8")
   close(stdin)
 
   # parse request and params
