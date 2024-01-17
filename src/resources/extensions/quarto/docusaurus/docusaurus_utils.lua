77b6992f4d555445604c45560e82833f9abe51e0
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2024-01-17 08:49:50 -0700

    lua, docusaurus - handle code-folding in docusaurus renderers

--- src/resources/extensions/quarto/docusaurus/docusaurus_utils.lua
+++ src/resources/extensions/quarto/docusaurus/docusaurus_utils.lua
@@ -1,6 +1,63 @@
 -- docusaurus_utils.lua
 -- Copyright (C) 2020-2023 Posit Software, PBC
 
+-- a slightly different version of render_folded_block
+-- that avoids emitting rawhtml blocks, since these are handled
+-- specially by the docusaurus filter
+function render_folded_block(block)
+  local make_code_fold_html = function(fold, summary)
+    local div = pandoc.Div({}, pandoc.Attr("", { 
+      "quarto-scaffold" 
+    }))
+    quarto_global_state.codeFoldingCss = _quarto.format.isHtmlOutput()
+    local open = ""
+    if fold == "show" then
+      open = " open"
+    end
+    local style = ""
+    local clz = 'code-fold'
+    if block.attr.classes:includes("hidden") then
+      clz = clz .. " hidden"
+    end
+
+    style = ' class="' .. clz .. '"'
+    local beginPara = pandoc.Plain({
+      pandoc.RawInline("markdown", "<details" .. open .. style .. ">\n<summary>"),
+    })
+    
+    if not isEmpty(summary) then
+      tappend(beginPara.content, markdownToInlines(summary))
+    end
+    beginPara.content:insert(pandoc.RawInline("markdown", "</summary>"))
+    div.content:insert(beginPara)
+    div.content:insert(block)
+    div.content:insert(pandoc.RawInline("markdown", "</details>"))
+    return div
+  end
+  local make_code_cell_scaffold = function(div)
+    return pandoc.Div({ block }, pandoc.Attr("", { "quarto-scaffold" }))
+  end
+  if not block.attr.classes:includes("cell-code") then
+    return nil
+  end
+  if not (_quarto.format.isHtmlOutput() or _quarto.format.isMarkdownWithHtmlOutput()) then
+    return make_code_cell_scaffold(block)
+  end
+  local fold = foldAttribute(block)
+  local summary = summaryAttribute(block)
+  if fold ~= nil or summary ~= nil then
+    block.attr.attributes["code-fold"] = nil
+    block.attr.attributes["code-summary"] = nil
+    if fold ~= "none" then 
+      return make_code_fold_html(fold, summary)
+    else
+      return block
+    end
+  else
+    return block
+  end
+end
+
 function code_block(code_block_slot, filename)
   function process(el)
     local lang = el.attr.classes[1]
@@ -35,6 +92,8 @@ function code_block(code_block_slot, filename)
       return el
     end
   end
+
+  code_block_slot = render_folded_block(code_block_slot) or code_block_slot
   
   if code_block_slot.t == "CodeBlock" then
     return process(code_block_slot)
