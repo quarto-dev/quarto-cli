ca10f511d188d8f9c4a87ab0b0d2596415888063
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-01-23 10:30:11 +0100

    simply julia code

--- src/resources/julia/quartonotebookrunner.jl
+++ src/resources/julia/quartonotebookrunner.jl
@@ -1,8 +1,3 @@
 using QuartoNotebookRunner
 
-function execute_qmd(sourcefile::String, output_ipynb::String)
-  server = QuartoNotebookRunner.Server()
-  run!(server, sourcefile, output=output_ipynb)
-end
-
-execute_qmd(ARGS[1], ARGS[2])
+QuartoNotebookRunner.render(ARGS[1]; output = ARGS[2])
