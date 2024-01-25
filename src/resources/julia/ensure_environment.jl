c79e5b20af4c3f1d4c2f12002d1e2269192e1573
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-01-25 19:35:48 +0100

    also check that julia version in manifest matches

--- src/resources/julia/ensure_environment.jl
+++ src/resources/julia/ensure_environment.jl
@@ -1,4 +1,5 @@
 using Pkg
+using TOML
 
 # If the manifest was resolved with the exact Project.toml that we have copied
 # over from quarto's resource folder, then we just ensure that it is instantiated,
@@ -6,7 +7,16 @@ using Pkg
 # Otherwise, we update to get the newest packages fulfilling the compat bounds set in
 # the Project.toml.
 
-if Pkg.is_manifest_current() === true # this returns nothing if there's no manifest
+function manifest_has_correct_julia_version()
+  project_file = Base.active_project()
+  manifest_file = joinpath(dirname(project_file), "Manifest.toml")
+  version = VersionNumber(TOML.parsefile(manifest_file)["julia_version"])
+  return version.major == VERSION.major && version.minor == VERSION.minor
+end
+
+manifest_matches_project_toml = Pkg.is_manifest_current() === true # this returns nothing if there's no manifest
+
+if manifest_matches_project_toml && manifest_has_correct_julia_version()
   Pkg.instantiate()
 else
   Pkg.update()
