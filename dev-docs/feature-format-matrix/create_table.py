082f4d4f70f4142b5ecab7ce76ab72ac8c0cae67
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2024-02-08 10:17:34 -0600

    feature-format-matrix - use single document for any one feature

--- dev-docs/feature-format-matrix/create_table.py
+++ dev-docs/feature-format-matrix/create_table.py
@@ -1,6 +1,6 @@
 import yaml
 import json
-import base64
+import glob
 
 class Trie:
 
@@ -24,7 +24,7 @@ class Trie:
     def tabulator_leaf(self):
         result = {}
         for v in self.values:
-            result[v["format"]] = v["file"]
+            result[v["format"]] = v["table_cell"]
         return result
 
     def tabulator(self):
@@ -52,88 +52,41 @@ class Trie:
             return 1
         return sum([v.size() for v in self.children.values()])
 
-def render_features_formats_table(filename):
-    s = yaml.load(open(filename), Loader=yaml.SafeLoader)
 
-    features = Trie()
-    for l in s:
-        features.insert(l["feature"].split("/"), l)
-
-    formats = {}
-    for l in s:
-        formats[l["format"]] = features.depth() + len(formats) - 1
-
-    n_columns = features.depth() + len(formats) 
-
-    table_cells = list(list(None for _ in range(n_columns)) for _ in range(features.size()))
-    table_headers = list(None for _ in range(n_columns))
-
-    def add_spanners(node, row, col):
-        # if node.children:
-        #     print("<<<")
-        #     print(row, col)
-        #     print(json.dumps(node.json(), indent=2))
-        #     print(">>>")
-        for k, v in node.children.items():
-            # print("::::", row, col)
-            table_cells[row][col] = "<td class='spanner' rowspan='%s'><div class='inner_spanner'>%s</div></td>" % (v.size(), k)
-            for i in range(row+1, row + v.size()):
-                table_cells[i][col] = ""
-            add_spanners(v, row, col + 1)
-            row += v.size()
-    def add_headers():
-        for k, i in formats.items():
-            table_headers[i] = "<th>%s</th>" % k
-    def add_entries(node, row, col):
-        for v in node.values:
-            content = "[{{< fa link >}}](%s/%s.qmd)" % (v["feature"], v["format"])
-            # content = "<a href='%s'><i class='fa-solid fa-link' aria-label='link'></i></a>" % v.get("file", "%s/%s.qmd" % (v["feature"], v["format"]))
-            # encode as base64
-            b64 = base64.encodebytes(content.encode("utf-8"))
-            table_cells[row][formats[v["format"]]] = "<td><span data-qmd-base64='%s'></span></td>" % b64.decode("utf-8").strip()
-        for _, v in node.children.items():
-            add_entries(v, row, col + 1)
-            row += v.size()
-    add_spanners(features, 0, 0)
-    add_entries(features, 0, 0)
-    add_headers()
-    table_headers[0] = '<th colspan="%s">↓ Features \\ Formats → </th>' % features.depth()
-    for i in range(1, features.depth()):
-        table_headers[i] = ""
-
-    def render_table():
-        html = []
-        html.append("```{=html}\n<table class='features-formats-table'>")
-        html.append("<tr class='header-row'>")
-        for h in table_headers:
-            if h is None:
-                html.append("<th></th>")
+def extract_metadata_from_file(file):
+    with open(file, "r") as f:
+        lines = f.readlines()
+    start = None
+    end = None
+    for i, line in enumerate(lines):
+        if line.strip() == "---":
+            if start is None:
+                start = i
             else:
-                html.append(h)
-        html.append("</tr>")
-        for row in table_cells:
-            html.append("<tr>")
-            for d in row:
-                if d is None:
-                    html.append("<td></td>")
-                else:
-                    html.append(d)
-            html.append("</tr>")
-        html.append("</table>\n```\n")
-        return "\n".join(html)
-    return render_table()
-import glob
+                end = i
+                metadata = yaml.load("".join(lines[start+1:end]), Loader=yaml.SafeLoader)
+                return metadata
+    raise ValueError("No metadata found in file %s" % file)
+
+def table_cell(entry, _feature, _format_name, _format_config):
+    return "<a href='%s' target='_blank'><i class='fa-solid fa-link' aria-label='link'></i></a>" % entry
 
 def render_features_formats_data():
     trie = Trie()
-    for entry in glob.glob("qmd-files/**/*.qmd", recursive=True):
-        feature = entry.split("/")[1:]
-        format = feature.pop().split(".")[0]
-        trie.insert(feature, {
-            "feature": "/".join(feature),
-            "format": format,
-            "file": "<a href='%s' target='_blank'><i class='fa-solid fa-link' aria-label='link'></i></a>" % entry
-        })
+    for entry in glob.glob("qmd-files/**/document.qmd", recursive=True):
+        feature = entry.split("/")[1:-1]
+        front_matter = extract_metadata_from_file(entry)
+        try:
+            format = front_matter["format"]
+        except KeyError:
+            raise Exception("No format found in %s" % entry)
+        for format_name, format_config in format.items():
+            trie.insert(feature, {
+                "feature": "/".join(feature),
+                "format": format_name,
+                "format_config": format_config,
+                "table_cell": table_cell(entry, feature, format_name, format_config)
+            })
     entries = trie.tabulator()
 
     return "```{=html}\n<script type='text/javascript'>\nvar tableData = %s;\n</script>\n```\n" % json.dumps(entries, indent=2)
