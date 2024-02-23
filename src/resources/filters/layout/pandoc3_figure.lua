7e0dca6698f35dcbdaeed2ba3b9f6b721eacc199
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2024-02-23 15:45:13 -0700

    lua - reorder stages so Pandoc.Figure subfigures in LaTeX center properly

--- src/resources/filters/layout/pandoc3_figure.lua
+++ src/resources/filters/layout/pandoc3_figure.lua
@@ -75,19 +75,66 @@ function render_pandoc3_figure()
     local filter
     filter = function(state)
       state = state or {}
-      return {
-        traverse = "topdown",
-        FloatRefTarget = function(float)
-          local count = 0
-          _quarto.ast.walk(float.content, {
-            Figure = function()
-              count = count + 1
+      local function figure_renderer(figure, is_subfig)
+        -- this is a figure that is not cross-referenceable
+        -- if this ends up in a layout without fig-pos = H, it'll fail
+        -- 'H' forces it to not float
+        if figure.identifier == "" then
+          figure = _quarto.ast.walk(figure, {
+            Image = function(image)
+              image.attributes['fig-pos'] = 'H'
+              return image
             end
           })
-          if count > 0 then
-            return nil, false
+        end
+        local image
+        _quarto.ast.walk(figure, {
+          Image = function(img)
+            image = img
           end
+        })
+        if image == nil then
+          return figure
+        end
+        if figure.caption.long ~= nil then
+          image.caption = quarto.utils.as_inlines(figure.caption.long)
+        end
+        for k, v in pairs(figure.attributes) do
+          image.attributes[k] = v
+        end
+        if is_subfig then
+          image.attributes['quarto-caption-env'] = 'subcaption'
+        end
+        image.classes:extend(figure.classes)
+        if state.in_column_margin then
+          image.classes:insert("column-margin")
+        end
+        return latexImageFigure(image)
+      end
+      local function float_renderer(float)
+        local count = 0
+        local new_content = _quarto.ast.walk(float.content, {
+          Figure = function(fig)
+            count = count + 1
+            return figure_renderer(fig, true), false
+          end
+        })
+        if count > 0 then
+          float.content = new_content
+          return float, false
+        end
+      end
+      return {
+        traverse = "topdown",
+        PanelLayout = function(panel)
+          panel.rows = _quarto.ast.walk(panel.rows, {
+            Figure = function(fig)
+              return figure_renderer(fig, true), false
+            end
+          })
+          return panel, false
         end,
+        FloatRefTarget = float_renderer,
         Div = function(div)
           if div.classes:includes("column-margin") then
             local new_state = {}
@@ -102,37 +149,7 @@ function render_pandoc3_figure()
           end
         end,
         Figure = function(figure)
-          -- this is a figure that is not cross-referenceable
-          -- if this ends up in a layout without fig-pos = H, it'll fail
-          -- 'H' forces it to not float
-          if figure.identifier == "" then
-            figure = _quarto.ast.walk(figure, {
-              Image = function(image)
-                image.attributes['fig-pos'] = 'H'
-                return image
-              end
-            })
-          end
-          local image
-          _quarto.ast.walk(figure, {
-            Image = function(img)
-              image = img
-            end
-          })
-          if image == nil then
-            return figure
-          end
-          if figure.caption.long ~= nil then
-            image.caption = quarto.utils.as_inlines(figure.caption.long)
-          end
-          for k, v in pairs(figure.attributes) do
-            image.attributes[k] = v
-          end
-          image.classes:extend(figure.classes)
-          if state.in_column_margin then
-            image.classes:insert("column-margin")
-          end
-          return latexImageFigure(image)
+          return figure_renderer(figure, false)
         end
       }
     end
