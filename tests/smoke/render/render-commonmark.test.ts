762c22cbdd7ccd262beba7d5cd1d882a0a8e201e
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-02-28 12:33:10 +0100

    re-add native julia commonmark smoke test

--- tests/smoke/render/render-commonmark.test.ts
+++ tests/smoke/render/render-commonmark.test.ts
@@ -10,10 +10,11 @@ import { ensureFileRegexMatches } from "../../verify.ts";
 import { testRender } from "./render.ts";
 
 const tests = [
-  { file: "commonmark-plain.qmd", python: false },
-  { file: "commonmark-r.qmd", python: false },
-  { file: "commonmark-python.qmd", python: true },
-  { file: "commonmark-julia-jupyter.qmd", python: false },
+  { file: "commonmark-plain.qmd" },
+  { file: "commonmark-r.qmd" },
+  { file: "commonmark-python.qmd"},
+  { file: "commonmark-julia-jupyter.qmd" },
+  { file: "commonmark-julia.qmd" },
 ];
 tests.forEach((test) => {
   const input = docs(join("markdown", test.file));
