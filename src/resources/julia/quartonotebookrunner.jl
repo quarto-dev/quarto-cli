e822d46e9389c3ce40db44b81c4aa6c49c3e072f
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-03-04 19:06:48 +0100

    add test connection before writing transport file

--- src/resources/julia/quartonotebookrunner.jl
+++ src/resources/julia/quartonotebookrunner.jl
@@ -31,6 +31,25 @@ if server === nothing
   error("Giving up.")
 end
 
+@info "trying to establish test connection to port $port"
+let
+  connected = false
+  for i in 1:20
+    try
+      sock = Sockets.connect(port)
+      println(sock, """{"type": "isready", "content": {}}""")
+      @assert readline(sock) == "true"
+      close(sock)
+      connected = true
+      break
+    catch
+      sleep(0.1)
+    end
+  end
+  connected || error("Test connection could not be established.")
+end
+@info "successful test connection"
+
 open(transport_file, "w") do io
   println(io, """{"port": $port, "pid": $(Base.Libc.getpid())}""")
 end
