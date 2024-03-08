9a90e7efc536edbcd91c01ae49459fe56b88fa9c
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-03-08 11:53:40 +0100

    move to quartonotebookrunner 0.5

--- src/resources/julia/quartonotebookrunner.jl
+++ src/resources/julia/quartonotebookrunner.jl
@@ -9,27 +9,8 @@ atexit() do
   rm(transport_file; force=true)
 end
 
-server, port = let
-  server = nothing
-  port = nothing
-
-  for i in 1:20
-    # find an open port by creating a server there and immediately closing it
-    port, _server = Sockets.listenany(8000)
-    close(_server)
-    try
-      server = QuartoNotebookRunner.serve(; port)
-      break
-    catch e
-      error("Opening server on port $port failed.")
-    end
-  end
-  server, port
-end
-
-if server === nothing
-  error("Giving up.")
-end
+server = QuartoNotebookRunner.serve()
+port = server.port
 
 @info "trying to establish test connection to port $port"
 let
