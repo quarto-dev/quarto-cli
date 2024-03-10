e6a2051d7326202d98558acf3dfd08e1d423be9b
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2024-03-09 19:25:07 -0500

    first pass

--- src/core/handlers/handlers.ts
+++ src/core/handlers/handlers.ts
@@ -1,11 +1,11 @@
 /*
-* handlers.ts
-*
-* Copyright (C) 2022 Posit Software, PBC
-*
-*/
+ * handlers.ts
+ *
+ * Copyright (C) 2022 Posit Software, PBC
+ */
 
 import "./mermaid.ts";
 import "./include.ts";
 import "./dot.ts";
 import "./embed.ts";
+import "./qrcode.ts";
