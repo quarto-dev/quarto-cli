1c70fe6458fca7278b128208716e6a6e6f153336
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-03-14 16:08:01 +0100

    simplify init script, server should already be `listen`ing

--- src/resources/julia/quartonotebookrunner.jl
+++ src/resources/julia/quartonotebookrunner.jl
@@ -12,27 +12,8 @@ end
 server = QuartoNotebookRunner.serve()
 port = server.port
 
-@info "trying to establish test connection to port $port"
-let
-  connected = false
-  for i in 1:20
-    try
-      sock = Sockets.connect(port)
-      println(sock, """{"type": "isready", "content": {}}""")
-      @assert readline(sock) == "true"
-      close(sock)
-      connected = true
-      break
-    catch
-      sleep(0.1)
-    end
-  end
-  connected || error("Test connection could not be established.")
-end
-@info "successful test connection"
-
 open(transport_file, "w") do io
-  println(io, """{"port": $port, "pid": $(Base.Libc.getpid())}""")
+  println(io, """{"port": $port, "pid": $(Base.Libc.getpid()), "key": "$(server.key)"}""")
 end
 
 wait(server)
