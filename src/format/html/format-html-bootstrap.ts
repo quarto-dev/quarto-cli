24df4ef43bd1a6b95eff4cb7b826241d50840f9e
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2024-03-14 15:15:26 -0700

    layout - don't set classes that will force css grid in layouts that require flex css (#9076)

--- src/format/html/format-html-bootstrap.ts
+++ src/format/html/format-html-bootstrap.ts
@@ -1159,7 +1159,10 @@ function processColumnElements(
         // Add the grid system. Children of the grid system
         // are placed into the body-content column by default
         // (CSS implements this)
-        if (!el.classList.contains("page-columns")) {
+        if (
+          !el.classList.contains("quarto-layout-row") &&
+          !el.classList.contains("page-columns")
+        ) {
           el.classList.add("page-columns");
         }
 
