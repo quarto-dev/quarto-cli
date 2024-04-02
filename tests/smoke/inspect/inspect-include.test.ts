dae58c17b5773dc1b92ca9eb7a84206c99d789d8
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2024-04-02 12:10:17 -0700

    add tests

--- tests/smoke/inspect/inspect-include.test.ts
+++ tests/smoke/inspect/inspect-include.test.ts
@@ -4,6 +4,7 @@
 * Copyright (C) 2020-2024 Posit Software, PBC
 *
 */
+import { assertObjectMatch } from "https://deno.land/std@0.93.0/assert/assert_object_match.ts";
 import { existsSync } from "../../../src/deno_ral/fs.ts";
 import {
   ExecuteOutput,
@@ -23,7 +24,11 @@ import { assert } from "testing/asserts.ts";
         verify: async (outputs: ExecuteOutput[]) => {
           assert(existsSync("docs/inspect/foo.json"));
           const json = JSON.parse(Deno.readTextFileSync("docs/inspect/foo.json"));
-          assert(json.fileInformation["docs/inspect/foo.qmd"].includeMap["docs/inspect/foo.qmd"] === "_bar.qmd");
+          assertObjectMatch(json.fileInformation["docs/inspect/foo.qmd"].includeMap[0],
+          {
+            source: input,
+            target: "_bar.qmd"
+          });
         }
       }
     ],
