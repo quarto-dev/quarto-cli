e2aca006114c08292d5bca3b4cdd9d8d2e1eeb54
Christophe Dervieux <christophe.dervieux@gmail.com>
2024-05-30 21:23:19 +0200

    Add a config to launch.json to run debug on quarto-preview build

--- src/webui/quarto-preview/build.ts
+++ src/webui/quarto-preview/build.ts
@@ -14,27 +14,34 @@ try {
 
 // check if any of our repo files have a later time
 let build = args.length > 0 && args.includes("--force");
-try {
-  const command = new Deno.Command("git", { args: ["ls-files"] });
-  const cmdOutput = await command.output();
-  if (cmdOutput.success) {
-    const output = new TextDecoder().decode(cmdOutput.stdout);
-    const files = output.split("\n").filter((line) => line.length > 0);
-    build = files.some((file) =>
-      Deno.statSync(file).mtime!.valueOf() > jsBuildTime
-    );
-  } else {
-    // not in a git repo, rebuild
+if (!build) {
+  try {
+    const command = new Deno.Command("git", { args: ["ls-files"] });
+    const cmdOutput = await command.output();
+    if (cmdOutput.success) {
+      const output = new TextDecoder().decode(cmdOutput.stdout);
+      const files = output.split("\n").filter((line) => line.length > 0);
+      build = files.some((file) =>
+        Deno.statSync(file).mtime!.valueOf() > jsBuildTime
+      );
+    } else {
+      // not in a git repo, rebuild
+      build = true;
+    }
+  } catch {
+    // git not installed, rebuild
     build = true;
   }
-} catch {
-  // git not installed, rebuild
-  build = true;
 }
 
 if (build) {
   const buildCommand = new Deno.Command(Deno.execPath(), {
     args: ["task", "build"],
   });
-  await buildCommand.spawn().status;
+  const process = buildCommand.spawn();
+  const status = await process.status;
+  if (!status) {
+    console.error("Error building quarto-preview.js");
+    Deno.exit(1);
+  }
 }
