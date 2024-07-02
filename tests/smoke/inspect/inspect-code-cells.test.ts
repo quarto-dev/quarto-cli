805d2d41924435d8ab0ae5f04684239b665bfacc
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2024-07-02 08:53:55 -0700

    regression test

--- tests/smoke/inspect/inspect-code-cells.test.ts
+++ tests/smoke/inspect/inspect-code-cells.test.ts
@@ -48,4 +48,48 @@ import { assert } from "testing/asserts.ts";
       }
     },
   );
+
+
+  
+})();
+
+(() => {
+  const input = "docs/inspect/10039.qmd";
+  const output = "docs/inspect/_10039.json";
+  testQuartoCmd(
+    "inspect",
+    [input, output],
+    [
+      {
+        name: "inspect-tagged-metadata",
+        verify: async (outputs: ExecuteOutput[]) => {
+          assert(existsSync(output));
+          const json = JSON.parse(Deno.readTextFileSync(output));
+          const info = json.fileInformation["docs/inspect/10039.qmd"];
+          const codeCells = info.codeCells;
+          assertObjectMatch(info.codeCells[1], {
+            "start": 14,
+            "end": 18,
+            "file": "docs/inspect/10039.qmd",
+            "source": "p[[1]]\n",
+            "language": "r",
+            "metadata": {
+              "label": "plot",
+              "fig-cap": {
+                "value": "names(p)[[1]]",
+                "tag": "!expr"
+              }
+            }
+          });
+        }
+      }
+    ],
+    {
+      teardown: async () => {
+        if (existsSync(output)) {
+          Deno.removeSync(output);
+        }
+      }
+    },
+  );
 })();
