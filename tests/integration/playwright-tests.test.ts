8618636afe9b409ef6da492eb358d4eb403049cd
Christophe Dervieux <christophe.dervieux@gmail.com>
2024-09-17 12:33:26 +0200

    clean ts code as server is run by playwright directly

--- tests/integration/playwright-tests.test.ts
+++ tests/integration/playwright-tests.test.ts
@@ -43,17 +43,6 @@ for (const { path: fileName } of globOutput) {
   fileNames.push(fileName);
 }
 
-// start a web server
-// This is attempt #3
-// attempt #1 through Deno.server causes hangs on repeated requests
-// attempt #2 through http/server causes a deno vendor crash: https://github.com/denoland/deno/issues/16861
-
-// we'll just use python :facepalm:
-
-// const proc = Deno.run({
-//   cmd: ["python", "-m", "http.server", "8080"],
-//   cwd: "docs/playwright",
-// });
 
 try {
   // run playwright
@@ -62,9 +51,6 @@ try {
     cwd: "integration/playwright",
   });
 } finally {
-  // cleanup
-  // proc.kill();
-  // proc.close();
   for (const fileName of fileNames) {
     cleanoutput(fileName, "html");
   }
