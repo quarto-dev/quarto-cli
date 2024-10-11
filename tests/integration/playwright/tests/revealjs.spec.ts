c39f090f58d15c4c86406820fe3aad2e3b93e7c0
Christophe Dervieux <christophe.dervieux@gmail.com>
2024-10-11 14:51:32 +0200

    Add  footer test for scrool mode too

--- tests/integration/playwright/tests/revealjs.spec.ts
+++ tests/integration/playwright/tests/revealjs.spec.ts
@@ -1,7 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('logo and footer are correctly shown', async ({ page }) => {
+test('logo and footer are correctly shown in default mode', async ({ page }) => {
   await page.goto('./revealjs/logo-footer.html#/slide-1');
+  await expect(page.locator('.reveal > .footer.footer-default')).toContainText('Footer text');
+  await expect(page.locator('.slide-logo')).toHaveAttribute("src", "quarto.png");
+  await page.keyboard.press('ArrowRight'); // Next slide
+  await expect(page.locator('.reveal > .footer.footer-default')).toContainText('Footer text');
+  await expect(page.locator('.slide-logo')).toHaveAttribute("src", "quarto.png");
+  await page.keyboard.press('ArrowRight'); // Next slide
+  await expect(page.locator('.reveal > .footer')).toBeHidden();
+  await expect(page.locator('.slide-logo')).toHaveAttribute("src", "quarto.png");
+  await page.keyboard.press('ArrowRight'); // Next slide
+  await expect(page.locator('.reveal > .footer.footer-default')).toBeHidden();
+  await expect(page.locator('.reveal > .footer:not(.footer-default)')).toContainText('A different footer');
+  await expect(page.locator('.slide-logo')).toHaveAttribute("src", "quarto.png");
+});
+
+test('logo and footer are correctly shown in scroll mode', async ({ page }) => {
+  // check scroll mode too
+  await page.goto('revealjs/logo-footer.html?view=scroll');
   await expect(page.locator('.reveal > .footer.footer-default')).toContainText('Footer text');
   await expect(page.locator('.slide-logo')).toHaveAttribute("src", "quarto.png");
   await page.keyboard.press('ArrowRight'); // Next slide
