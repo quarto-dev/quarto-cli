c2ae1c23c63c83f77bd4a45e3428c7b07ddfe82e
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2024-10-28 08:45:22 -0700

    use strict version specifiers in import maps

--- package/scripts/deno_std/deno_std.ts
+++ package/scripts/deno_std/deno_std.ts
@@ -42,13 +42,13 @@ import * as uuid from "https://deno.land/std@0.159.0/uuid/mod.ts";
 // The import map "run_import_map.json" needs to mirror these imports
 // If you edit this file, you need to update the import map as well
 
-import * as tar from "jsr:/@std/archive@^0.224.0/tar";
-import * as async from "jsr:/@std/async@^0.224.0";
-import * as bytes from "jsr:/@std/bytes@^0.224.0";
-import * as collections from "jsr:/@std/collections@^0.224.0";
-import * as crypto from "jsr:/@std/crypto@^0.224.0";
-import * as datetime from "jsr:/@std/datetime@^0.224.0";
-import * as dotenv from "jsr:/@std/dotenv@^0.224.0";
+import * as tar from "jsr:/@std/archive@0.224.3/tar";
+import * as async from "jsr:/@std/async@0.224.2";
+import * as bytes from "jsr:/@std/bytes@0.224.0";
+import * as collections from "jsr:/@std/collections@0.224.2";
+import * as crypto from "jsr:/@std/crypto@0.224.0";
+import * as datetime from "jsr:/@std/datetime@0.224.5";
+import * as dotenv from "jsr:/@std/dotenv@0.224.2";
 
 // encoding has no mod.ts
 import * as ascii85 from "https://deno.land/std@0.224.0/encoding/ascii85.ts";
@@ -56,28 +56,28 @@ import * as base32 from "https://deno.land/std@0.224.0/encoding/base32.ts";
 import * as base58 from "https://deno.land/std@0.224.0/encoding/base58.ts";
 import * as base64 from "https://deno.land/std@0.224.0/encoding/base64.ts";
 import * as base64url from "https://deno.land/std@0.224.0/encoding/base64url.ts";
-import * as csv from "jsr:/@std/csv@^0.224.0";
-import * as front_matter from "jsr:/@std/front-matter@^0.224.0";
+import * as csv from "jsr:/@std/csv@0.224.3";
+import * as front_matter from "jsr:/@std/front-matter@0.224.3";
 import * as hex from "https://deno.land/std@0.224.0/encoding/hex.ts";
-import * as jsonc from "jsr:/@std/jsonc@^0.224.0";
-import * as toml from "jsr:/@std/toml@^0.224.0";
+import * as jsonc from "jsr:/@std/jsonc@0.224.3";
+import * as toml from "jsr:/@std/toml@0.224.1";
 import * as varint from "https://deno.land/std@0.224.0/encoding/varint.ts";
-import * as yaml from "jsr:/@std/yaml@^0.224.0";
+import * as yaml from "jsr:/@std/yaml@0.224.3";
 
 import * as flags from "jsr:/@std/flags@^0.224.0";
 import * as fmt_bytes from "https://deno.land/std@0.224.0/fmt/bytes.ts";
 import * as fmt_colors from "https://deno.land/std@0.224.0/fmt/colors.ts";
 import * as fmt_printf from "https://deno.land/std@0.224.0/fmt/printf.ts";
-import * as fs from "jsr:/@std/fs@^0.224.0";
-import * as http from "jsr:/@std/http@^0.224.0";
-import * as io from "jsr:/@std/io@^0.224.0";
-import * as log from "jsr:/@std/log@^0.224.0";
-import * as mediaTypes from "jsr:/@std/media-types@^0.224.0";
-import * as path from "jsr:/@std/path@^0.224.0";
-import * as permissions from "jsr:/@std/permissions@^0.224.0";
-import * as semver from "jsr:/@std/semver@^0.224.0";
-import * as streams from "jsr:/@std/streams@^0.224.0";
-import * as uuid from "jsr:/@std/uuid@^0.224.0";
+import * as fs from "jsr:/@std/fs@0.224.0";
+import * as http from "jsr:/@std/http@0.224.5";
+import * as io from "jsr:/@std/io@0.224.8";
+import * as log from "jsr:/@std/log@0.224.7";
+import * as mediaTypes from "jsr:/@std/media-types@0.224.1";
+import * as path from "jsr:/@std/path@0.224.0";
+import * as permissions from "jsr:/@std/permissions@0.224.0";
+import * as semver from "jsr:/@std/semver@0.224.3";
+import * as streams from "jsr:/@std/streams@0.224.5";
+import * as uuid from "jsr:/@std/uuid@0.224.3";
 
 // seems like jsr doesn't export version numbers?!
 // import * as version from "jsr:/@std/version@^0.224.0";
