e846425dddd9bf3648efb73aac64a79146cf615d
Christophe Dervieux <christophe.dervieux@gmail.com>
2024-10-30 11:33:38 +0000

    Add a test for the environment variable QUARTO_VERSION_REQUIREMENT
    Follow up on 00249a46bab6e3e45adb9900677a33e599cb10d3 which closed #10442

--- tests/smoke/render/render-required.test.ts
+++ tests/smoke/render/render-required.test.ts
@@ -16,3 +16,26 @@ testQuartoCmd(
   [input],
   [printsMessage("ERROR", /does not satisfy version/)]
 );
+
+let oldVersionRequirement: string | undefined;
+
+testQuartoCmd(
+  "render",
+  [input, "--to", "html+norequire"],
+  [printsMessage("ERROR", /does not meet semver requirement/)],
+  {
+    setup: async () => {
+      // Save current version of QUARTO_VERSION_REQUIREMENT env var and set it to a value that will not be satisfied
+      oldVersionRequirement = Deno.env.get("QUARTO_VERSION_REQUIREMENT");
+      Deno.env.set("QUARTO_VERSION_REQUIREMENT", "< 0.0.0");
+    },
+    teardown: async () => {
+      // Restore QUARTO_VERSION_REQUIREMENT
+      if (oldVersionRequirement) {
+        Deno.env.set("QUARTO_VERSION_REQUIREMENT", oldVersionRequirement);
+      } else {
+        Deno.env.delete("QUARTO_VERSION_REQUIREMENT");
+      }
+    },
+  }
+);
