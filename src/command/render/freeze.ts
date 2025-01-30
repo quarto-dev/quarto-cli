5325c4fe3d27572dce1fc602971140fea0ad377a
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2025-01-30 13:30:58 -0500

    [perf] freeze - no cloneDeep

--- src/command/render/freeze.ts
+++ src/command/render/freeze.ts
@@ -55,13 +55,15 @@ export function freezeExecuteResult(
   result: ExecuteResult,
 ) {
   // resolve includes within executeResult
-  result = cloneDeep(result) as ExecuteResult;
+  const innerResult = {
+    ...result,
+  } as ExecuteResult;
   const resolveIncludes = (
     name: "include-in-header" | "include-before-body" | "include-after-body",
   ) => {
-    if (result.includes) {
-      if (result.includes[name]) {
-        result.includes[name] = result.includes[name]!.map((file) =>
+    if (innerResult.includes) {
+      if (innerResult.includes[name]) {
+        innerResult.includes[name] = innerResult.includes[name]!.map((file) =>
           // Storing file content using LF line ending
           format(Deno.readTextFileSync(file), LF)
         );
@@ -73,7 +75,7 @@ export function freezeExecuteResult(
   resolveIncludes(kIncludeAfterBody);
 
   // make the supporting dirs relative to the input file dir
-  result.supporting = result.supporting.map((file) => {
+  innerResult.supporting = innerResult.supporting.map((file) => {
     if (isAbsolute(file)) {
       return relative(normalizePath(dirname(input)), file);
     } else {
@@ -88,7 +90,7 @@ export function freezeExecuteResult(
   const freezeJsonFile = freezeResultFile(input, output, true);
   Deno.writeTextFileSync(
     freezeJsonFile,
-    JSON.stringify({ hash, result }, undefined, 2),
+    JSON.stringify({ hash, result: innerResult }, undefined, 2),
   );
 
   // return the file
