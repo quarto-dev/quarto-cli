ac2ecdf17f4a454fdfc83fb1bb7c30c7f32247c3
Gordon Woodhull <gordon.woodhull@posit.co>
2025-02-24 11:15:52 -0500

    update ctheorem for typst 0.13 compatibility

--- src/resources/filters/customnodes/theorem.lua
+++ src/resources/filters/customnodes/theorem.lua
@@ -93,7 +93,7 @@ local letted_typst_theorem = {}
 local function ensure_typst_theorems(reftype)
   if not included_typst_theorems then
     included_typst_theorems = true
-    quarto.doc.include_text("in-header", "#import \"@preview/ctheorems:1.1.0\": *")
+    quarto.doc.include_text("in-header", "#import \"@preview/ctheorems:1.1.3\": *")
     quarto.doc.include_text("in-header", "#show: thmrules")
   end
   if not letted_typst_theorem[reftype] then
