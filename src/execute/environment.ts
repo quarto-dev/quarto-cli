7ed6a0382e896ee8e49d50bdf4e5979ccbb6c7fc
Christophe Dervieux <christophe.dervieux@gmail.com>
2025-03-12 14:30:20 +0100

    computation - set `QUARTO_DOCUMENT_FILE` to the path of the input file so that execution engine can access it
    And add test for each engine we support

--- src/execute/environment.ts
+++ src/execute/environment.ts
@@ -4,7 +4,7 @@
  * Copyright (C) 2024 Posit Software, PBC
  */
 
-import { dirname } from "../deno_ral/path.ts";
+import { basename, dirname } from "../deno_ral/path.ts";
 import { ExecuteOptions } from "./types.ts";
 import { InternalError } from "../core/lib/error.ts";
 
@@ -14,7 +14,10 @@ export const setExecuteEnvironment: (options: ExecuteOptions) => void = (
   if (options.projectDir) {
     Deno.env.set("QUARTO_PROJECT_ROOT", options.projectDir);
     Deno.env.set("QUARTO_DOCUMENT_PATH", dirname(options.target.source));
+    Deno.env.set("QUARTO_DOCUMENT_FILE", basename(options.target.source));
   } else {
+    // FIXME: This should not be passthrough anymore as singleFileProjectContext always set `options.projectDir`
+    // https://github.com/quarto-dev/quarto-cli/pull/8771
     if (!options.cwd) {
       throw new InternalError(
         "No project directory or current working directory",
@@ -22,5 +25,6 @@ export const setExecuteEnvironment: (options: ExecuteOptions) => void = (
     }
     Deno.env.set("QUARTO_PROJECT_ROOT", options.cwd);
     Deno.env.set("QUARTO_DOCUMENT_PATH", options.cwd);
+    Deno.env.set("QUARTO_DOCUMENT_FILE", basename(options.target.source));
   }
 };
