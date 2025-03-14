41526a7ec4749d81b3866f16d1a4e3480e76b060
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2025-03-14 21:40:50 +0100

    add description for `call`

--- src/command/call/cmd.ts
+++ src/command/call/cmd.ts
@@ -3,6 +3,7 @@ import { engineCommand } from "../../execute/engine.ts";
 
 export const callCommand = new Command()
   .name("call")
+  .description("Access functions of Quarto subsystems such as its rendering engines.")
   .action(() => {
     callCommand.showHelp();
     Deno.exit(1);
