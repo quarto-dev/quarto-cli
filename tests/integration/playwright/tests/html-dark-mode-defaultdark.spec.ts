a3e6b94af860dab19982d294f545a5fb7c837bea
Gordon Woodhull <gordon.woodhull@posit.co>
2025-04-05 19:14:08 -0400

    set visual toggle once page is loaded
    also, exhaustively test visual toggle

--- tests/integration/playwright/tests/html-dark-mode-defaultdark.spec.ts
+++ tests/integration/playwright/tests/html-dark-mode-defaultdark.spec.ts
@@ -15,6 +15,11 @@ async function check_backgrounds(page, class_, primary, secondary) {
   await expect(locatr2).toHaveCSS('background-color', secondary);
 }
 
+async function check_toggle(page, alternate) {
+  const locatr = await page.locator("a.quarto-color-scheme-toggle");
+  await expect(locatr).toHaveClass(`top-right quarto-color-scheme-toggle${alternate?" alternate":""}`)
+}
+
 test.use({
   colorScheme: 'dark'
 });
@@ -27,26 +32,34 @@ const red = 'rgb(66, 7, 11)';
 test('Dark and light brand after user themes', async ({ page }) => {
   // brand overrides theme background color
   await page.goto('./html/dark-brand/brand-after-theme.html');
+  await check_toggle(page, true);
   await check_backgrounds(page, 'quarto-dark', red, blue);
+  await check_toggle(page, false);
 });
 
 // project tests
 
 test('Project specifies light and dark brands', async ({ page }) => {
   await page.goto('./html/dark-brand/project-light/simple.html');
+  await check_toggle(page, false);
   await check_backgrounds(page, 'quarto-light', blue, red);
+  await check_toggle(page, true);
 });
 
 
 test('Project specifies dark and light brands', async ({ page }) => {
   await page.goto('./html/dark-brand/project-dark/simple.html');
+  await check_toggle(page, true);
   await check_backgrounds(page, 'quarto-dark', red, blue);
+  await check_toggle(page, false);
 });
 
 
 test('Project specifies light and dark brands and respect-user-color-scheme', async ({ page }) => {
   await page.goto('./html/dark-brand/project-light/simple-respect-color-scheme.html');
+  await check_toggle(page, true);
   await check_backgrounds(page, 'quarto-dark', red, blue);
+  await check_toggle(page, false);
 });
 
 test('Project specifies light and dark brands, dynamic respect-user-color-scheme', async ({ page }) => {
@@ -54,10 +67,12 @@ test('Project specifies light and dark brands, dynamic respect-user-color-scheme
   const locatr = await page.locator('body').first();
   await expect(locatr).toHaveClass(`fullcontent quarto-dark`);
   await expect(locatr).toHaveCSS('background-color', red);
+  await check_toggle(page, true);
 
   await page.emulateMedia({ colorScheme: 'light' });
   await expect(locatr).toHaveClass(`fullcontent quarto-light`);
   await expect(locatr).toHaveCSS('background-color', blue);
+  await check_toggle(page, false);
 });
 
 test('Project specifies dark and light brands, dynamic respect-user-color-scheme', async ({ page }) => {
@@ -65,8 +80,10 @@ test('Project specifies dark and light brands, dynamic respect-user-color-scheme
   const locatr = await page.locator('body').first();
   await expect(locatr).toHaveClass(`fullcontent quarto-dark`);
   await expect(locatr).toHaveCSS('background-color', red);
+  await check_toggle(page, true);
 
   await page.emulateMedia({ colorScheme: 'light' });
   await expect(locatr).toHaveClass(`fullcontent quarto-light`);
   await expect(locatr).toHaveCSS('background-color', blue);
+  await check_toggle(page, false);
 });
