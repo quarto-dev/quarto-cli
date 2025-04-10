2624a2f6b0b55b1a40342e7df6ac62b3977ace00
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2025-04-10 13:48:22 -0400

    kbd,asciidoc - explicitly escape inline

--- src/resources/extensions/quarto/kbd/kbd.lua
+++ src/resources/extensions/quarto/kbd/kbd.lua
@@ -52,7 +52,7 @@ return {
           shortcutText = shortcutText:gsub("]", "\\]")
         end
 
-        return pandoc.RawInline("asciidoc", "kbd:[" .. shortcutText .. "]")
+        return pandoc.RawInline("asciidoc", "kbd:[" .. stringEscape(shortcutText, "asciidoc") .. "]")
       else
         return quarto.shortcode.error_output("kbd", "kbd only supports one positional argument", "inline")
       end
