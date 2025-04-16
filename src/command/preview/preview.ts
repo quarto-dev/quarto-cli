177e821129d79b70bfb565244e8a965d9f9ff9b1
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2025-04-16 14:49:59 -0400

    deno 2 - serveHttp -> serve

--- src/command/preview/preview.ts
+++ src/command/preview/preview.ts
@@ -195,9 +195,10 @@ export async function preview(
     ...(await resolvePreviewOptions(options)),
   };
 
+  const ac = new AbortController();
   // create listener and callback to stop the server
-  const listener = Deno.listen({ port: options.port!, hostname: options.host });
-  const stopServer = () => listener.close();
+  // const listener = Deno.listen({ port: options.port!, hostname: options.host });
+  const stopServer = () => ac.abort();
 
   // ensure resources
   previewEnsureResources(stopServer);
@@ -271,23 +272,22 @@ export async function preview(
   previewMonitorResources(stopServer);
 
   // serve project
-  for await (const conn of listener) {
-    (async () => {
+  const server = Deno.serve(
+    { signal: ac.signal, port: options.port!, hostname: options.host },
+    async (req: Request) => {
       try {
-        for await (const { request, respondWith } of Deno.serveHttp(conn)) {
-          await respondWith(handler(request));
-        }
+        return await handler(req);
       } catch (err) {
-        warning(err.message);
-        try {
-          conn.close();
-        } catch {
-          //
+        if (err instanceof Error) {
+          warning(err.message);
         }
+        throw err;
       }
-    })();
-  }
+    },
+  );
+  await server.finished;
 }
+
 export interface PreviewRenderRequest {
   version: 1 | 2;
   path: string;
@@ -530,7 +530,7 @@ export function createChangeHandler(
 
       return result;
     } catch (e) {
-      if (e.message) {
+      if (e instanceof Error && e.message) {
         // jupyter notebooks being edited in juptyerlab sometimes get an
         // "Unexpected end of JSON input" error that remedies itself (so we ignore).
         // this may be a result of an intermediate save result?
