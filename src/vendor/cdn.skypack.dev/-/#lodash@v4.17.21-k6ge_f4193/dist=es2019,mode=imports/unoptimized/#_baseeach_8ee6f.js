09bd1262c25130d9c96663a42716d4e076574cf5
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2025-04-22 14:23:49 -0400

    chore - deno 2, no more explicit vendoring

--- src/vendor/cdn.skypack.dev/-/#lodash@v4.17.21-k6ge_f4193/dist=es2019,mode=imports/unoptimized/#_baseeach_8ee6f.js
+++
@@ -1,5 +0,0 @@
-import baseForOwn2 from "../unoptimized/_baseForOwn.js";
-import createBaseEach2 from "../unoptimized/_createBaseEach.js";
-var baseEach = createBaseEach2(baseForOwn2);
-var __VIRTUAL_FILE = baseEach;
-export default __VIRTUAL_FILE;
--- src/vendor/cdn.skypack.dev/-/#lodash@v4.17.21-k6ge_f4193/dist=es2019,mode=imports/unoptimized/constant.js
+++
@@ -1,7 +0,0 @@
-function constant(value) {
-  return function() {
-    return value;
-  };
-}
-var __VIRTUAL_FILE = constant;
-export default __VIRTUAL_FILE;
