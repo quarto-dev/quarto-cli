56d4d57646e417b6786f9cb08bc3e30f86a939b9
Gordon Woodhull <gordon.woodhull@posit.co>
2025-10-20 19:44:32 -0400

    EngineProjectContext is an adapted, limited ProjectContext
    with only the types and utilities that engines need
    
    add @quarto/types package, secret for now,
    but otherwise as if we were publishing types
    
    wip, somewhat sloppy implementation by Claude
    will need cleanup!

---
+++ packages/quarto-types/src/constants.ts
@@ -0,0 +1,36 @@
+/**
+ * Constants used across Quarto engine interfaces
+ */
+
+/**
+ * Project type constants
+ */
+export const kProjectType = "type";
+
+/**
+ * Project output directory key
+ */
+export const kProjectOutputDir = "output-dir";
+
+/**
+ * Project execution directory key
+ */
+export const kProjectExecuteDir = "execute-dir";
+
+/**
+ * Project library directory key
+ */
+export const kProjectLibDir = "lib-dir";
+
+/**
+ * Standard engine names
+ */
+export const kMarkdownEngine = "markdown";
+export const kJupyterEngine = "jupyter";
+export const kKnitrEngine = "knitr";
+export const kJuliaEngine = "julia";
+
+/**
+ * Standard file extensions
+ */
+export const kQmdExtensions = [".qmd"];
