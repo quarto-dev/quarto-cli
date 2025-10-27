2e16fca93ff4a73780c9078bc220c54bb9b0e849
Christophe Dervieux <christophe.dervieux@gmail.com>
2025-10-27 16:05:05 +0100

    Add missing tests from tinytex R package known errors

--- tests/unit/latexmk/parse-error.test.ts
+++ tests/unit/latexmk/parse-error.test.ts
@@ -49,6 +49,29 @@ unitTest("Detect missing files with `findMissingFontsAndPackages`", async () => 
   assertFound("! I can't find file `hyph-de-1901.ec.tex'.", "hyph-de-1901.ec.tex");
   assertFound("luaotfload-features.lua:835: module 'lua-uni-normalize' not found:", "lua-uni-algos.lua");
   assertFound("! LuaTeX-ja error: File 'jfm-zh_CN.lua' not found.", "jfm-zh_CN.lua");
+
+  // Additional test cases from tinytex R package examples (latex.R lines 537-607)
+  // https://github.com/rstudio/tinytex/blob/e96be3143b9af07768a124215b5fb5a1e6d183d3/R/latex.R#L538-L558
+  // xdvipdfmx variant of TFM error
+  assertFound('xdvipdfmx:fatal: Unable to find TFM file "rsfs10"', fontSearchTerm("rsfs10"));
+  // biblatex bibliography style file
+  assertFound("Package biblatex Info: ... file 'trad-abbrv.bbx' not found", "trad-abbrv.bbx");
+  // epstopdf - eps-converted-to.pdf pattern
+  assertFound("! Package pdftex.def Error: File `logo-mdpi-eps-converted-to.pdf' not found", "epstopdf");
+  // epstopdf - pdf_ref_obj pattern
+  assertFound("! xdvipdfmx:fatal: pdf_ref_obj(): passed invalid object.", "epstopdf");
+  // tikz library with error context
+  assertFound(
+    "! Package tikz Error: I did not find the tikz library 'hobby'. This error message was issued because the library or one of its sublibraries could not be found, probably because of a misspelling. Processed options: \"library={hobby}\". The possibly misspelled library name is \"hobby\". The library name should be one of the following (or you misspelled it): named tikzlibraryhobby.code.tex",
+    "tikzlibraryhobby.code.tex"
+  );
+  // support file missing
+  assertFound("support file `supp-pdf.mkii' (supp-pdf.tex) is missing", "supp-pdf.mkii");
+
+  // Note: The following error types from tinytex are not yet testable because
+  // patterns don't exist in parse-error.ts:
+  // - .fd files: "No file LGRcmr.fd. ! LaTeX Error: This NFSS system isn't set up properly."
+  // - pdfx color profiles: "! Package pdfx Error: No color profile sRGB_IEC61966-2-1_black_scaled.icc found"
 },{
   cwd: () => "unit/latexmk/"
 })
