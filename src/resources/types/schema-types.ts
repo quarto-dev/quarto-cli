26cbcf6c4a7e4705dad8310a5dcdc1c8ed9e001f
Gordon Woodhull <gordon.woodhull@posit.co>
2025-11-13 18:33:28 -0500

    artifacts

--- src/resources/types/schema-types.ts
+++ src/resources/types/schema-types.ts
@@ -154,6 +154,10 @@ website:
 ``` */
 };
 
+export type ExternalEngine = {
+  path: string; /* Path to the TypeScript module for the execution engine */
+}; /* An execution engine not pre-loaded in Quarto */
+
 export type DocumentCommentsConfiguration = false | {
   giscus?: GiscusConfiguration;
   hypothesis?: boolean | {
