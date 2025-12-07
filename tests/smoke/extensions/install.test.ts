9d4a4213fbea6a06f8b984453728c81b36a9ee18
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-12-07 17:24:57 +0100

    chore: rm duplicated test

--- tests/smoke/extensions/install.test.ts
+++ tests/smoke/extensions/install.test.ts
@@ -153,30 +153,6 @@ testQuartoCmd(
   },
 );
 
-// Verify that updating an extension uses the normalized source
-testQuartoCmd(
-  "install",
-  ["extension", "quarto-ext/lightbox@v0.1.4", "--no-prompt"],
-  [
-    noErrorsOrWarnings,
-    verifySubDirCount("_extensions", 1),
-    verifySubDirName("_extensions", "quarto-ext"),
-    verifyExtensionSourceField(
-      join("_extensions", "quarto-ext", "lightbox"),
-      "quarto-ext/lightbox",
-    ),
-  ],
-  {
-    cwd: () => {
-      return workingDir;
-    },
-    teardown: () => {
-      Deno.removeSync("_extensions", { recursive: true });
-      return Promise.resolve();
-    },
-  },
-);
-
 // Verify that 'quarto update extension' uses the normalized source from manifest
 testQuartoCmd(
   "update",
