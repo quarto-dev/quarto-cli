quarto-dev/quarto-cli

commit 72452ee4adfe548c6633d14ecebc5f484cb2ff53

pandoc, format extensions - Don't use yet new table_attributes extension
This new syntax is like the one in Quarto
````
  : Here's the caption. {#ident .class key="value"}
````

So it needs some adaptation to work with our crossref system to avoid duplicate ids.

src/resources/pandoc/datadir/readqmd.lua
@@ -138,6 +138,11 @@ local function readqmd(txt, opts)
     end
   end
 
+  -- ### Opt-out some extensions that we know we won't support for now ###
+  -- https://pandoc.org/MANUAL.html#extension-table_attributes
+  -- https://github.com/quarto-dev/quarto-cli/pull/13249#issuecomment-3715267414
+  flavor.extensions["table_attributes"] = false
+
   -- Format flavor, i.e., which extensions should be enabled/disabled.
   local function restore_invalid_tags(tag)
     return tags[tag] or tag

commit ee3f2322c83ac1068c7ca1ec99293da4c2ffdf66

pandoc, format extensions - Check extension support before disabling
The unconditional disabling of table_attributes in 72452ee caused errors for formats like commonmark_x that don't support this extension. Pandoc 3.8.3+ validates all extension keys (both enabled and disabled) and rejects unsupported extensions.

Use pandoc.format.all_extensions() to check if table_attributes is supported by the format before attempting to disable it.

src/resources/pandoc/datadir/readqmd.lua
@@ -141,7 +141,11 @@ local function readqmd(txt, opts)
   -- ### Opt-out some extensions that we know we won't support for now ###
   -- https://pandoc.org/MANUAL.html#extension-table_attributes
   -- https://github.com/quarto-dev/quarto-cli/pull/13249#issuecomment-3715267414
-  flavor.extensions["table_attributes"] = false
+  -- Only disable if the extension is actually supported by the format
+  local all_exts = pandoc.format.all_extensions(flavor.format)
+  if all_exts:includes('table_attributes') then
+    flavor.extensions["table_attributes"] = false
+  end
 
   -- Format flavor, i.e., which extensions should be enabled/disabled.
   local function restore_invalid_tags(tag)