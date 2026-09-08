b2cf43bfd5da6d50380f897103cbbddd03f27465
Christophe Dervieux <christophe.dervieux@gmail.com>
2026-09-08 16:14:11 +0200

    update renv

--- tests/renv/activate.R
+++ tests/renv/activate.R
@@ -2,8 +2,8 @@
 local({
 
   # the requested version of renv
-  version <- "1.2.3"
-  attr(version, "md5") <- "1bd9f58e1cfe27ce035933937c6f03de"
+  version <- "1.2.4"
+  attr(version, "md5") <- "632e650d47112edabd7cf692f950e5ac"
   attr(version, "sha") <- NULL
 
   # the project directory
